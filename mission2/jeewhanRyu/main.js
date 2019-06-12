742eebb885c06507331c09a8472793bd5e510634
Jeewhan <distinct19@gmail.com>
2019-06-12 23:50:58 +0900

    Update: arrow function

--- mission2/jeewhanRyu/main.js
+++ mission2/jeewhanRyu/main.js
@@ -1,8 +1,8 @@
 (function() {
   document
     .querySelector("#search-keyword")
-    .addEventListener("keyup", function(e) {
-      fetch(`https://jjalbot.com/api/jjals?text=${e.target.value}`)
+    .addEventListener("keyup", ({ target: { value } }) => {
+      fetch(`https://jjalbot.com/api/jjals?text=${value}`)
         .then(x => x.json())
         .then(data => {
           console.log(JSON.stringify(data, null, 2));
