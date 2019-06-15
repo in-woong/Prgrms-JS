1c83504d76b3f4bb51ad066b18739ed2c14c235b
이강호 <kangho@bluehole.net>
2019-06-15 11:03:45 +0900

    isLoading 가능하도록 변경

--- mission2/kangho/search-result.js
+++ mission2/kangho/search-result.js
@@ -2,18 +2,28 @@ const SearchResult = function(data, selector) {
 
   this.$container = document.querySelector(selector);
   this.result = data;
+  this.isLoading = false;
 
-  this.render = function() {
-    const template = this.result
-      .map(item => `<div><img src="${item.imageUrl}" /></div>`)
-      .join('');
+  this.render = function(template) {
     this.$container.innerHTML = template;
   }
 
+  this.createTemplate = function() {
+    let template = '';
+    template += `${this.isLoading ? '<div>Loading....</div>' : ''}`;
+    template += this.result
+      .map(item => `<div><img src="${item.imageUrl}" /></div>`)
+      .join('');
+    return template;
+  }
+
   this.setState = function(data) {
-    if (data !== this.result) {
-      this.result = data;
-      this.render();
+    const { result, isLoading } = data;
+    if (result !== this.result || isLoading !== this.isLoading) {
+      this.result = result;
+      this.isLoading = isLoading;
+      const template = this.createTemplate();
+      this.render(template);
     }
   }
 }
