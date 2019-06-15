0497b69901c75d87e86da61910f9e4977c6039fd
rotoshine <rotoshine@gmail.com>
2019-06-15 16:20:36 +0900

    검색 키워드 화면에 렌더링하도록 수정

--- mission2/roto/SearchResult.js
+++ mission2/roto/SearchResult.js
@@ -1,5 +1,5 @@
 function SearchResult(initalData, target) {
-  const $target = document.querySelector(target);
+  const $target = document.querySelector(target)
   let data = initalData
 
   this.setState = function (nextData) {
@@ -7,6 +7,20 @@ function SearchResult(initalData, target) {
     this.render()
   }
   this.render = function () {
-    $target.innerHTML = `${data.map((d) => `<img src="${d.imageUrl}">`).join('')}`
+    if (!data) {
+      throw new Error('[SearchResult] data가 올바르지 않습니다.')
+    }
+    if (!data.hasOwnProperty('images')) {
+      throw new Error('[SearchResult] data에 images parameter가 없습니다.')
+    }
+
+    const keyword = data.keyword
+    const images = data.images
+
+    const searchKeywordHTMLString = typeof keyword === 'string' && keyword.length > 0 ? `<h1>${keyword} search result</h1>` : ''
+    const imagesHTMLString = `${images.map((d) => `<img src="${d.imageUrl}">`).join('')}`
+
+    $target.innerHTML = `${searchKeywordHTMLString}<div>${imagesHTMLString}</div>`
+
   }
 }
