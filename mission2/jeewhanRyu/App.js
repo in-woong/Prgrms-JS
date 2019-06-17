059897bfbb59234bf94d3eff91b0572f0cb82380
Jeewhan <distinct19@gmail.com>
2019-06-18 00:45:40 +0900

    Apply: try catch, remove symbol

--- mission2/jeewhanRyu/App.js
+++ mission2/jeewhanRyu/App.js
@@ -5,40 +5,30 @@ import SearchResult from "./SearchResult.js";
 
 import * as types from "./types.js";
 
-const App = (() => {
-  const initialize = Symbol("initialize");
-  const setState = Symbol("setState");
-
-  return class {
-    constructor($target) {
-      if (!$target) {
-        thrower(types.SEARCH_INPUT__INVALID_TARGET);
-      } else {
-        Object.assign(this, { $target });
-        this[initialize]();
-      }
+export default class App {
+  constructor($target) {
+    if (!$target) {
+      thrower(types.SEARCH_INPUT__INVALID_TARGET);
+    } else {
+      Object.assign(this, { $target });
+      this.initialize();
     }
+  }
 
-    [setState](keyword, data) {
-      this.$searchKeywordResult.innerHTML = `'${keyword}' 검색 결과`;
-      this.SearchResult.render(data);
-    }
+  setState(keyword, data) {
+    this.$searchKeywordResult.innerHTML = `'${keyword}' 검색 결과`;
+    this.SearchResult.render(data);
+  }
 
-    [initialize]() {
-      this.$target.innerHTML = `<input id="${
-        types.SEARCH_KEYWORD_ID
-      }" /><span id="${types.SEARCH_KEYWORD_RESULT_ID}"></span><div id="${
-        types.SEARCH_RESULT_ID
-      }"></div>`;
+  initialize() {
+    this.$target.innerHTML = `<input id="${
+      types.SEARCH_KEYWORD_ID
+    }" /><span id="${types.SEARCH_KEYWORD_RESULT_ID}"></span><div id="${
+      types.SEARCH_RESULT_ID
+    }"></div>`;
 
-      this.SearchInput = new SearchInput(
-        this.$target,
-        this[setState].bind(this)
-      );
-      this.SearchResult = new SearchResult($(`#${types.SEARCH_RESULT_ID}`));
-      this.$searchKeywordResult = $(`#${types.SEARCH_KEYWORD_RESULT_ID}`);
-    }
-  };
-})();
-
-export default App;
+    this.SearchInput = new SearchInput(this.$target, this.setState.bind(this));
+    this.SearchResult = new SearchResult($(`#${types.SEARCH_RESULT_ID}`));
+    this.$searchKeywordResult = $(`#${types.SEARCH_KEYWORD_RESULT_ID}`);
+  }
+}
--- mission2/jeewhanRyu/SearchInput.js
+++ mission2/jeewhanRyu/SearchInput.js
@@ -3,37 +3,34 @@ import fetcher from "./fetcher.js";
 
 import * as types from "./types.js";
 
-const SearchInput = (() => {
-  const initialize = Symbol("initialize");
-
-  return class {
-    constructor($target, handleChange) {
-      if (!$target) {
-        thrower(types.SEARCH_INPUT__INVALID_TARGET);
-      } else if (typeof handleChange !== "function") {
-        thrower(types.SEARCH_INPUT__INVALID_HANDLE_CHANGE);
-      } else {
-        Object.assign(this, { $target, handleChange });
-        this[initialize]();
-      }
+export default class SearchInput {
+  constructor($target, handleChange) {
+    if (!$target) {
+      thrower(types.SEARCH_INPUT__INVALID_TARGET);
+    } else if (typeof handleChange !== "function") {
+      thrower(types.SEARCH_INPUT__INVALID_HANDLE_CHANGE);
+    } else {
+      Object.assign(this, { $target, handleChange });
+      this.initialize();
     }
+  }
 
-    [initialize]() {
-      $(`#${types.SEARCH_KEYWORD_ID}`).addEventListener(
-        "keyup",
-        ({ target: { value } }) => {
-          if (this.timer) clearTimeout(this.timer);
+  initialize() {
+    $(`#${types.SEARCH_KEYWORD_ID}`).addEventListener(
+      "keyup",
+      ({ target: { value } }) => {
+        if (this.timer) clearTimeout(this.timer);
 
-          if (value) {
-            this.timer = setTimeout(
-              async () => this.handleChange(value, await fetcher(value)),
-              1000
-            );
-          }
+        if (value) {
+          this.timer = setTimeout(async () => {
+            try {
+              this.handleChange(value, await fetcher(value));
+            } catch (error) {
+              throw error;
+            }
+          }, 1000);
         }
-      );
-    }
-  };
-})();
-
-export default SearchInput;
+      }
+    );
+  }
+}
