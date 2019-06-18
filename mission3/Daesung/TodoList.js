1d4f12af315e060bb2bc6941eb3b9da9e0921718
DS Park <cckn1985@gmail.com>
2019-06-19 01:04:18 +0900

    ### remove button 추가 및 기능 구현 (6/19) - todo item에 id 부여 - remove Button 추가   - click eventListener 추가   - id 비교하여 todo 삭제 - 삭제된 데이터가 todo 추가시 다시 살아나는 문제 수정   - main.js에서 input form을 이용해 todo 추가시 todoList.data 사용하도록 변경

--- mission3/Daesung/TodoList.js
+++ mission3/Daesung/TodoList.js
@@ -1,19 +1,46 @@
 function TodoList($target, data) {
   this.$target = $target;
   this.data = data;
+  console.log(this.data);
 
   this.setState = function(data) {
     this.data = data;
     this.render();
   };
 
+  this.addEvents = () => {
+    $target.addEventListener("click", event => {
+      const id = parseInt(event.target.parentNode.id);
+      this.data = this.data.filter(todo => {
+        return todo.id !== id;
+      });
+      console.log(this.data);
+
+      this.setState(this.data);
+    });
+  };
+
   this.render = function() {
-    const htmlString = this.data.map(function(todo) {
-      return `<li>${todo.text}</li>`;
+    const $todoList = document.createElement("ul");
+
+    this.data.map(function(todo) {
+      const li = document.createElement("li");
+      li.id = todo.id;
+
+      const delBtn = document.createElement("button");
+      const span = document.createElement("span");
+      delBtn.innerText = "remove";
+      delBtn.classList = "removeBotton";
+      span.innerText = li.id + todo.text;
+      li.appendChild(span);
+      li.appendChild(delBtn);
+
+      $todoList.appendChild(li);
     });
 
-    $target.innerHTML = `<ul>${htmlString.join("")}</ul>`;
+    $target.innerHTML = $todoList.innerHTML;
   };
 
   this.render();
+  this.addEvents();
 }
--- mission3/Daesung/main.js
+++ mission3/Daesung/main.js
@@ -2,37 +2,39 @@
   // 요 기본 데이터는 자유롭게 작성하세요.
   const data = [
     {
+      id: 0,
       text: "JavaScript 문서 읽기",
-      isCompleted: false,
+      isCompleted: false
     },
     {
+      id: 1,
       text: "로토에게 질문하기",
-      isCompleted: false,
-    },
-    {
-      text: "Pull Request 만들기",
-      isCompleted: false,
-    },
-    {
-      text: "Slack 접속하기",
-      isCompleted: true,
-    },
+      isCompleted: false
+    }
+    // {
+    //   text: "Pull Request 만들기",
+    //   isCompleted: false,
+    // },
+    // {
+    //   text: "Slack 접속하기",
+    //   isCompleted: true,
+    // },
   ];
 
   const todoList = new TodoList(document.querySelector("#todo-list"), data);
+  let id = 2;
 
   document
     .querySelector("#add-todo-button")
     .addEventListener("click", function() {
       const todoText = document.querySelector("#todo-input").value;
-
       if (todoText.length > 0) {
-        data.push({
+        todoList.data.push({
+          id: id++,
           text: todoText,
-          isCompleted: false,
+          isCompleted: false
         });
-
-        todoList.setState(data);
+        todoList.setState(todoList.data);
       }
     });
 })();
