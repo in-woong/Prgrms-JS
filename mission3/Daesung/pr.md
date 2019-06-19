7c8a96f6bfda88602ebdc3cbe2861a3f846f52a1
DS Park <cckn1985@gmail.com>
2019-06-20 01:00:21 +0900

    ### 데이터 형식 변경, 기본 기능 구현 완료 (6/20) - 데이터 형식 Array에서 Object로 변경 - 기본 기능 구현 완료 - TODO : 한 파일에 들어있는 기능들을 컴포넌트화 시켜야 함.

--- mission3/Daesung/pr.md
+++ mission3/Daesung/pr.md
@@ -1,13 +1,14 @@
+# 작업내역 
 ## 기본 요구사항
 
 - [x] 변수 선언은 `var` 대신 `let`, `const`를 사용해주세요.
 - [x] `<input type="text">` input에 할 일을 입력하여 `TodoList` 컴포넌트의 데이터에 추가하도록 합니다.
   - [x] 별도의 입력 버튼을 만들어서 버튼 클릭 시 추가 되게 하거나
   - [ ] 엔터키 입력으로 처리하거나 하는 건 자유롭게 합니다.
-- [ ] `TodoList`로 그려지는 할 일 목록에 아래의 기능을 추가합니다.
+- [x] `TodoList`로 그려지는 할 일 목록에 아래의 기능을 추가합니다.
   - [x] 할 일 텍스트 뒤에 버튼을 하나 추가합니다. 
     - [x] 해당 버튼을 클릭하여 할 일이 삭제되게 만듭니다.
-  - [ ] 할 일 텍스트를 클릭하면 해당 Todo의 `isCompleted` 값을 `true`로 만듭니다.
+  - [x] 할 일 텍스트를 클릭하면 해당 Todo의 `isCompleted` 값을 `true`로 만듭니다.
     - [x] `isCompleted`가 `true`인 경우와 `false`인 경우를 구분할 수 있도록 `TodoList`의 html string을 작성합니다.
       - [x] Todo Text 앞에 `(완료)` 라는 텍스트를 붙이는 방법
       - [x] 삭선 처리 하는 방법(`<strike>`태그로 감싸기)
@@ -23,8 +24,8 @@
   - [ ] 해당 컴포넌트는 총 Todo의 갯수, 완료처리된 Todo의 갯수를 표시합니다.
   - [ ] `TodoList` 컴포넌트 아래에 렌더링 되도록 합니다.
 - [ ] `App` 이라는 이름의 컴포넌트를 만든 뒤 위 3개의 컴포넌트가 유기적으로 동작하도록 만듭니다.
-- [ ] Todo의 완료 처리 및 삭제 시 event deligation을 이용합니다.
-  - [ ] https://joshua1988.github.io/web-development/javascript/event-propagation-delegation/#%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EC%9C%84%EC%9E%84---event-delegation 요 게시글을 참고하여 이벤트 위임을 구현합니다.
+- [x] Todo의 완료 처리 및 삭제 시 event deligation을 이용합니다.
+  - [x] https://joshua1988.github.io/web-development/javascript/event-propagation-delegation/#%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EC%9C%84%EC%9E%84---event-delegation 요 게시글을 참고하여 이벤트 위임을 구현합니다.
 
 ## Bonus - Hard
 
@@ -40,12 +41,8 @@
   - [ ] 이후 새로고침 시 `localStorage`에서 데이터가 있는 경우 꺼내서 쓰도록 하는 처리를 합니다.
 
 
+---
 
-## TODO 
-- [ ] 임시로 id++ 되도록 해놨으나 추후 아이디 부여 방법 추가 할 것 
-
-
---- 
 ## COMMIT LOG
 
 ### remove button 추가 및 기능 구현 (6/19)
@@ -56,8 +53,23 @@
 - 삭제된 데이터가 todo 추가시 다시 살아나는 문제 수정 
   - main.js에서 input form을 이용해 todo 추가시 todoList.data 사용하도록 변경 
 
-
-
 ### 폴더 구조 변경, isComplete 상태에 따라 표시 형식 변경 (6/19)
 - render html 수정
 - 완료 상태에 따라 표시 형식 변경 
+
+### 데이터 형식 변경, 기본 기능 구현 완료 (6/20)
+- 데이터 형식 Array에서 Object로 변경
+- 기본 기능 구현 완료  
+- TODO : 한 파일에 들어있는 기능들을 컴포넌트화 시켜야 함. 
+
+---
+
+## TODO 
+- [ ] 임시로 id++ 되도록 해놨으나 추후 아이디 부여 방법 추가 할 것 
+- [ ] removeAll, countTodo, inputTodo 컴포넌트화 필요함
+
+# 궁금한 점 
+- 데이터 형식 Array에서 Object로 변경하였습니다. 
+  - 이벤트시 데이터 처리 로직을 간편화하고자 한 변경인데 성능 저하나 문제점이 있는지 궁금합니다. 
+
+
--- mission3/Daesung/src/js/TodoList.js
+++ mission3/Daesung/src/js/TodoList.js
@@ -3,63 +3,59 @@ function TodoList($target, data) {
   this.data = data;
   // console.log(this.data);
 
-  this.addEvents = () => {
+  this.init = () => {
     $target.addEventListener("click", event => {
-      const id = parseInt(event.target.parentNode.id);
-      this.data = this.data.filter(todo => {
-        return todo.id !== id;
-      });
-      console.log(this.data);
+      const targetId = Number(event.target.closest("li").dataset.id);
 
-      this.setState(this.data);
+      if (event.target.className === "removeButton") {
+        this.removeTodo(targetId);
+      } else {
+        // clicked checkbox or text
+        this.toggleTodo(targetId);
+      }
     });
   };
 
-  this.removeTodo = () => {};
+  this.removeTodo = todoId => {
+    delete this.data[todoId];
+    this.setState(this.data);
+  };
 
+  this.removeAllTodo = () => {
+    this.data = {};
+    this.setState(this.data);
+  };
 
+  this.toggleTodo = todoId => {
+    this.data[todoId].isCompleted = !this.data[todoId].isCompleted;
+    this.setState(this.data);
+  };
+
+  this.countTodo = () => {
+    return this.data.length;
+  };
 
   this.setState = data => {
     this.data = data;
     this.render();
   };
 
+  this.render = () => {
+    const REMOVE_BTN_TEXT = "remove";
 
-  this.render = function() {
-    const removeButtonText = "remove"
-
-    const htmlString = this.data.map(function(todo) {
-      return `<li id=${todo.id}>
-                <input type="checkbox" ${todo.isCompleted?"checked":""}>
-                <span>${todo.isCompleted?`(완료) <s>${todo.text}</s>`:todo.text}</span>          
-                <button class="removeButton">${removeButtonText}</button>
+    const htmlString = Object.values(this.data).map(function(todo) {
+      return `<li data-id=${todo.id}>
+                <input type="checkbox" ${todo.isCompleted ? "checked" : ""}>
+                <span>${
+                  todo.isCompleted ? `(완료) <s>${todo.text}</s>` : todo.text
+                }</span>          
+                <button class="removeButton">${REMOVE_BTN_TEXT}</button>
               </li>`;
     });
 
     $target.innerHTML = `<ul>${htmlString.join("")}</ul>`;
   };
 
-
-    // const $todoList = document.createElement("ul");
-
-    // this.data.map(function(todo) {
-    //   const li = document.createElement("li");
-    //   li.id = todo.id;
-
-    //   const delBtn = document.createElement("button");
-    //   const span = document.createElement("span");
-    //   delBtn.innerText = "remove";
-    //   delBtn.classList = "removeBotton";
-    //   span.innerText = li.id + todo.text;
-    //   li.appendChild(span);
-    //   li.appendChild(delBtn);
-
-    //   $todoList.appendChild(li);
-  //   });
-
-  //   $target.innerHTML = $todoList.innerHTML;
-  // };
-
+  this.init();
   this.render();
-  this.addEvents();
 }
