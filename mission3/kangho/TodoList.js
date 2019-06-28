37a99b9bba84295b18d033c1f055ea9b724b3814
이강호 <kangho@bluehole.net>
2019-06-28 23:33:16 +0900

    기본 코드 클래스화

--- mission3/kangho/TodoList.js
+++ mission3/kangho/TodoList.js
@@ -1,19 +1,22 @@
-function TodoList($target, data) {
-  this.$target = $target;
-  this.data = data;
+export default class TodoList {
+  constructor($target, data) {
+    this.$target = $target;
+    this.data = data;  
+  }
 
-  this.setState = function(data) {
+  setState(data) {
     this.data = data;
     this.render();
   };
 
-  this.render = function() {
-    const htmlString = this.data.map(function(todo) {
+  createTemplate() {
+    return this.data.map((todo) => {
       return `<li>${todo.text}</li>`;
     });
+  }
 
-    $target.innerHTML = `<ul>${htmlString.join("")}</ul>`;
+  render() {
+    const htmlString = this.createTemplate();
+    this.$target.innerHTML = `<ul>${htmlString.join("")}</ul>`;
   };
-
-  this.render();
 }
--- mission3/kangho/main.js
+++ mission3/kangho/main.js
@@ -1,38 +1,51 @@
-(function() {
-  // 요 기본 데이터는 자유롭게 작성하세요.
-  const data = [
-    {
-      text: "JavaScript 문서 읽기",
-      isCompleted: false,
-    },
-    {
-      text: "로토에게 질문하기",
-      isCompleted: false,
-    },
-    {
-      text: "Pull Request 만들기",
-      isCompleted: false,
-    },
-    {
-      text: "Slack 접속하기",
-      isCompleted: true,
-    },
-  ];
+import TodoList from './TodoList.js';
 
-  const todoList = new TodoList(document.querySelector("#todo-list"), data);
+const data = [
+  {
+    text: "JavaScript 문서 읽기",
+    isCompleted: false,
+  },
+  {
+    text: "로토에게 질문하기",
+    isCompleted: false,
+  },
+  {
+    text: "Pull Request 만들기",
+    isCompleted: false,
+  },
+  {
+    text: "Slack 접속하기",
+    isCompleted: true,
+  },
+];
 
-  document
-    .querySelector("#add-todo-button")
-    .addEventListener("click", function() {
-      const todoText = document.querySelector("#todo-input").value;
+class App {
+  constructor(todoList) {
+    this.todoList = todoList;
+  }
 
-      if (todoText.length > 0) {
-        data.push({
-          text: todoText,
-          isCompleted: false,
-        });
+  init() {
+    this.todoList.setState(data);
+    document
+      .querySelector("#add-todo-button")
+      .addEventListener("click", () => {
+        const todoText = document.querySelector("#todo-input").value;
 
-        todoList.setState(data);
-      }
-    });
-})();
+        if (todoText.length > 0) {
+          data.push({
+            text: todoText,
+            isCompleted: false,
+          });
+
+          this.todoList.setState(data);
+        }
+      });
+  }
+}
+
+
+const app = new App(
+  new TodoList(document.querySelector("#todo-list")),
+);
+
+app.init();
