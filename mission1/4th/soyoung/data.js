91054f627f296b4524ebdee42752ed8c07f02890
박소영 <soyoungp02@gmail.com>
2020-01-30 16:26:23 +0900

    [Mission 1] [박소영] 필수 구현사항 / 보너스 구현사항 / 질문사항 (#121)
    * [미션 1] 박소영 - TodoList 컴포넌트 작성
    
    * [미션 1] 보너스 구현사항 (#50) 및 data 파일 분리
    
    * [미션 1] 보너스 구현사항 - 다중 컴포넌트 (#57)
    
    * css 추가
    
    * [미션1] 보너스 구현사항 - isCompleted 처리 (#66)
    
    * [미션1] 보너스 구현사항 - isCompleted 처리 (#66) - 유효성 검사 변경
    
    * [미션1] 보너스 구현사항 - setState (#67)
    
    * class에 $element 변수 추가하여 dom 접근 최소화하도록 수정
    
    * 불필요한 class member 변수 삭제
    
    * 기존 div tag에서 semantic한 tag로 변경
    
    * list, v 등의 변수명을 좀 더 의미있는 변수명으로 변경
    
    * hasValidProperty 함수 로직 오류 수정
    
    * 가독성을 위해 render함수 내에서 forEach를 map으로 변경

---
+++ mission1/4th/soyoung/data.js
@@ -0,0 +1,38 @@
+var data = [
+  {
+    text: 'JS 공부하기',
+    isCompleted: true,
+  },
+  {
+    text: 'JS 복습하기',
+    isCompleted: false,
+  },
+]
+var eat_data = [
+  {
+    text: '승록이네 떡볶이',
+    isCompleted: true,
+  },
+  {
+    text: '충무로 함박 스테이크',
+    isCompleted: false,
+  },
+  {
+    text: '등촌 샤브샤브',
+    isCompleted: true,
+  },
+]
+var go_data = [
+  {
+    text: '제주도',
+    isCompleted: true,
+  },
+  {
+    text: '스페인',
+    isCompleted: false,
+  },
+  {
+    text: '캐나다',
+    isCompleted: false,
+  },
+]
