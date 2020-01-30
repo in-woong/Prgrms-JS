9bcd2e4bd75edceb095d6350e51a401dfe515e67
dbwls94 <youjinp6801@gmail.com>
2020-01-30 16:26:59 +0900

    [Mission1] [박유진] TodoList 필수/보너스 구현 (#123)
    * (#49)[Mission1] TodoList 컴포넌트 작성
    
    * (#50)[Mission1] validation 구현
    
    * (#57)[Mission1] 다중 컴포넌트 구현
    
    * (#66)[Mission1] isCompleted 처리 구현
    
    * (#67)[Mission1] setState 구현
    
    * [Mission1] 코드리뷰 반영 - validate 수정, forEach -> map 변경
    
    * [Mission 1] validate 함수명 변경, strike -> del 태그로 대체
    
    * [Mission 1] indent 정리, render 함수 data가 유효할 때만 ul태그 노출

---
+++ mission1/4th/youjin/js/data.js
@@ -0,0 +1,37 @@
+var todo_data = [
+    {
+      text: 'JS 공부하기',
+      isCompleted: true
+    },
+    {
+      text: 'JS 복습하기',
+      isCompleted: false
+    }
+];
+
+var trip_data = [
+    {
+        text: '독일',
+        isCompleted: false
+    },
+    {
+        text: '프랑스',
+        isCompleted: false
+    },
+    {
+        text: '미국',
+        isCompleted: false
+    }
+];
+
+var study_data = [
+    {
+        text: '블로그 정리하기',
+        isCompleted: false
+    },
+    {
+        text: '글 공유하기',
+        isCompleted: true
+    }
+];
+
