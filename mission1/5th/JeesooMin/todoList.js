ddf5b550d749d3b41a7de6c6e774b6b4e4788446
Jeesoo Min <kuongee@gmail.com>
2020-02-08 23:30:13 +0900

    [mission1] 보너스 구현사항 - setState
    Signed-off-by: Jeesoo Min <kuongee@gmail.com>

--- mission1/5th/JeesooMin/todoList.js
+++ mission1/5th/JeesooMin/todoList.js
@@ -1,3 +1,22 @@
+const isParamValid = function(param) {
+  // 데이터가 null / undefined 또는 array가 아닐 경우
+  if (!param || !Array.isArray(param)) {
+    return false
+  }
+
+  // 데이터 내용이 이상할 때
+  var result = true
+  if (param.length > 0) {
+    result = param.every(
+      item =>
+        Object.prototype.toString.call(item) === '[object Object]' &&
+        'text' in item
+    )
+  }
+
+  return result
+}
+
 // 전달 받은 Todo list 데이터를 그리는 컴포넌트
 function TodoList(data, elementId) {
   // new 키워드를 붙이지 않고 함수 실행 시
@@ -5,26 +24,8 @@ function TodoList(data, elementId) {
     throw new Error('new 키워드로 실행되지 않았습니다.')
   }
 
-  var isParamValid = (function(param) {
-    // 데이터가 null / undefined 또는 array가 아닐 경우
-    if (!param || !Array.isArray(param)) {
-      return false
-    }
-
-    // 데이터 내용이 이상할 때
-    var result = true
-    if (param.length > 0) {
-      result = param.every(
-        item =>
-          Object.prototype.toString.call(item) === '[object Object]' &&
-          'text' in item
-      )
-    }
-
-    return result
-  })(data)
-
-  if (!isParamValid) {
+  // 전달받은 데이터가 올바른 데이터인지 확인
+  if (!isParamValid(data)) {
     throw new Error('데이터가 올바르지 않습니다.')
   }
 
@@ -42,5 +43,16 @@ function TodoList(data, elementId) {
     var renderData = this.data.map(item => renderItem(item))
     var container = `<div class="todo-list"> ${renderData.join('')} </div>`
     document.querySelector(`#${this.elementId}`).innerHTML = container
+  }
+
+  this.setState = function(nextData) {
+    if (!isParamValid(nextData)) {
+      throw new Error('데이터가 올바르지 않습니다.')
+    }
+
+    if (this.data !== nextData) {
+      this.data = nextData
+      this.render()
+    }
   }
 }
