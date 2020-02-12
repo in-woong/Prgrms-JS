7214db60bc240678f8e0c517a8c18850a80eea11
limkangho <49905142+limkangho@users.noreply.github.com>
2020-02-12 14:12:10 +0900

    [Mission 1]강호_new 연산자 체크

--- mission1/5th/kangho/render/method4.js
+++ mission1/5th/kangho/render/method4.js
@@ -2,6 +2,9 @@
 //  idTodoList에 직접 html string 렌더링, 왼쪽 값에 오른쪽 값들을 차례로 더하며 누적 후 리턴
 
 function todoList(data) {
+    if (!(this instanceof todoList)) {
+        throw new Error('Error 1: no new keyword');
+    }
     checkData(data);
     this.data = data;
     this.render = function() {
