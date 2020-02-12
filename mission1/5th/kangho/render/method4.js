04b40dc0b68c0b4bcc0598b3a6c814b9e7c7cfbc
limkangho <49905142+limkangho@users.noreply.github.com>
2020-02-12 13:31:39 +0900

    [Mission 1]강호_checkData() 업데이트

--- mission1/5th/kangho/render/method4.js
+++ mission1/5th/kangho/render/method4.js
@@ -2,6 +2,7 @@
 //  idTodoList에 직접 html string 렌더링, 왼쪽 값에 오른쪽 값들을 차례로 더하며 누적 후 리턴
 
 function todoList(data) {
+    checkData(data);
     this.data = data;
     this.render = function() {
         idTodoList.innerHTML = this.data.reduce((htmlString, list) => htmlString + `<div> ${list.text} </div>`, '');
