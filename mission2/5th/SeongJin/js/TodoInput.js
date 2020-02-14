83b09dbd6a0e1a680baf3f5ba82d37b2b322b7d4
JayKim3 <lueky4300@naver.com>
2020-02-14 15:15:40 +0900

    [Mission2] 보너스 구현 input 컴포넌트화
    컴포넌트 별로 파일 나누기

--- mission2/5th/SeongJin/js/TodoInput.js
+++ mission2/5th/SeongJin/js/TodoInput.js
@@ -1,2 +1,15 @@
 // 준비 중
-function TodoInput() {}
+function TodoInput(addTodo) {
+  this.addTodo = addTodo
+  this.$addButton = document.getElementById('addBtn')
+
+  this.$addButton.addEventListener('click', () => {
+    const $input = document.getElementById('newTodo')
+    const newTodo = { text: $input.value, isCompleted: false }
+    this.addTodo(newTodo)
+    $input.value = ''
+    $input.focus()
+  })
+}
+
+export default TodoInput
