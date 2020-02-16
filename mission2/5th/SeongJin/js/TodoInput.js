74c38f6f5f805a42ab6d487b71db4cde58624675
JayKim3 <lueky4300@naver.com>
2020-02-16 18:16:19 +0900

    [Mission2] 커스텀이벤트, 에러처리 수정
    커스텀이벤트 & 에러처리 코드 수정

--- mission2/5th/SeongJin/js/TodoInput.js
+++ mission2/5th/SeongJin/js/TodoInput.js
@@ -1,14 +1,24 @@
-// 준비 중
-function TodoInput(addTodo) {
+import errorCheck from './errorCheck.js'
+
+function TodoInput(addTodo, customEvent) {
   this.addTodo = addTodo
+  this.customEvent = customEvent
+
   this.$addButton = document.getElementById('addBtn')
+  this.$removeAllButton = document.getElementById('removeBtn')
 
   this.$addButton.addEventListener('click', () => {
     const $input = document.getElementById('newTodo')
     const newTodo = { text: $input.value, isCompleted: false }
+    errorCheck.isEmptyText($input.value)
     this.addTodo(newTodo)
     $input.value = ''
     $input.focus()
+  })
+
+  this.$removeAllButton.addEventListener('click', () => {
+    const removeAllEvent = new CustomEvent('removeAll')
+    this.customEvent(removeAllEvent)
   })
 }
 
