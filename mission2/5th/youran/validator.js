cbc91da7dc8495434f171ab9b64b50e3d3447f0a
yourankim <yourankim.tech@gmail.com>
2020-02-17 23:19:00 +0900

    format 적용

--- mission2/5th/youran/validator.js
+++ mission2/5th/youran/validator.js
@@ -1,35 +1,33 @@
-const ERROR_MESSAGE_SELECTOR = '#error-message';
+const ERROR_MESSAGE_SELECTOR = '#error-message'
 
 function isValidSelector(selector) {
-
-  if(!selector || !typeof selector === "string" || selector.length < 1) {
-    throw new Error("selector가 올바르지 않습니다.");
+  if (!selector || !typeof selector === 'string' || selector.length < 1) {
+    throw new Error('selector가 올바르지 않습니다.')
   }
 
-  const $domWithGivenSelector = document.querySelectorAll(`${selector}`);
-  if(!$domWithGivenSelector || $domWithGivenSelector.length !== 1) {
-    throw new Error("target element를 찾을 수 없습니다.");
+  const $domWithGivenSelector = document.querySelectorAll(`${selector}`)
+  if (!$domWithGivenSelector || $domWithGivenSelector.length !== 1) {
+    throw new Error('target element를 찾을 수 없습니다.')
   }
 }
 
 function isValidData(data) {
-
-  if(!Array.isArray(data)) {
-    throw new Error("데이터 형식이 올바르지 않습니다.");
+  if (!Array.isArray(data)) {
+    throw new Error('데이터 형식이 올바르지 않습니다.')
   }
 
-  if(data.length === 0) {
-    return;
-  } 
-
-  const isPlainObject = (item) => toString.call(item) === "[object Object]";
-  const isString = (v) => typeof v === "string";
-  if(!data.some((item) => isPlainObject(item) && isString(item.text))) {
-    throw new Error("사용 가능한 데이터가 아닙니다.");
+  if (data.length === 0) {
+    return
   }
-} 
+
+  const isPlainObject = item => toString.call(item) === '[object Object]'
+  const isString = v => typeof v === 'string'
+  if (!data.some(item => isPlainObject(item) && isString(item.text))) {
+    throw new Error('사용 가능한 데이터가 아닙니다.')
+  }
+}
 
 function showErrorMessage(error, selector = ERROR_MESSAGE_SELECTOR) {
-  document.querySelector(selector).innerHTML = `ERROR! ${error.message}`;
-  console.log(error);
+  document.querySelector(selector).innerHTML = `ERROR! ${error.message}`
+  console.log(error)
 }
