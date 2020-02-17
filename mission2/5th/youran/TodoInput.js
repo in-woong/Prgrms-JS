510527be43eb8a5542e30fe18d33ef9a0d059288
yourankim <yourankim.tech@gmail.com>
2020-02-17 22:06:31 +0900

    리뷰 반영: keycode 상수화 및 조건문 하나로 합치기

--- mission2/5th/youran/TodoInput.js
+++ mission2/5th/youran/TodoInput.js
@@ -1,10 +1,12 @@
 function TodoInput(targetId,insertTodo) {
+
+  const ENTER_KEYCODE = 13;
+
   this.$todoInput = document.querySelector(`#${targetId}`);
   this.insertTodo = insertTodo;
 
   this.$todoInput.addEventListener('keyup', e => {
-    if(e.keyCode !== 13) return;
-    if(!e.target.value) return;
+    if(e.keyCode !== ENTER_KEYCODE || !e.target.value) return;
     this.insertTodo(e.target.value);
     this.$todoInput.value = '';
   });
