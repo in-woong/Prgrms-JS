55f40eaf35090f6c40de1639d118b2c779839dc9
KwonYG <andamirocll@gmail.com>
2020-02-22 19:02:46 +0900

    [Mission3] 권영근 - 리팩토링, console 제거

--- mission3/5th/KwonYoungGeun/js/utils/index.js
+++ mission3/5th/KwonYoungGeun/js/utils/index.js
@@ -5,12 +5,9 @@ const $ = target => {
 const objectToQueryString = parameters => {
   let queries = []
 
-  console.log(parameters)
   for (let key in parameters) {
     queries.push(`${key}=${parameters[key]}`)
   }
-
-  console.log('결과:', queries.join('&'))
 
   return queries.join('&')
 }
