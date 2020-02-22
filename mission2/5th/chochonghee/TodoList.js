6c4a5655d6bd3c613a59df11d037609ccff6c96b
choya <gontry2@naver.com>
2020-02-23 08:01:23 +0900

    [Mission2] chochonghee 필수구현사항

--- mission2/5th/chochonghee/TodoList.js
+++ mission2/5th/chochonghee/TodoList.js
@@ -1,8 +1,3 @@
-const $inputBox = document.querySelector('#js-inputBox')
-const $todoListElement = document.querySelector('#todo-list')
-
-let toDos = []
-
 // 데이터 객체의 속성 체크
 const propertyChk = data => {
     for (let item in data) {
@@ -16,10 +11,11 @@ const propertyChk = data => {
 }
 
 // TodoList 생성자
-function TodoList(data, $divName) {
+let idNum = 0
+
+function TodoList($divName, data) {
     this.data = data
     this.$divName = $divName
-
     try {
         // 객체인지 체크
         if (typeof this !== 'object') {
@@ -44,34 +40,44 @@ function TodoList(data, $divName) {
                     item =>
                     `<div id="todo">
         ${item.isCompleted ? `<del> ${item.text} </del>` : item.text}
-          <span id="mark">X</span></div>`
+          <button id="delBtn" value=${item.id}>X</button></div>`
       )
       .join('')
-    this.data.forEach((item, index) => {
-      toDos.push(item)
-    })
 
-    const $listSelector = document.querySelectorAll('#todo')
-    $listSelector.forEach((item, index) => {
-      item.addEventListener('click', event => {
-        toDos[index].isCompleted = true
-        item.innerHTML = `<div><del>${toDos[index].text}</del> <span id="mark">X</span></div>`
+    // 목록 삭제
+    const $delBtns = document.querySelectorAll('#delBtn')
+    $delBtns.forEach(delbtn => {
+      delbtn.addEventListener('click', e => {
+        this.deleteTodo(event.target.value)
       })
     })
 
-    // 리스트 삭제
-    const $mark = document.querySelectorAll('#mark')
-    $mark.forEach((item, index) => {
-      item.addEventListener('click', event => {
-        console.log(event)
-        $todoListElement.removeChild(event.target.parentNode)
-        // toDos 의 요소를 삭제하는 방법..??
-      })
-    })
+    this.deleteTodo = todoId => {
+      console.log(todoId)
+      const nextData = data.filter(item => item.id !== parseInt(todoId))
+      this.data.splice(parseInt(todoId), 1)
+      todoList.newSetState(nextData)
+    }
+
+    idNum = 0
+    for (var i = 0; i < this.data.length; i++) {
+      this.data[i].id = idNum++
+    }
   }
 
   // 새로운 데이터를 받아서 이어서 화면에 렌더링
   this.setState = nextData => {
+    $divName.innerHTML = ''
+    for (var i = 0; i < nextData.length; i++) {
+      nextData[i].id = idNum++
+      data.push(nextData[i])
+    }
+    this.data = data
+    this.render()
+  }
+
+  this.newSetState = nextData => {
+    $divName.innerHTML = ''
     this.data = nextData
     this.render()
   }
@@ -79,12 +85,4 @@ function TodoList(data, $divName) {
   this.render()
 }
 
-// 할 일 추가하기
-$inputBox.addEventListener('keydown', event => {
-  if (event.key === 'Enter') {
-    event.preventDefault()
-    const todo = [{ text: $inputBox.value, isCompleted: false }]
-    todoList.setState(todo)
-    $inputBox.value = ''
-  }
-})
+export default { TodoList }
--- mission2/5th/chochonghee/index.js
+++ mission2/5th/chochonghee/index.js
@@ -1,21 +1,3 @@
-// 데이터
-const data = [{
-        text: 'JS 공부하기',
-        isCompleted: true,
-    },
-    {
-        text: 'JS 복습하기',
-        isCompleted: true,
-    },
-]
-const dataNext = [{
-        text: '영어소설책 10페이지 읽기',
-        isCompleted: false,
-    },
-    {
-        text: '영어문장 5개 만들기',
-        isCompleted: false,
-    },
-]
-const todoList = new TodoList(data, $todoListElement)
-todoList.setState(dataNext)
+import App from './App'
+
+new App()
