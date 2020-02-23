14aaf050cba75bf32e33e8bd7ac3636f6f235b03
sujin-shin <sujin.shin@nts-corp.com>
2020-02-23 18:36:31 +0900

    [Mission 3] 신수진 (#94) debounce  구현

--- mission3/5th/sujin/js/SearchKeyword.js
+++ mission3/5th/sujin/js/SearchKeyword.js
@@ -1,6 +1,15 @@
 function SearchKeyword ($target, { fetchData }) {
     $target.addEventListener('keyup', e => {
-        fetchData(e.target.value)
+        //디바운스
+        let timer = 0
+        if (timer) {
+            clearTimeout(timer);
+        }
+        
+        timer = setTimeout( () => {
+            fetchData(e.target.value)
+        }, 1000);
+        // fetchData(e.target.value)
     })
 }
 export default SearchKeyword
