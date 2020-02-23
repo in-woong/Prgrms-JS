1983cb711b595fdfbad6ef991d8f0f0c0f3f421c
sujin-shin <sujin.shin@nts-corp.com>
2020-02-23 20:28:48 +0900

    [Mission 3] 신수진 (#102) 검색 히스토리  구현

--- mission3/5th/sujin/js/SearchKeyword.js
+++ mission3/5th/sujin/js/SearchKeyword.js
@@ -1,15 +1,21 @@
-function SearchKeyword ($target, { fetchData }) {
-    $target.addEventListener('keyup', e => {
+function SearchKeyword ($target, { search, addHistory }) {
+    $target.addEventListener('keydown', e => {
+        const ENTER_KEY = 13
         //디바운스
         let timer = 0
+        
         if (timer) {
             clearTimeout(timer);
         }
         
         timer = setTimeout( () => {
-            fetchData(e.target.value)
-        }, 1000);
-        // fetchData(e.target.value)
+            search(e.target.value)
+        }, 300);
+
+        if (e.keyCode === ENTER_KEY) {
+            addHistory(e.target.value)
+            e.target.value=''
+        }
     })
 }
 export default SearchKeyword
