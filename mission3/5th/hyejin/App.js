89b0f652695f852046ef7dfcfec2f3c1fe93a160
decaffeine <decaffeine12@gmail.com>
2020-02-24 01:35:31 +0900

    [mission3] [김혜진] api url 상수로

--- mission3/5th/hyejin/App.js
+++ mission3/5th/hyejin/App.js
@@ -1,4 +1,5 @@
 function App() {
+  const API_URL = 'https://jjalbot.com/api/jjals?text='
   document.getElementById('search-keyword').addEventListener('keydown', e => {
     if (e.key === 'Enter') {
       this.onSearch(e.target.value)
@@ -6,7 +7,7 @@ function App() {
     }
   })
   this.onSearch = function(keyword) {
-    const data = fetch(`https://jjalbot.com/api/jjals?text=${keyword}`)
+    const data = fetch(`${API_URL}${keyword}`)
       .then(response => response.json())
       .then(
         data => new SearchResult(data, document.getElementById('search-result'))
