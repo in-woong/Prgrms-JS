4577d8fb088ba4c57dc6df39144e3c0769a12a22
yourankim <yourankim.tech@gmail.com>
2020-02-23 16:33:19 +0900

    update: debounce처리를 함수로 분리

--- mission3/5th/youran/components/SearchKeyword.js
+++ mission3/5th/youran/components/SearchKeyword.js
@@ -1,14 +1,22 @@
 function SearchKeyword(target, { onKeyUp }) {
   this.$searchKeyword = document.querySelector(target)
-  this.onKeyUp = onKeyUp
+  this.onKeyUp = debounce(event => onKeyUp(event.target.value), 300)
   let timer = null
   this.$searchKeyword.addEventListener('keyup', event => {
+    this.onKeyUp(event)
+  })
+}
+
+const debounce = (targetFunction, waitTime) => {
+  let timer = null
+  return (...args) => {
+    const debouncedFunction = () => {
+      clearTimeout(timer)
+      return targetFunction.apply(this, args)
+    }
     if (timer) {
       clearTimeout(timer)
     }
-    timer = setTimeout(() => {
-      console.log(`keyword:${event.target.value}`)
-      this.onKeyUp(event.target.value)
-    }, 300)
-  })
+    timer = setTimeout(debouncedFunction, waitTime)
+  }
 }
