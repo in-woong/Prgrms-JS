70ab7d3c2d5f1ad314e45ec2bf4fb95b2070f50f
yourankim <yourankim.tech@gmail.com>
2020-02-23 15:51:49 +0900

    3. 보너스 미션 - debounce #94

--- mission3/5th/youran/components/SearchKeyword.js
+++ mission3/5th/youran/components/SearchKeyword.js
@@ -1,7 +1,14 @@
 function SearchKeyword(target, { onKeyUp }) {
   this.$searchKeyword = document.querySelector(target)
   this.onKeyUp = onKeyUp
+  let timer = null
   this.$searchKeyword.addEventListener('keyup', event => {
-    this.onKeyUp(event.target.value)
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      console.log(`keyword:${event.target.value}`)
+      this.onKeyUp(event.target.value)
+    }, 300)
   })
 }
