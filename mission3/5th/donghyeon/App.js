60605e6321bd858a0438eb64f4b77568fed409db
Donghyeon Lim <gmaeodeod@gmail.com>
2020-02-25 00:42:44 +0900

    #93 api.js 로 분리 및 async await로 변경

--- mission3/5th/donghyeon/App.js
+++ mission3/5th/donghyeon/App.js
@@ -1,25 +1,26 @@
 import SearchKeyword from './SearchKeyword.js'
 import SearchResult from './SearchResult.js'
-
-const API_URL = 'https://jjalbot.com/api/jjals?text='
+import { fetchImages } from './api.js'
 
 function App($target) {
     this.$target = $target
     this.data = []
 
-    this.search = keyword => {
-        this.searchUrl = `${API_URL}${keyword}`
-        fetch(this.searchUrl)
-        .then(x => x.json())
-        .then(data => {
-            this.setState(data)
-        })
-    }
+    this.search = async (queryString) => {
+        try {
+            const nextData = await fetchImages(queryString)
+            this.setState(nextData)
+        }
+        catch (error) {
+            console.error(error)
+        }
+    } 
 
     this.searchKeyword = new SearchKeyword('#search-keyword', { search: this.search })
     this.searchResult = new SearchResult('#search-result', this.data)
 
     this.setState = (nextData) => {
+        this.data = nextData
         this.searchResult.setState(nextData)
     }
 }
