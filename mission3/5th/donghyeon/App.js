92c3777fc2c0eb0fd6ca29ce6aad59c082dd9d88
Donghyeon Lim <gmaeodeod@gmail.com>
2020-02-25 01:32:40 +0900

    code formatting

--- mission3/5th/donghyeon/App.js
+++ mission3/5th/donghyeon/App.js
@@ -4,29 +4,33 @@ import SearchHistory from './SearchHistory.js'
 import { fetchImages } from './api.js'
 
 function App($target) {
-    this.$target = $target
-    this.data = []
-    this.searchedKeywords = new Set()
+  this.$target = $target
+  this.data = []
+  this.searchedKeywords = new Set()
 
-    this.search = async (queryString) => {
-        try {
-            const nextData = await fetchImages(queryString)
-            this.data = nextData
-            this.setState(queryString)
-        }
-        catch (error) {
-             console.error(error)
-        }
-    } 
-
-    this.searchKeyword = new SearchKeyword('#search-keyword', { search: this.search })
-    this.searchResult = new SearchResult('#search-result', this.data)
-    this.searchHistory = new SearchHistory('#search-history', this.searchedKeywords)
-
-    this.setState = (queryString) => {
-        this.searchResult.setState(this.data)
-        this.searchHistory.setState(queryString)
+  this.search = async queryString => {
+    try {
+      const nextData = await fetchImages(queryString)
+      this.data = nextData
+      this.setState(queryString)
+    } catch (error) {
+      console.error(error)
     }
+  }
+
+  this.searchKeyword = new SearchKeyword('#search-keyword', {
+    search: this.search,
+  })
+  this.searchResult = new SearchResult('#search-result', this.data)
+  this.searchHistory = new SearchHistory(
+    '#search-history',
+    this.searchedKeywords
+  )
+
+  this.setState = queryString => {
+    this.searchResult.setState(this.data)
+    this.searchHistory.setState(queryString)
+  }
 }
 
 export default App
--- mission3/5th/donghyeon/SearchResult.js
+++ mission3/5th/donghyeon/SearchResult.js
@@ -1,20 +1,20 @@
 function SearchResult(target, data) {
-    this.$target = document.querySelector(target)
-    this.data = data
+  this.$target = document.querySelector(target)
+  this.data = data
 
-    this.render = () => {
-        const htmlString = `${this.data
-            .map(d => `<img src="${d.imageUrl}">`)
-            .join('')}`
-        this.$target.innerHTML = htmlString
-    }
+  this.render = () => {
+    const htmlString = `${this.data
+      .map(d => `<img src="${d.imageUrl}">`)
+      .join('')}`
+    this.$target.innerHTML = htmlString
+  }
 
-    this.setState = (nextData) => {
-        this.data = nextData
-        this.render()
-    }
-
+  this.setState = nextData => {
+    this.data = nextData
     this.render()
+  }
+
+  this.render()
 }
 
 export default SearchResult
