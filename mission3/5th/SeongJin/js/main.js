55200feef5dd758298bc4fcc7cc1337c43fb575e
JayKim3 <lueky4300@naver.com>
2020-02-24 16:03:26 +0900

    [Mission3] 김성진 필수구현사항
    움짤 검색기 만들기

---
+++ mission3/5th/SeongJin/js/main.js
@@ -0,0 +1,24 @@
+import SearchResult from './SearchResult.js'
+import SearchKeyword from './SearchKeyword.js'
+// import { dummyData } from "./dummyData.js";
+
+export default function main() {
+  this.init = () => {
+    this.$resultElement = document.querySelector('#search-result')
+    this.searchResult = new SearchResult('', this.$resultElement)
+    this.searchKeyword = new SearchKeyword(this.search)
+  }
+
+  this.search = async e => {
+    try {
+      const data = await fetch(
+        `https://jjalbot.com/api/jjals?text=${e.target.value}`
+      ) // async  await 적용하는법?
+
+      this.searchResult.setState(data.json())
+    } catch (e) {
+      throw new Error(e)
+    }
+  }
+  this.init()
+}
