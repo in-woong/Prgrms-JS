ee07b81cfbe1539598cae432dfe86b74456ec8d8
boeunseo <sseobe@gmail.com>
2020-02-25 11:56:25 +0800

    [mission3] boeun - try catch 추가

--- mission3/5th/boeun/js/api.js
+++ mission3/5th/boeun/js/api.js
@@ -5,8 +5,13 @@ async function fetchJjalbot(inputValue) {
         return
     }
 
-    const response = await fetch(`https://jjalbot.com/api/jjals?text=${inputValue}`);
-    return response.json();
+    try {
+        const response = await fetch(`https://jjalbot.com/api/jjals?text=${inputValue}`);
+        return response.json();
+    } catch(error) {
+        alert('API 에러입니다.');
+        console.error(error);
+    }
 }
 
 export { fetchJjalbot }
