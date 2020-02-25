e18eefdc20697eeaf6f797e88a4c9f2d25f75b1a
yourankim <yourankim.tech@gmail.com>
2020-02-25 23:20:56 +0900

    refactor: export 선언 위치 변경

--- mission3/5th/youran/constant.js
+++ mission3/5th/youran/constant.js
@@ -1,18 +1,16 @@
-const URL = {
+export const URL = {
   IMAGE_API: 'https://jjalbot.com/api/jjals',
 }
 
-const SELECTOR = {
+export const SELECTOR = {
   SEARCH_KEYWORD: '#search-keyword',
   SEARCH_RESULT: '#search-result',
   SEARCH_HISTORY: '#search-history',
   ERROR_MESSAGE: '#error-message',
 }
 
-const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = {
   TYPE_ERROR: '데이터 형식이 올바르지 않습니다.',
   ELEMENT_ERROR: 'target element를 찾을 수 없습니다.',
   API_ERROR: 'API 요청 실패',
 }
-
-export { URL, SELECTOR, ERROR_MESSAGE }
