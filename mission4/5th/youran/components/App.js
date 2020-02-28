95ff90c7aff05fb02d331bb76207801cb3bc04f4
yourankim <yourankim.tech@gmail.com>
2020-02-28 22:47:44 +0900

    update: App 컴포넌트 기본 코드 작성

--- mission4/5th/youran/components/App.js
+++ mission4/5th/youran/components/App.js
@@ -1,1 +1,40 @@
-function App() {}
+import { $ } from '../utils/'
+import { SELECTOR, USER } from '../utils/constants.js'
+
+function App() {
+  this.init = () => {
+    this.data = []
+    this.todoList = new TodoList(this.data, $(SELECTOR.TODO_LIST), {
+      onToggleTodo: this.onToggleTodo,
+      onRemoveTodo: this.onRemoveTodo,
+    })
+    this.todoInput = new TodoInput($(SELECTOR.TODO_INPUT), {
+      onAddTodo: this.onAddTodo,
+    })
+  }
+
+  this.setState = newTodo => {
+    this.data = [...this.data, newTodo]
+    this.todoList.setState()
+  }
+  
+  this.getAllTodos = async (username) => {
+    const updateData = await fetchAllTodos(username)
+    this.setState(updateData)
+  }
+
+  this.onAddTodo = async (newTodo) => {
+    await postTodo(newTodo)
+    this.getAllTodos(USER.NAME)
+  }
+  
+  this.onRemoveTodo = (todoId) => {
+    await deleteTodo(todoId)
+    this.getAllTodos(USER.NAME)
+  }
+
+  this.onToggleTodo = (todoId) => {
+    await updateTodo(todoId)
+    this.getAllTodos(USER.NAME)
+  }
+}
