03d47ae124d285dfc91408fb94a7e0edc750ae36
SeongJin <45479309+JayKim3@users.noreply.github.com>
2020-03-02 11:16:57 +0900

    Delete errorComponent.js

--- mission3/5th/SeongJin/js/errorComponent.js
+++
@@ -1,9 +0,0 @@
-export default function errorComponent(error) {
-  this.$errorElement = document.createElement('div')
-  const $app = document.querySelector('#app')
-  $app.appendChild(this.$errorElement)
-
-  this.render = () => {
-    this.$errorElement.innerHTML = `<strong>${error}</strong>`
-  }
-}
