78a16f3f44e12746813fa7750736c8da8ba320e6
decaffeine <decaffeine12@gmail.com>
2020-03-02 23:36:37 +0900

    [mission3] [김혜진] 코드리뷰 반영 및 import, export로 변경

--- mission3/5th/hyejin/App.js
+++ mission3/5th/hyejin/App.js
@@ -1,16 +1,27 @@
-function App() {
-  const API_URL = 'https://jjalbot.com/api/jjals?text='
+import { API_URL, ENTER_KEY } from './Constants.js'
+import { SearchResult } from './SearchResult.js'
+
+export default function App() {
   document.getElementById('search-keyword').addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
+    if (e.key === ENTER_KEY) {
       this.onSearch(e.target.value)
       e.target.value = ''
     }
   })
+
+  this.init = function() {
+    this.searchResult = new SearchResult([], document.getElementById('search-result'))
+  }
+
   this.onSearch = function(keyword) {
     const data = fetch(`${API_URL}${keyword}`)
       .then(response => response.json())
       .then(
-        data => new SearchResult(data, document.getElementById('search-result'))
-      )
+        data => this.setState(data)
+      )}
+
+  this.setState = function(nextData) {
+    this.searchResult.setState(nextData)
   }
+  this.init()
 }
--- mission3/5th/hyejin/SearchResult.js
+++ mission3/5th/hyejin/SearchResult.js
@@ -2,16 +2,16 @@ function SearchResult(data, $target) {
   this.data = data
   this.$target = $target
 
-  this.setstate = function(nextData) {
+  this.render = function() {
+    $target.innerHTML = this.data.map(item => `<img src="${item.imageUrl}">`).join('')
+  }
+
+  this.setState = function(nextData) {
     this.data = nextData
     this.render()
   }
 
-  this.render = function() {
-    $target.innerHTML = `${this.data
-      .map(d => `<img src="${d.imageUrl}">`)
-      .join('')}`
-  }
-
   this.render()
 }
+
+export { SearchResult }
