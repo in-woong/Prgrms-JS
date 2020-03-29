6e262e0345606a5ef27fa1e831964721ff6b1e91
So0 <syngshm@gmail.com>
2020-03-29 21:28:11 +0900

    [Mission4] 소영 - 보너스 구현사항 API가 느린 경우의 인터랙션 처리 #144

--- mission4/5th/soyoung/App.js
+++ mission4/5th/soyoung/App.js
@@ -1,54 +1,67 @@
 function App($target) {
   this.$target = $target
   this.username = 'soyoung'
-  this.setUserName = function(currentUserName) {
-    this.username = currentUserName
-  }
-  this.render = async function() {
-    this.$target.innerHTML = `
-    <div style="width: 50%; border-right:1px solid gray; float:left;">
-      <div id="user-list"></div>
-    </div>
-    <div style="float:left; padding:10px;">
-      <div id="todo-input"></div>
-      <div id="todo-list"></div>
-    </div>`
-
-    this.data = await getItem(this.username)
-    this.userListData = await getUserList()
-    this.userList = new UserList({
-      $target: document.querySelector('#user-list'),
-      username: this.username,
-      data: this.userListData,
-      onClick: async username => {
-        this.setUserName(username)
-        const updatedData = await getItem(this.username)
-        this.todoList.setState(updatedData)
-        this.userList.setUser(this.username)
-      },
+  ;(this.isLoading = false),
+    (this.setUserName = currentUserName => {
+      this.username = currentUserName
+    }),
+    (this.render = async function() {
+      this.$target.innerHTML = `
+        <div style="width: 50%; border-right:1px solid gray; float:left;">
+          <div id="user-list"></div>
+        </div>
+        <div style="float:left; padding:10px;">
+          <div id="todo-input"></div>
+          <div id="todo-list"></div>
+        </div>`
+      this.data = await getItem(this.username)
+      this.userListData = await getUserList()
+      loaderStop()
+      this.userList = new UserList({
+        $target: document.querySelector('#user-list'),
+        username: this.username,
+        data: this.userListData,
+        onClick: async username => {
+          this.setUserName(username)
+          const updatedData = await getItem(this.username)
+          loaderStop()
+          this.todoList.setState(updatedData)
+          this.userList.setUser(this.username)
+        },
+      })
+      this.todoInput = new TodoInput({
+        $target: document.querySelector('#todo-input'),
+        onAdd: async todoText => {
+          await addItem(this.username, todoText)
+          const updatedData = await getItem(this.username)
+          loaderStop()
+          this.todoList.setState(updatedData)
+        },
+      })
+      this.todoList = new TodoList({
+        $target: document.querySelector('#todo-list'),
+        data: this.data,
+        onClick: async id => {
+          await editItem(this.username, id)
+          const updatedData = await getItem(this.username)
+          loaderStop()
+          this.todoList.setState(updatedData)
+        },
+        onRemove: async id => {
+          await removeItem(this.username, id)
+          const updatedData = await getItem(this.username)
+          loaderStop()
+          this.todoList.setState(updatedData)
+        },
+      })
     })
-    this.todoInput = new TodoInput({
-      $target: document.querySelector('#todo-input'),
-      onAdd: async todoText => {
-        await addItem(this.username, todoText)
-        const updatedData = await getItem(this.username)
-        this.todoList.setState(updatedData)
-      },
-    })
-    this.todoList = new TodoList({
-      $target: document.querySelector('#todo-list'),
-      data: this.data,
-      onClick: async id => {
-        await editItem(this.username, id)
-        const updatedData = await getItem(this.username)
-        this.todoList.setState(updatedData)
-      },
-      onRemove: async id => {
-        await removeItem(this.username, id)
-        const updatedData = await getItem(this.username)
-        this.todoList.setState(updatedData)
-      },
-    })
-  }
   this.render()
+}
+function loaderStart() {
+  document.getElementById('loader').style.display = 'block'
+  document.getElementById('app').style.display = 'none'
+}
+function loaderStop() {
+  document.getElementById('loader').style.display = 'none'
+  document.getElementById('app').style.display = 'block'
 }
--- mission4/5th/soyoung/api.js
+++ mission4/5th/soyoung/api.js
@@ -1,10 +1,12 @@
 async function getItem(username) {
-  const res = await fetch(`https://todo-api.roto.codes/${username}`)
+  loaderStart()
+  const res = await fetch(`https://todo-api.roto.codes/${username}?delay=1`)
   return await res.json()
 }
 
 async function addItem(username, todoText) {
-  fetch(`https://todo-api.roto.codes/${username}`, {
+  loaderStart()
+  return fetch(`https://todo-api.roto.codes/${username}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -16,18 +18,21 @@ async function addItem(username, todoText) {
 }
 
 async function editItem(username, id) {
+  loaderStart()
   return fetch(`https://todo-api.roto.codes/${username}/${id}/toggle`, {
     method: 'PUT',
   })
 }
 
 async function removeItem(username, id) {
+  loaderStart()
   return fetch(`https://todo-api.roto.codes/${username}/${id}`, {
     method: 'DELETE',
   })
 }
 
 async function getUserList() {
+  loaderStart()
   const res = await fetch(`https://todo-api.roto.codes/users`)
   return await res.json()
 }
