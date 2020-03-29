7a066ddd3c5c051f2e54467ff1e3fa338d89c3eb
So0 <syngshm@gmail.com>
2020-03-29 20:42:50 +0900

    [Mission4] 소영 - 보너스 구현사항 User Component (#107)

--- mission4/5th/soyoung/App.js
+++ mission4/5th/soyoung/App.js
@@ -1,12 +1,32 @@
 function App($target) {
   this.$target = $target
   this.username = 'soyoung'
+  this.setUserName = function(currentUserName) {
+    this.username = currentUserName
+  }
   this.render = async function() {
     this.$target.innerHTML = `
-    <div id="todo-input"></div>
-    <div id="todo-list"></div>`
+    <div style="width: 50%; border-right:1px solid gray; float:left;">
+      <div id="user-list"></div>
+    </div>
+    <div style="float:left; padding:10px;">
+      <div id="todo-input"></div>
+      <div id="todo-list"></div>
+    </div>`
 
     this.data = await getItem(this.username)
+    this.userListData = await getUserList()
+    this.userList = new UserList({
+      $target: document.querySelector('#user-list'),
+      username: this.username,
+      data: this.userListData,
+      onClick: async username => {
+        this.setUserName(username)
+        const updatedData = await getItem(this.username)
+        this.todoList.setState(updatedData)
+        this.userList.setUser(this.username)
+      },
+    })
     this.todoInput = new TodoInput({
       $target: document.querySelector('#todo-input'),
       onAdd: async todoText => {
--- mission4/5th/soyoung/api.js
+++ mission4/5th/soyoung/api.js
@@ -1,10 +1,10 @@
 async function getItem(username) {
-  const res = await fetch(`http://todo-api.roto.codes/${username}`)
+  const res = await fetch(`https://todo-api.roto.codes/${username}`)
   return await res.json()
 }
 
 async function addItem(username, todoText) {
-  fetch(`http://todo-api.roto.codes/${username}`, {
+  fetch(`https://todo-api.roto.codes/${username}`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -16,13 +16,18 @@ async function addItem(username, todoText) {
 }
 
 async function editItem(username, id) {
-  return fetch(`http://todo-api.roto.codes/${username}/${id}/toggle`, {
+  return fetch(`https://todo-api.roto.codes/${username}/${id}/toggle`, {
     method: 'PUT',
   })
 }
 
 async function removeItem(username, id) {
-  return fetch(`http://todo-api.roto.codes/${username}/${id}`, {
+  return fetch(`https://todo-api.roto.codes/${username}/${id}`, {
     method: 'DELETE',
   })
+}
+
+async function getUserList() {
+  const res = await fetch(`https://todo-api.roto.codes/users`)
+  return await res.json()
 }
