cec7edbc70b094d38a6c57d93595340a9821cbde
oceanide <oceanide@gmail.com>
2020-05-01 23:56:10 +0900

    [Mission1] [oceanide] 보너스 구현사항 #50

--- mission1/6th/oceanide/index.js
+++ mission1/6th/oceanide/index.js
@@ -7,7 +7,28 @@ let data = [
   },
 ]
 
+const getType = (value) => Object.prototype.toString.call(value).slice(8, -1)
+const isNull = (value) => getType(value) === 'Null'
+const isUndefined = (value) => getType(value) === 'Undefined'
+const isString = (value) => getType(value) === 'String'
+const isArray = (value) => getType(value) === 'Array'
+const isObject = (value) => getType(value) === 'Object'
+
+const isValidData = function (value) {
+  if (isNull(value) || isUndefined(value)) return false
+  if (!(isArray(value) && value.length > 0)) return false
+
+  return value.every((element) => {
+    return isObject(element) && 'text' in element && isString(element.text)
+  })
+}
+
 function TodoList(data) {
+  if (!(this instanceof arguments.callee))
+    throw new Error('Function must be called with new')
+
+  if (!isValidData(data)) throw new Error('Invalid data')
+
   this.todos = data
   this.render = function () {
     this.todos.forEach((todo) => {
