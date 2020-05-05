3bcd1f91ea503f0bd4005759b6d1d2a532ca11f1
ksy <ksy8230@hanmail.net>
2020-05-05 09:41:32 +0900

    feat: 타입스크립트를 이용한 mission1

---
+++ mission1/6th/kimsuyoung/typescript.js
@@ -0,0 +1,70 @@
+var data = [
+    {
+        text: 'JS 공부하기',
+        isCompleted: true
+    },
+    {
+        text: 'JS 복습하기',
+        isCompleted: false
+    },
+];
+var data2 = [
+    {
+        text: '',
+        isCompleted: true
+    },
+    {
+        text: 'TS 복습하기',
+        isCompleted: false
+    },
+];
+var dataChange = [
+    {
+        text: 'TS 공부하기',
+        isCompleted: true
+    },
+    {
+        text: 'TS 복습하기',
+        isCompleted: true
+    },
+    {
+        text: '출근 준비하기',
+        isCompleted: false
+    },
+];
+var createTodoHTMLString = function (data) {
+    return data.isCompleted
+        ? "<div><s>" + data.text + "</s></div>"
+        : "<div>" + data.text + "</div>";
+};
+function TodoList(data, id) {
+    var _this = this;
+    this.data = data;
+    this.validate = function (data) {
+        if (data === null || data === undefined)
+            throw new Error('null이나 undefined면 안 됩니다.');
+        if (!Array.isArray(_this.data))
+            throw new Error('배열이 아닌 값이 오면 안 됩니다.');
+        if (!(_this instanceof TodoList))
+            throw new Error('new를 사용하세요.');
+    };
+    this.render = function () {
+        _this.validate(data);
+        var html = data.map(function (value) { return createTodoHTMLString(value); }).join('');
+        return (document.querySelector("#" + id).innerHTML = html);
+    };
+    this.setState = function (nextData) {
+        data = nextData;
+        _this.render();
+    };
+}
+try {
+    var todoList = new TodoList(data, 'todo-list-1');
+    var todoList2 = new TodoList(data2, 'todo-list-2');
+    todoList.render();
+    todoList2.render();
+    todoList2.setState(dataChange);
+}
+catch (error) {
+    console.error(error);
+}
---
+++ mission1/6th/kimsuyoung/typescript.ts
@@ -0,0 +1,86 @@
+const data = [
+  {
+    text: 'JS 공부하기',
+    isCompleted: true,
+  },
+  {
+    text: 'JS 복습하기',
+    isCompleted: false,
+  },
+]
+
+const data2 = [
+  {
+    text: '',
+    isCompleted: true,
+  },
+  {
+    text: 'TS 복습하기',
+    isCompleted: false,
+  },
+]
+
+const dataChange = [
+  {
+    text: 'TS 공부하기',
+    isCompleted: true,
+  },
+  {
+    text: 'TS 복습하기',
+    isCompleted: true,
+  },
+  {
+    text: '출근 준비하기',
+    isCompleted: false,
+  },
+]
+
+interface DataProps {
+  text: string
+  isCompleted: boolean
+}
+
+type TodoListProps = {
+  // TodoList 매개변수 타입을 한번에 지정해주는 법?
+  data: DataProps[]
+  id: string
+}
+
+const createTodoHTMLString = (data) => {
+  return data.isCompleted
+    ? `<div><s>${data.text}</s></div>`
+    : `<div>${data.text}</div>`
+}
+
+function TodoList(data: DataProps[], id: string) {
+  this.data = data
+
+  this.validate = (data) => {
+    if (data === null || data === undefined)
+      throw new Error('null이나 undefined면 안 됩니다.')
+    if (!Array.isArray(this.data))
+      throw new Error('배열이 아닌 값이 오면 안 됩니다.')
+    if (!(this instanceof TodoList)) throw new Error('new를 사용하세요.')
+  }
+
+  this.render = () => {
+    this.validate(data)
+    const html = data.map((value) => createTodoHTMLString(value)).join('')
+    return (document.querySelector(`#${id}`).innerHTML = html)
+  }
+
+  this.setState = (nextData) => {
+    data = nextData
+    this.render()
+  }
+}
+
+try {
+  const todoList = new TodoList(data, 'todo-list-1')
+  const todoList2 = new TodoList(data2, 'todo-list-2')
+  todoList.render()
+  todoList2.render()
+  todoList2.setState(dataChange)
+} catch (error) {
+  console.error(error)
+}
