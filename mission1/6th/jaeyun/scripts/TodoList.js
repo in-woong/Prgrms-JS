eb0d5240199a6ba6b5ddbfd80de4ee84176300b4
ag502 <ag502@naver.com>
2020-05-06 00:52:17 +0900

    error message 추가

--- mission1/6th/jaeyun/scripts/TodoList.js
+++ mission1/6th/jaeyun/scripts/TodoList.js
@@ -1,6 +1,29 @@
 class TodoList {
   constructor(data) {
+    this.checkData(data)
     this.todoList = data
+  }
+
+  checkData(data) {
+    if (!data) {
+      throw new Error('Invalid Data')
+    } else if (!Array.isArray(data)) {
+      throw new Error('Data must be Array')
+    } else if (!this.checkDataElem(data)) {
+      throw new Error('Data Element has Error')
+    }
+  }
+
+  checkDataElem(data) {
+    let isValidData = true
+    data.forEach((elem) => {
+      const { text } = elem
+      if (typeof text !== 'string') {
+        isValidData = false
+        return
+      }
+    })
+    return isValidData
   }
 
   addListToTag() {
