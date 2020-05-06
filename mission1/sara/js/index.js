2f8a26d997af4322ca76c16fe2aa2db22d4297dd
DESKTOP-GPG4R2K\SUNAH <tjsdkz3@naver.com>
2020-05-06 18:35:53 +0900

    [mission1] 보너스 구현사항 - setState

--- mission1/sara/js/index.js
+++ mission1/sara/js/index.js
@@ -20,40 +20,60 @@ var todoData = [
     ],
     monthData = [
         {
-            text: '프로그래머스 이수',
+            text: '프로그래머스 참여',
             isCompleted: false
         }
     ];
 
 class TodoList {
     constructor(data, selector) {
-        this.dataValidator(data, selector);
+        this.dataValidator(data);
+        this.selectorValidator(selector);
         this.data = data;
         this.element = document.querySelector(selector);
     }
 
-    dataValidator = function(data, selector) {
+    dataValidator = function(data) {
         if(data === null || data === undefined) {
             throw new Error('data 값을 전달해주세요.');
         } else if(!Array.isArray(data)) {
             throw new Error('data 는 배열형태입니다.');
         }
+    }
 
+    selectorValidator = function(selector) {
         if(document.querySelector(selector) === null) {
             throw new Error('해당 element 가 존재하지 않습니다.');
         }
     }
 
-    render = function() {
+    getInnerHTML = function() {
         const innerText = this.data.map((value) => {
-            if(value.isCompleted) {
-               return `<li><s>${value.text}</s></li>`
-            } else {
+            if(value.isCompleted)
+                return `<li><s>${value.text}</s></li>`
+            else
                 return `<li>${value.text}</li>`;
-            }
         }).join('');
 
-        this.element.innerHTML = `<ul>${innerText}</ul>`;
+        return innerText;
+    }
+
+    dataChangeText = function(array) {
+        return array.map(value => JSON.stringify(value)).join('');
+    }
+
+    setState = function(nextData) {
+        this.dataValidator(nextData);
+
+        if(this.dataChangeText(nextData) === this.dataChangeText(this.data))
+            return;
+
+        this.data = nextData;
+        this.element.innerHTML = `<ul>${this.getInnerHTML()}</ul>`;
+    }
+
+    render = function() {
+        this.element.innerHTML = `<ul>${this.getInnerHTML()}</ul>`;
     }
 }
 
@@ -64,3 +84,4 @@ var monthList = new TodoList(monthData, '#month-list');
 todoList.render();
 weekendList.render();
 monthList.render();
+monthList.setState([{text: '프로그래머스 이수'}]);
