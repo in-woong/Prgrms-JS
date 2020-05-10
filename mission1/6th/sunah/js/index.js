ea5e30142ad9564d0ed60808c3cb874ba9c842f5
DESKTOP-GPG4R2K\SUNAH <tjsdkz3@naver.com>
2020-05-10 13:55:53 +0900

    [mission1] TodoList 코드리뷰 반영

--- mission1/6th/sunah/js/index.js
+++ mission1/6th/sunah/js/index.js
@@ -27,53 +27,61 @@ var todoData = [
 
 class TodoList {
     constructor(data, selector) {
-        this.dataValidator(data);
-        this.selectorValidator(selector);
+        this.validateData(data);
+        this.validateSelector(selector);
         this.data = data;
         this.element = document.querySelector(selector);
     }
 
-    dataValidator = function(data) {
+    validateData = function(data) {
         if(data === null || data === undefined) {
             throw new Error('data 값을 전달해주세요.');
         } else if(!Array.isArray(data)) {
             throw new Error('data 는 배열형태입니다.');
+        } else {
+            data.forEach(item => {
+                if(!item.hasOwnProperty('text')) {
+                    throw new Error('data는 text 속성값을 가지고 있어야합니다.');
+                } else if (typeof item.text !== 'string') {
+                    throw new Error('data는 text 속성은 string 타입입니다.');
+                }
+
+                if (typeof item.isCompleted !== 'boolean')
+                    throw new Error('data는 isCompleted 속성은 boolean 타입입니다.');
+            });
         }
     }
 
-    selectorValidator = function(selector) {
+    validateSelector = function(selector) {
         if(document.querySelector(selector) === null) {
             throw new Error('해당 element 가 존재하지 않습니다.');
         }
     }
 
-    getInnerHTML = function() {
+    createInnerHTML = function() {
         const innerText = this.data.map((value) => {
-            if(value.isCompleted)
-                return `<li><s>${value.text}</s></li>`
-            else
-                return `<li>${value.text}</li>`;
+            return value.isCompleted ? `<li><s>${value.text}</s></li>` : `<li>${value.text}</li>`;
         }).join('');
 
         return innerText;
     }
 
-    dataChangeText = function(array) {
+    convertDataToString = function(array) {
         return array.map(value => JSON.stringify(value)).join('');
     }
 
     setState = function(nextData) {
-        this.dataValidator(nextData);
+        this.validateData(nextData);
 
-        if(this.dataChangeText(nextData) === this.dataChangeText(this.data))
+        if(this.convertDataToString(nextData) === this.convertDataToString(this.data))
             return;
 
         this.data = nextData;
-        this.element.innerHTML = `<ul>${this.getInnerHTML()}</ul>`;
+        this.render();
     }
 
     render = function() {
-        this.element.innerHTML = `<ul>${this.getInnerHTML()}</ul>`;
+        this.element.innerHTML = `<ul>${this.createInnerHTML()}</ul>`;
     }
 }
 
