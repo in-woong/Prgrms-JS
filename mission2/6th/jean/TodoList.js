342052ea2aae33fd75a0f8bfbeca87d8c3ff1fe9
jeanP-tech <jeanP.tech@gmail.com>
2020-05-12 02:16:14 +0900

    Add main mission

---
+++ mission2/6th/jean/TodoList.js
@@ -0,0 +1,24 @@
+function TodoList($target, data) {
+  this.$target = $target
+  this.data = data
+
+  this.render = function() {
+    this.$target.innerHTML =
+    `<ul>
+      ${this.data.map(({ text, isCompleted }, index) =>
+        `<li data-index="${index}">
+          ${isCompleted ? `<s>${text}</s>` : text}
+          <button class="delete-btn">X</button>
+        </li>`
+      ).join('')}
+    </ul>`
+  }
+
+  this.setState = function(nextData) {
+    this.data = nextData
+    this.render()
+  }
+
+  this.render()
+
+}
