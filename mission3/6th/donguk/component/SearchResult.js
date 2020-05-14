b37f7599165b1471104a74112ea96104cc1b3c5b
donguk hong <mbxd1@naver.com>
2020-05-15 00:04:43 +0900

    mission3 > SearchResult > 로딩바 구현

--- mission3/6th/donguk/component/SearchResult.js
+++ mission3/6th/donguk/component/SearchResult.js
@@ -1,12 +1,26 @@
 import Component from './Component.js'
 import { checkSelector } from '../utils/validation.js'
 
-const createHTMLElement = (image) => {
-  const { url, title } = image
-  return `<div class="image-item">
-            <img src=${url} alt=""/>
-            <div class="item-title">${title}</div>
-         </div>`
+const createHTMLElement = (element) => {
+  const { url, title } = element
+  const $item = document.createElement('div')
+  $item.setAttribute('class', 'image-item')
+  const $loadingDiv = document.createElement('div')
+  $loadingDiv.className = 'loading'
+  $loadingDiv.textContent = 'loading...'
+  const $itemTitle = document.createElement('div')
+  $itemTitle.className = 'item-title'
+  $itemTitle.textContent = title
+  const image = new Image()
+  image.src = url
+  image.alt = ''
+  image.onload = () => {
+    $item.removeChild($loadingDiv)
+  }
+  $item.appendChild($loadingDiv)
+  $item.appendChild(image)
+  $item.appendChild($itemTitle)
+  return $item
 }
 
 export default class SearchResult extends Component {
@@ -16,14 +30,38 @@ export default class SearchResult extends Component {
     checkSelector(selector)
     this.$target = document.querySelector(selector)
     this.images = images
+    this.componentMount()
     this.render()
   }
 
   render() {
-    this.$target.innerHTML = this.images.map((image) => createHTMLElement(image)).join('')
+    // 해봐야 하는 것 >검색 안했을 때, 로딩 중 + lazy loading?, 검색 후
+    if (this.isUseComponent) {
+      const startIndex = (this.currentPage - 1) * (this.size)
+      const endIndex = startIndex + this.size
+      const currentRenderImages = this.images.slice(startIndex, endIndex)
+      if (startIndex === 0) {
+        this.$target.innerHTML = `<h2>총 ${this.images.length}개 짤 검색완료.</h2>` // 처음에만,
+      }
+      currentRenderImages.forEach((image) => {
+        this.$target.appendChild(createHTMLElement(image))
+      })
+    } else { // initial render
+      this.$target.innerHTML = '<div>검색어를 기다리고 있어요.</div>'
+    }
   }
 
-  setState(nextData){
+  componentMount() {
+    this.currentPage = 1 // scroll에 따른 lazy loading
+    this.size = 10
+    this.isUseComponent = false // 검색 안했을 때, 했을 때 flag개
+    window.addEventListener('scroll', () => {
+
+    })
+  }
+
+  setState(nextData) {
+    this.isUseComponent = true
     this.images = nextData
     this.render()
   }
