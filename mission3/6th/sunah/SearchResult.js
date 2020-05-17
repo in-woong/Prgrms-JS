cc2ad3064456017ae88b522d991a5d81731d556e
DESKTOP-GPG4R2K\SUNAH <tjsdkz3@naver.com>
2020-05-17 14:54:15 +0900

    [mission3] 움짤 검색기

---
+++ mission3/6th/sunah/SearchResult.js
@@ -0,0 +1,25 @@
+function SearchResult(data, selector) {
+    this.data = data;
+    this.$target = document.querySelector(selector);
+
+    this.createInnerHTML = function() {
+        const innerText = this.data.map(({title, tags, imageUrl}, index) => {
+            return `<li class="item">
+                        <h3>${title}</h3>
+                        <img src="${imageUrl}" >
+                    </li>`;
+        }).join('');
+
+        return innerText;
+    }
+
+    this.setState = function(nextData) {
+        console.log(nextData);
+        this.data = nextData;
+        this.render();
+    };
+
+    this.render = function() {
+        this.$target.innerHTML = `<ul>${this.createInnerHTML()}</ul>`;
+    }
+}
