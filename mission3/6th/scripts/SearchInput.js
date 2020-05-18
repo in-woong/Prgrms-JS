06778db9b83abbedeb811aa03a15ee2d72de8668
ag502 <ag502@naver.com>
2020-05-19 06:37:14 +0900

    event debounce

--- mission3/6th/scripts/SearchInput.js
+++ mission3/6th/scripts/SearchInput.js
@@ -1,9 +1,15 @@
 class SearchInput {
   constructor(searchHandler) {
+    let timer = null
     const $searchInput = document.querySelector('#search-keyword')
     $searchInput.addEventListener('keyup', (e) => {
-      searchHandler(e.target.value)
-      console.log(e.target.value)
+      if (timer) {
+        clearTimeout(timer)
+      }
+      timer = setTimeout(() => {
+        searchHandler(e.target.value)
+        console.log(e.target.value)
+      }, 250)
     })
   }
 }
