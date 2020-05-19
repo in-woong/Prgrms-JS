bcaaf485b84b2aa40ed134c92f232ab00e403c35
wkimdev <wkimdev@gmail.com>
2020-05-19 20:29:47 +0900

    [ mission 2 ]  필수구현사항 수정 및 보너스 구현사항(#81, #90, #91)

--- mission2/6th/updownup/src/components/App.js
+++ mission2/6th/updownup/src/components/App.js
@@ -1,12 +1,30 @@
 import TodoList from './TodoList.js'
 import TodoInput from './TodoInput.js'
 import TodoCount from './TodoCount.js'
+import { fetchTodoList, postTodo } from '../api/index.js'
 
 export default function App () {
 
-  let data = []
+  let data = fetchTodoList() || []
   const $target = document.querySelector('#App')
-  const inputElement = document.getElementById('todo-list-input')
+  const inputElement = document.querySelector('#todo-list-input')
+  const removeAllElement = document.querySelector('#todo-remove-all')
+
+  // 커스텀 이벤트 등록
+  $target.addEventListener('removeAll', () => {
+    const isRemoveAll = confirm('todo리스트를 전부 삭제 하시겠습니까?')
+    if (isRemoveAll) {
+      updateTodoList([])
+    }
+  })
+
+  removeAllElement.addEventListener('click', (event) => {
+    event.target.dispatchEvent(removeAllEvent)
+  })
+
+  const removeAllEvent = new CustomEvent('removeAll', {
+    bubbles: true,
+  });
 
   // todo 입력
   const addTodoList = (todoData) => {
@@ -18,12 +36,14 @@ export default function App () {
   // todo 삭제 혹은 완료 처리
   const updateTodoList = (todoData) => {
     data = todoData
+    postTodo(data)
     this.todoList.setState(data)
     this.TodoCount.setState(data)
   }
 
   this.setState = (nextData) => {
     data.push(nextData)
+    postTodo(data)
     this.todoList.setState(data)
     this.TodoCount.setState(data)
   }
--- mission2/6th/updownup/src/components/TodoList.js
+++ mission2/6th/updownup/src/components/TodoList.js
@@ -17,32 +17,38 @@ function TodoList(props) {
   }
   
   this.$target.addEventListener('click', (event) => {
+    const isDeleteBtn = event.target.className === 'delete-btn'
+    const isDeleteNode = event.target.nodeName === 'S';
     
-    if (event.target.className === 'delete-btn') {  // 삭제 처리 - 클릭한 인자의 id요소 1개 삭제
+    if (isDeleteBtn) {  // 삭제 처리 - 클릭한 인자의 id요소 1개 삭제
       data.splice(event.target.parentNode.getAttribute('data-id'), 1)
-    } else if (event.target.dataset.id) { // 완료
-      const todoIndex = event.target.dataset.id
+    } else { // 완료
+      const todoIndex = isDeleteNode ?
+        event.target.parentNode.getAttribute('data-id') : 
+        event.target.getAttribute('data-id')
       data[todoIndex].isCompleted = !data[todoIndex].isCompleted
     }
     this.updateTodoList(data)
   })
 
-  this.setState = (nextData) => {
-    data = nextData
-    this.render()
-  }
-
   this.render = () => {
     this.data = this.checkParams(data)
 
     this.$target.innerHTML = `<ul>${data.map(({text, isCompleted}, index) => 
-          `<li data-id=${index}>
+          `<li data-id=${index} class="pointer">
             ${isCompleted ? '<s>' + text + '</s>' : text }
             <button class="delete-btn">삭제</button>
             </li>`
           ).join('')}
         </ul>`
   }
+
+  // 새로고침 후 데이터를 불러오지 않는 이슈
+  this.setState = (nextData) => {
+    data = nextData
+    this.render()
+  }
+
 }
 
 export default TodoList
