af41e10adfc774fd35a75546d3dbb06a957a8d4a
ksy <ksy8230@hanmail.net>
2020-05-23 21:25:41 +0900

    feat: 할 일 추가하기

--- mission4/6th/kimsuyoung/api/index.js
+++ mission4/6th/kimsuyoung/api/index.js
@@ -12,9 +12,26 @@ export const getUsers = async () => {
 
 export const getTodoLists = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/roto`)
+    const response = await fetch(`${BASE_URL}/suyoung`)
     const result = await response.json()
     return result
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export const postTodoList = async (text) => {
+  console.log(text)
+  try {
+    await fetch(`${BASE_URL}/suyoung`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: text,
+      }),
+    })
   } catch (error) {
     console.error(error)
   }
--- mission4/6th/kimsuyoung/components/App.js
+++ mission4/6th/kimsuyoung/components/App.js
@@ -1,6 +1,7 @@
 import UserList from './UserList.js'
 import TodoList from './TodoList.js'
-import { getUsers, getTodoLists } from '../api/index.js'
+import TodoInput from './TodoInput.js'
+import { getUsers, getTodoLists, postTodoList } from '../api/index.js'
 
 export default function App() {
   this.data = {
@@ -18,12 +19,21 @@ export default function App() {
     data: this.data.todoList,
   })
 
+  this.TodoInput = new TodoInput({
+    $todoInput: document.querySelector('#todo-input'),
+    onInput: async (text) => {
+      await postTodoList(text)
+      this.setState()
+    },
+  })
+
   this.setState = async (nextData) => {
     this.userListData = await getUsers()
     this.todoListData = await getTodoLists()
 
     this.UserList.setState(this.userListData)
     this.TodoList.setState(this.todoListData)
+    this.TodoInput.setState('')
   }
 
   this.setState()
