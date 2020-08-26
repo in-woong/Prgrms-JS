052c352eeee2c57ec0edb03e1503b481cbaac25f
Jiwoo Kang <jiiw.kang@gmail.com>
2020-08-26 20:06:54 +0900

    [미션1] constmoon - TodoList 컴포넌트 작성 (#286)
    * [미션1] TodoList 컴포넌트 작성 #49
    
    * [미션1] 보너스 구현사항 - 다중 컴포넌트 #57
    
    * [미션1] 보너스 구현사항 - isCompleted 처리 #66

---
+++ mission1/7th/data.js
@@ -0,0 +1,14 @@
+var studyData = [
+  { text: 'JS 공부하기', isCompleted: true },
+  { text: 'JS 복습하기', isCompleted: true },
+];
+
+var workoutData = [
+  { text: '유산소 30분 이상 하기', isCompleted: false },
+  { text: '스쿼트 15회 3세트 하기', isCompleted: true },
+];
+
+var sleepData = [
+  { text: '오전 2시 이전에 잠들기', isCompleted: false },
+  { text: '오후 10시 이후에 먹지 않기', isCompleted: false },
+];
