bed07e9b24b21baf1c6ded4741c03011ded36999
dimsssss <caruen@hanmail.net>
2020-08-29 18:36:09 +0900

    ETC: 오늘, 내일 할 일 데이터 추가

--- mission1/8th/dimsss/todoData.js
+++ mission1/8th/dimsss/todoData.js
@@ -1,8 +1,27 @@
-export default [{
+export const todoData = [{
   text: 'JS 공부하기',
   isCompleted: true,
 },
 {
   text: 'JS 복습하기',
+  isCompleted: false,
+}]
+
+export const todayTodoData = [{
+  text: '세탁기 돌리기',
+  isCompleted: true,
+},
+{
+  text: '우산 챙기기',
+  isCompleted: false,
+}]
+
+
+export const tomorrowTodoData = [{
+  text: '과제 제출',
+  isCompleted: true,
+},
+{
+  text: '코드리뷰 받기',
   isCompleted: false,
 }]
