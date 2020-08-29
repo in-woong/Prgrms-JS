0a47939e91a4804f7f225adddd7a72a755f3eb30
hongsun <hongsun3152@gmail.com>
2020-08-29 16:46:28 +0900

    [Mission1] 보너스 구현사항 (#57)

--- mission1/8th/hongsun/app.js
+++ mission1/8th/hongsun/app.js
@@ -1,4 +1,9 @@
+import { data, csTodoData, jsTodoData } from './data.js';
 import TodoList from './components/TodoList.js';
 
-const todoList = new TodoList('todo-wrapper', data);
+const todoList = new TodoList('todo-default-wrapper', data);
+const csTodoList = new TodoList('todo-cs-wrapper', csTodoData);
+const jsTodoList = new TodoList('todo-js-wrapper', jsTodoData);
 todoList.render();
+csTodoList.render();
+jsTodoList.render();
---
+++ mission1/8th/hongsun/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    text: 'JS 공부하기',
+    isCompleted: true,
+  },
+  {
+    text: 'JS 복습하기',
+    isCompleted: false,
+  },
+];
+const csTodoData = [
+  {
+    text: '네트워크 공부하기',
+    isCompleted: false,
+  },
+  {
+    text: '자료구조 공부하기',
+    isCompleted: false,
+  },
+];
+const jsTodoData = [
+  {
+    text: 'Class 깊게 공부하기',
+    isCompleted: false,
+  },
+  {
+    text: 'XHR 공부하기',
+    isCompleted: false,
+  },
+];
+
+export { data, csTodoData, jsTodoData };
