250d3e58232699c1f4a7fcd2d152225906bb5074
handhand <sohnjunior@naver.com>
2020-08-29 13:20:16 +0900

    refactor: data 모듈화
    * 기존에 script 내부에 선언되어있던 data 들을 별도의 모듈로 변경

---
+++ mission1/8th/handhand/data.js
@@ -0,0 +1,45 @@
+const data1 = [
+  {
+    text: 'JS 공부하기',
+    isCompleted: true,
+  },
+  {
+    text: 'JS 복습하기',
+    isCompleted: true,
+  }
+];
+
+const data2 = [
+  {
+    text: '미션 수행하기',
+    isCompleted: true,
+  },
+  {
+    text: 'PR 보내기',
+    isCompleted: false,
+  }
+];
+
+const data3 = [
+  {
+    text: '피드백 반영하기',
+    isCompleted: false,
+  },
+  {
+    text: '리팩토링하기',
+    isCompleted: false,
+  }
+];
+
+const data4 = [
+  {
+    text: '핸드폰 바꾸기',
+    isCompleted: false,
+  },
+  {
+    text: '귀여운 라이언',
+    isCompleted: false,
+  }
+];
+
+export { data1, data2, data3, data4 };
