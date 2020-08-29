5e4ea6478183d4d78c3bd82331126d3b755c2d68
dimsssss <caruen@hanmail.net>
2020-08-29 18:36:32 +0900

    Feat: 오늘, 내일 할 일 컴포넌트 추가

--- mission1/8th/dimsss/index.js
+++ mission1/8th/dimsss/index.js
@@ -1,7 +1,12 @@
 import { TodoList } from './todolist.js';
-import todoData from './todoData.js';
+import {todoData, todayTodoData, tomorrowTodoData} from './todoData.js';
 
 (() => {
   const todoListElement = document.querySelector('#todo-list');
+  const todayTodoListElement = document.querySelector('#todo-list-today');
+  const tomorrowTodoListElement = document.querySelector('#todo-list-tomorrow');
+
   const TodoListComponent = new TodoList(todoListElement, todoData);
+  const todayTodoListComponent = new TodoList(todayTodoListElement, todayTodoData);
+  const tomorrowTodoListComponent = new TodoList(tomorrowTodoListElement, tomorrowTodoData);
 })()
