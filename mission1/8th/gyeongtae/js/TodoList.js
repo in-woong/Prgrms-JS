ee37bc6e037d9907cf4abb079af662632fa9779d
letsdothat <38155640+letsdothat@users.noreply.github.com>
2020-08-31 20:33:31 +0900

    [Mission1] 김경태 - TodoList 컴포넌트 불필요한 코드 삭제및 data체크 관련 함수 arrow function으로 변환

--- mission1/8th/gyeongtae/js/TodoList.js
+++ mission1/8th/gyeongtae/js/TodoList.js
@@ -6,16 +6,6 @@ const isCorrectData = (value) => {
 }
 // TodoList data체크 함수
 function todoListDataCheck(data) {
-  /*
-      if (data === null || data === undefined) {
-        throw new Error('데이터가 null이거나 undefined상태입니다')
-      } else if (!Array.isArray(data)) {
-        throw new Error('데이터가 array상태가 아닙니다')
-        // 배열의 요소중 하나라도 text, isCompleted프로퍼티중 하나가 없다면 false를 반환하여 error를 발생
-      } else if (!data.every(isCorrectData)) {
-        throw new Error('데이터 내용이 이상합니다')
-      }
-      */
   if (data === null || data === undefined) {
     throw new Error('데이터가 null이거나 undefined상태입니다')
   }
@@ -30,7 +20,6 @@ function todoListDataCheck(data) {
 
 // function style TodoList 컴포넌트
 function TodoList(querySelect, data) {
-  //console.log(new.target)
   this.data = data
   this.querySelect = document.querySelector(querySelect)
   // 만약 this가 window인경우 (생성자 함수에 new연산자를 붙이지 않은경우)
@@ -42,14 +31,12 @@ function TodoList(querySelect, data) {
   todoListDataCheck(this.data)
   // data값에 따라 todo-list를 동적으로 렌더링한다
   this.render = () => {
-    //console.log(new.target)
     let html = ''
     this.data.forEach((data) => {
       console.log(data)
       html += `<div>${
         data.isCompleted ? `<s>${data.text}</s>` : data.text
       }</div>`
-      // document.querySelector(querySelect).innerHTML += html
     })
     this.querySelect.innerHTML = html
   }
