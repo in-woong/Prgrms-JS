fab1fc20cdbeaa2c744b2a08292fb2c34845cbe3
letsdothat <38155640+letsdothat@users.noreply.github.com>
2020-09-06 19:17:55 +0900

    [Mission2] 김경태 - localStorage를 활용하여 새로고침하여도 데이터를 불러올 수 있도록 구현

--- mission2/8th/gyeongtae/js/App.js
+++ mission2/8th/gyeongtae/js/App.js
@@ -1,11 +1,15 @@
 import TodoList from './TodoList.js'
 import TodoInput from './TodoInput.js'
-import { data, data2, data3, data4, data5 } from './data.js'
 import TodoCount from './TodoCount.js'
 import TodoRemoveAll from './TodoRemoveAll.js'
 
 export default function App() {
   this.render = () => {
+    let data = window.localStorage.getItem('todoList')
+    data = JSON.parse(data)
+    if (!data) {
+      data = []
+    }
     this.todoList = new TodoList('#todo-list', data)
     this.todoCount = new TodoCount('#todo-count', data)
     this.todoInput = new TodoInput()
--- mission2/8th/gyeongtae/js/index.js
+++ mission2/8th/gyeongtae/js/index.js
@@ -4,6 +4,7 @@ const app = new App()
 
 const TodoListInputEle = document.querySelector('#input-todo-list')
 const TodoListFormEle = document.querySelector('#todo-list-input-form')
+const localStorage = window.localStorage
 
 // todo list 추가 함수
 function addTodoList(app) {
@@ -16,6 +17,7 @@ function addTodoList(app) {
     isCompleted: false,
   }
   const todoListData = [...app.todoList.data, todoListItem]
+  localStorage.setItem('todoList', JSON.stringify(todoListData))
   app.todoList.setState(todoListData)
   app.todoCount.setState(todoListData)
   // input값 초기화 및 input에 값 바로 입력할 수 있도록 함
@@ -28,6 +30,7 @@ function removeTodoList(app, key) {
   let todoListData = [...app.todoList.data]
   todoListData.splice(key, 1)
   todoListData = [...todoListData]
+  localStorage.setItem('todoList', JSON.stringify(todoListData))
   app.todoList.setState(todoListData)
   app.todoCount.setState(todoListData)
 }
@@ -35,6 +38,7 @@ function removeTodoList(app, key) {
 // todo list 전체 삭제 함수
 function removeAllTodoList(app) {
   let todoListData = []
+  localStorage.setItem('todoList', '[]')
   app.todoList.setState(todoListData)
   app.todoCount.setState(todoListData)
 }
@@ -50,6 +54,7 @@ function setTodoListIsCompleted(app, key) {
     todoListData[key].isCompleted = true
   }
   todoListData = [...todoListData]
+  localStorage.setItem('todoList', JSON.stringify(todoListData))
   app.todoList.setState(todoListData)
   app.todoCount.setState(todoListData)
 }
