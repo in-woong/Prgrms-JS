e616973fa3d414dc9583181a483d666bb2e7c5e3
inhyuck choi <dev.inhyuck@gmail.com>
2020-09-06 11:52:42 +0900

    [Mission2] inhyuck - input 컴포넌트화 하기 #76
    - todo 입력하는 영역(input, 입력 button) 컴포넌트화

--- mission2/8th/inhyuck/index.js
+++ mission2/8th/inhyuck/index.js
@@ -4,27 +4,7 @@ setTimeout(() => todoList.setState({nextTodoItems: nextData}), 500);
 const developerTodoList = new TodoList({todoItems: developerTodoItems, selector: '#developer-todo-list'});
 const happyLifeTodoList = new TodoList({todoItems: happyLifeTodoItems, selector: '#happy-life-todo-list'});
 
-//Mission2 필수구현
-const $todoItemAddButton = document.querySelector('#todo-item-add-button');
-const $todoItemAddInput = document.querySelector('#todo-item-add-input');
-
-const ALLOWED_ADD_TODO_ITEM_KEY = {
-    ENTER: 'Enter',
-    TAB: 'Tab',
-};
-//입력창에서 Enter, Tab 키 동작 시 TodoItem 추가
-$todoItemAddInput.addEventListener('keydown', event => {
-    if (Object.values(ALLOWED_ADD_TODO_ITEM_KEY).some(key => key === event.key)) {
-        saveNewTodoItem();
-    }
-});
-//Save 버튼 클릭 시 저장 TodoItem 추가
-$todoItemAddButton.addEventListener('click', () => {
-    saveNewTodoItem();
-}, );
-
-function saveNewTodoItem() {
-    todoList.addTodoItem({todoItemText: $todoItemAddInput.value});
-    $todoItemAddInput.value = '';
-    setTimeout(() => $todoItemAddInput.focus(), 0);
+function onSaveTodoItem({todoItemText}) {
+    todoList.addTodoItem({todoItemText});
 }
+const todoInput = new TodoInput({selector: '#todo-input', onSaveTodoItem});
