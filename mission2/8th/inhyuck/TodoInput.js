a5959a863c5db1820775f6f3285986a574e727ce
inhyuck choi <dev.inhyuck@gmail.com>
2020-09-08 21:07:04 +0900

    [Mission2] inhyuck - 코드리뷰 수정. TodoInput 컴포넌트 불필요한 render() 함수 제거.
    - TodoInput 컴포넌트의 경우 다시 render 되는 경우가 없으므로 불필요하게 render() 함수로 분리하지 않음.

--- mission2/8th/inhyuck/TodoInput.js
+++ mission2/8th/inhyuck/TodoInput.js
@@ -4,36 +4,29 @@ const ALLOWED_ADD_TODO_ITEM_KEY = {
 };
 
 function TodoInput({$targetElement, onSaveTodoItem}) {
-    this.$todoInputWrap = $targetElement;
-    this.$todoItemAddInput = null;
-    this.$todoItemAddButton = null;
-
-    this.render = function () {
-        this.$todoInputWrap.innerHTML = `
+    $targetElement.innerHTML = `
             <label for="todo-item-add-input">Enter what to do</label>
             <input type="text" id="todo-item-add-input">
             <button id="todo-item-add-button">Save</button>
         `;
 
-        this.$todoItemAddInput = this.$todoInputWrap.querySelector('#todo-item-add-input');
-        this.$todoItemAddButton = this.$todoInputWrap.querySelector('#todo-item-add-button');
-        //입력창에서 Enter, Tab 키 동작 시 TodoItem 추가
-        this.$todoItemAddInput.addEventListener('keydown', event => {
-            if (Object.values(ALLOWED_ADD_TODO_ITEM_KEY).some(key => key === event.key)) {
-                this.saveTodoItem();
-            }
-        });
-        //Save 버튼 클릭 시 저장 TodoItem 추가
-        this.$todoItemAddButton.addEventListener('click', () => {
-            this.saveTodoItem();
-        });
-    };
+    this.$todoItemAddInput = $targetElement.querySelector('#todo-item-add-input');
+    this.$todoItemAddButton = $targetElement.querySelector('#todo-item-add-button');
 
-    this.saveTodoItem = function () {
+    const saveTodoItem = () => {
         onSaveTodoItem({todoItemText: this.$todoItemAddInput.value});
         this.$todoItemAddInput.value = '';
         setTimeout(() => this.$todoItemAddInput.focus(), 0);
     };
 
-    this.render();
+    //입력창에서 Enter, Tab 키 동작 시 TodoItem 추가
+    this.$todoItemAddInput.addEventListener('keydown', event => {
+        if (Object.values(ALLOWED_ADD_TODO_ITEM_KEY).some(key => key === event.key)) {
+            saveTodoItem();
+        }
+    });
+    //Save 버튼 클릭 시 저장 TodoItem 추가
+    this.$todoItemAddButton.addEventListener('click', () => {
+        saveTodoItem();
+    });
 }
