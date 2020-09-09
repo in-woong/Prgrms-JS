6b338c6285f03df00bc5610ce246bd3b9b0f80a6
sarah1446 <lemonview2@gmail.com>
2020-09-09 11:04:43 +0900

    [Mission1] 윤영미 - 구조변경, 2차피드백 반영

--- mission1/8th/youngmi/index.js
+++ mission1/8th/youngmi/index.js
@@ -1,81 +1,78 @@
 
 const data = [
   {
-    text: '1_1',
-    isCompleted: true,
+    text: '공부하기',
+    isCompleted: false,
   },
   {
-    text: '1_2',
+    text: '복습하기',
     isCompleted: false,
   }
 ];
 
 const data2 = [
   {
-    text: '2_1',
+    text: '운동하기',
     isCompleted: true,
   },
   {
-    text: '2_2',
+    text: '청소하기',
     isCompleted: false,
   }
 ];
 
 const data3 = [
   {
-    text: '3_1',
+    text: '노래듣기',
     isCompleted: true,
   },
   {
-    text: '3_2',
+    text: '춤추기',
     isCompleted: false,
   }
 ];
 
 
 function TodoList(data, element) {
-
   this.data = data;
   this.element = element;
 
-  if (Array.isArray(this.data)) {
-    let content = '';
-    this.render = function() {
-      this.data.forEach((todo) => {
-        todo.isCompleted ? content += `<div>${todo.text}</div>` : content += `<div><s>${todo.text}</s></div>`
-      });
-      this.element.innerHTML = content;
-      // return content;
+  this.validateData = function() {
+    if (!Array.isArray(this.data)) {
+      throw new Error("배열형태가 아닙니다.")
+    } else if (this.data === (null || undefined)) {
+      throw new Error("데이터가 올바르지 않습니다.");
+    } else if (!Array.isArray(data)) {
+      throw new Error("배열 형식에 맞게 넣어주시길 바랍니다.");
+    } else if (this === window) {
+      throw new Error("new키워드를 확인해주시길 바랍니다.");
     }
-  } else {
-    errorMsg(this.data);
+
+    this.data.forEach((todo) => {
+      if (typeof todo.text !== 'string' || typeof todo.isCompleted !== 'boolean') {
+        throw new Error('올바른 데이터를 넣어주세요.');
+      }
+    });
+  }
+
+  this.render = function() {
+    this.element.innerHTML = this.data.map((todo) => {
+      return todo.isCompleted ? `<div>${todo.text}</div>` : `<div><s>${todo.text}</s></div>`
+    }).join('');
   }
 
   this.setState = function(nextData) {
     this.data = nextData;
-    // this.element.innerHTML = '';
-    // console.log(document.querySelector('#todo-list3'));
     this.render();
   }
-}
 
-function errorMsg(data) {
-  this.data = data;
-  if (this.data === (null || undefined)) {
-    throw new Error("데이터가 올바르지 않습니다.");
-  } else if (!Array.isArray(data)) {
-    throw new Error("배열 형식에 맞게 넣어주시길 바랍니다.");
-  }
+  this.render();
+  this.validateData();
 }
 
 const todoList = new TodoList(data, document.querySelector('#todo-list'));
-todoList.render();
-
 const todoList2 = new TodoList(data2, document.querySelector('#todo-list2'));
-todoList2.render();
-
-const todoList3 = new TodoList(data3, document.querySelector('#todo-list3'));
-todoList3.render();
+const todoList3 = TodoList(data3, document.querySelector('#todo-list3'));
 // todoList3.setState(data);
 
 
