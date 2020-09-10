1ebb6287f57666fa9bf9eeae7bceda3adac513c1
inhyuck choi <dev.inhyuck@gmail.com>
2020-09-11 07:50:53 +0900

    [Mission2] inhyuck - 페어프로그래밍 피드백 적용. localStorage.setItem() 호출 시 try/catch 적용

--- mission2/8th/inhyuck/App.js
+++ mission2/8th/inhyuck/App.js
@@ -99,7 +99,12 @@ function getStorage({key}) {
     }
 }
 function setStorage({key, value}) {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    try {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+        //저장공간을 초과하거나 브라우저설정에 따라 QUOTA_EXCEEDED_ERR 에러가 발생할 수 있음.
+        errorHandler({errorMessage: error.message})
+    }
 }
 
 function errorHandler({ errorMessage }) {
