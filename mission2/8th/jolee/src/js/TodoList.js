6672c212914cbe1abc2ee4420ce02d85f52b7629
jioun.lee.dev <jioun.lee@nts-corp.com>
2020-09-10 23:17:32 +0900

    [Mission2] JOLEE - TodoApp 업그레이드하기 #75

--- mission2/8th/jolee/src/js/TodoList.js
+++ mission2/8th/jolee/src/js/TodoList.js
@@ -2,15 +2,47 @@ function TodoList($target, data) {
   this.$target = $target
   this.data = data
 
-  this.render = function() {
+  this.render = function () {
     this.$target.innerHTML = this.data
-      .map(todo => `<div>${todo.text}</div>`)
+      .map(
+        (todo) =>
+          `<li class="todo-item">${
+            todo.isCompleted ? `<s>${todo.text}</s>` : `${todo.text}`
+          } <button class="delete-button">삭제</button></li>`
+      )
       .join('')
+
+    const $todoItems = document.querySelectorAll('.todo-item')
+    $todoItems.forEach(($todoItem, index) => {
+      $todoItem.addEventListener('click', () => {
+        this.data[index].isCompleted = !this.data[index].isCompleted
+        this.render()
+      })
+    })
+
+    const $deleteTodoBtns = document.querySelectorAll('.delete-button')
+    $deleteTodoBtns.forEach(($deleteTodoBtn, index) => {
+      $deleteTodoBtn.addEventListener('click', () => {
+        data.splice(index, 1)
+        this.render()
+      })
+    })
   }
 
-  this.setState = function(nextData) {
+  this.setState = function (nextData) {
     this.data = nextData
     this.render()
+  }
+
+  this.addTodoItem = function ({ todoText }) {
+    const newTodoItem = {
+      text: todoText,
+      isCompleted: false,
+    }
+
+    this.data.push(newTodoItem)
+    this.render()
+    console.log(this.data)
   }
 
   this.render()
--- mission2/8th/jolee/src/js/index.js
+++ mission2/8th/jolee/src/js/index.js
@@ -1,11 +1,31 @@
 var data = [
   {
     text: 'JS 공부하기',
+    isCompleted: false,
   },
   {
     text: 'JS 복습하기',
+    isCompleted: true,
   },
 ]
 
 const $target = document.querySelector('#todo-list')
 const todoList = new TodoList($target, data)
+
+// Mission2
+const $inputTodoText = document.querySelector('#todo-input-text')
+const $addTodoButton = document.querySelector('#todo-add-button')
+
+$addTodoButton.addEventListener('click', () => {
+  addNewTodoItem()
+})
+$inputTodoText.addEventListener('keydown', (event) => {
+  if(event.keyCode === 13) {
+    addNewTodoItem()
+  }
+})
+
+function addNewTodoItem() {
+  todoList.addTodoItem({ todoText: $inputTodoText.value })
+  $inputTodoText.value = ''
+}
