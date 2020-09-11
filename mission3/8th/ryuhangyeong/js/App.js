b99b8a7dc0b66e0e5d402b931dfe4747ea1604cd
ryuhangyeong <hangyeong.ryu@gmail.com>
2020-09-11 16:10:03 +0900

    [추가] 보너스 미션 - 검색 히스토리 #102

--- mission3/8th/ryuhangyeong/js/App.js
+++ mission3/8th/ryuhangyeong/js/App.js
@@ -1,12 +1,33 @@
 class App {
+  state = {
+    historyData: [],
+  }
+
   constructor($target) {
-    this.searchKeyword = new SearchKeyword({
+    this.searchHistory = new SearchHistory({
+      $target,
+      onSearch: async (value) => {
+        try {
+          const data = await getListByKeyword(value)
+          this.searchResult.setState(data)
+        } catch (e) {
+          alert(e)
+        }
+      },
+    })
+
+    this.searchInput = new SearchInput({
       $target,
       onSearch: (value) => {
         try {
           debounce(async () => {
             const data = await getListByKeyword(value)
+            const { historyData } = this.state
+
             this.searchResult.setState(data)
+
+            historyData.unshift(value)
+            this.searchHistory.setState(historyData)
           }, 400)
         } catch (e) {
           alert(e)
---
+++ mission3/8th/ryuhangyeong/js/components/SearchHistory.js
@@ -0,0 +1,40 @@
+class SearchHistory {
+  $target = null
+  $searchHistory = null
+  data = null
+  onSearch = null
+
+  constructor({ $target, initialData = [], onSearch }) {
+    this.$target = $target
+    this.data = initialData
+    this.onSearch = onSearch
+
+    this.$searchHistory = document.createElement('ul')
+
+    this.$target.appendChild(this.$searchHistory)
+
+    this.render()
+
+    this.$searchHistory.addEventListener('click', (e) => {
+      const { target } = e
+      const { tagName } = target
+
+      if (tagName === 'LI') this.onSearch(target.innerText)
+    })
+  }
+
+  setState(nextData) {
+    this.data = nextData
+    this.render()
+  }
+
+  render() {
+    this.$searchHistory.innerHTML = this.data
+      .map(
+        (item) => `
+      <li>${item}</li>
+    `
+      )
+      .join('')
+  }
+}
