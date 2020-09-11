8cf36c35dc2d5579088c156cca697778c5e6b4d6
letsdothat <38155640+letsdothat@users.noreply.github.com>
2020-09-11 23:29:27 +0900

    [Mission3] 김경태 - 움짤 검색기 구현 #92

--- mission3/8th/gyeongtae/SearchResult.js
+++ mission3/8th/gyeongtae/SearchResult.js
@@ -1,3 +1,23 @@
-export default function SearchResult() {
-  this.render = () => {}
+export default function SearchResult(data, target) {
+  this.data = data
+  this.target = document.querySelector(target)
+  this.render = () => {
+    const htmlString = this.data
+      .map(
+        (element) => `
+      <div class='serch-result'>
+      <img src="${element.imageUrl}">
+      <h3>${element.title}</h3>
+      <h6>#${element.tags.join('#')}</h6>
+      </div>
+      `
+      )
+      .join('')
+    this.target.innerHTML = htmlString
+  }
+  this.setState = (nextData) => {
+    this.data = nextData
+    this.render()
+  }
+  this.render()
 }
