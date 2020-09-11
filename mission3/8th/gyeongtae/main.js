3495895be11f6fd0cd22f2ea00496c495c3d8e42
letsdothat <38155640+letsdothat@users.noreply.github.com>
2020-09-12 00:46:36 +0900

    [Mission3] 김경태 - 움짤 검색기 async ~ awiat을 통하여 구현 #93

--- mission3/8th/gyeongtae/main.js
+++ mission3/8th/gyeongtae/main.js
@@ -4,10 +4,21 @@ function setEvent() {
   document
     .querySelector('#search-keyword')
     .addEventListener('keyup', function (e) {
-      fetch(`https://jjalbot.com/api/jjals?text=${e.target.value}`)
-        .then((data) => data.json())
-        .then((data) => serchResult.setState(data))
+      setSearchResult(e.target.value, searchResult)
     })
 }
-const serchResult = new SearchResult([], '#search-results')
+
+// 짤봇 api를 통해 짤 데이터를 가져옴
+async function getSearchResult(value) {
+  let searchResult = await fetch(`https://jjalbot.com/api/jjals?text=${value}`)
+  searchResult = await searchResult.json()
+  return searchResult
+}
+// 짤봇 api를 통해 가져온 짤 데이터를 컴포넌트에 설정함
+async function setSearchResult(value, SearchResultComponent) {
+  const searchResult = await getSearchResult(value)
+  SearchResultComponent.setState(searchResult)
+}
+
+const searchResult = new SearchResult([], '#search-results')
 setEvent()
