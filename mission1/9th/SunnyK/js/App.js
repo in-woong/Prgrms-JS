b47832319100afc4295d8051d581437dd882469e
sunyoung <sunnyk5780@gmail.com>
2020-10-30 00:49:02 +0900

    [Mission1] 컴포넌트 생성 시 const 변수 할당 삭제

--- mission1/9th/SunnyK/js/App.js
+++ mission1/9th/SunnyK/js/App.js
@@ -2,10 +2,10 @@ import TodoList1 from './TodoList1.js'
 import TodoList2 from './TodoList2.js'
 import { data1, data2, data3 } from './data.js'
 
-const todoList1 = new TodoList1(data1, 'todo-list1')
-const todoList2 = new TodoList1(data2, 'todo-list2')
-const todoList3 = new TodoList1(data3, 'todo-list3')
+new TodoList1(data1, 'todo-list1')
+new TodoList1(data2, 'todo-list2')
+new TodoList1(data3, 'todo-list3')
 
-const todoList4 = new TodoList2(data1, 'todo-list1')
-const todoList5 = new TodoList2(data2, 'todo-list2')
-const todoList6 = new TodoList2(data3, 'todo-list3')
+new TodoList2(data1, 'todo-list1')
+new TodoList2(data2, 'todo-list2')
+new TodoList2(data3, 'todo-list3')
