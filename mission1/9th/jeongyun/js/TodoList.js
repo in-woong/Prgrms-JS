dbb55c11cb2f2b94539a9d0082320746ff8faf60
choon <chachooon@gmail.com>
2020-10-29 10:24:39 +0900

    chore: 주석추가

--- mission1/9th/jeongyun/js/TodoList.js
+++ mission1/9th/jeongyun/js/TodoList.js
@@ -1,3 +1,7 @@
+/**
+ * validate data
+ * @param {Array} data 
+ */
 const checkData = (data) => {
   if(!data) {
     throw new Error('TypeError: null or undefined has no properties');
@@ -15,6 +19,11 @@ const checkData = (data) => {
   })
 }
 
+/**
+ * update TodoList to data
+ * @param {HTMLLIElement} element 
+ * @param {Array} data 
+ */
 const addToDo = (element, data) => {
   data.forEach(item => {
     const child = document.createElement("li");
@@ -24,36 +33,41 @@ const addToDo = (element, data) => {
 }
 
 function TodoList(id, data) {
+  // 파라메터로 받은 data this.data에 할당
+  this.id = id;
+  this.toDos = data;
+
   // new 키워드 안 붙이고 함수 실행 시 에러 처리
   if (!new.target) {
     throw new Error('not called with new');
   } 
 
-  // 파라메터로 받은 data this.data에 할당
-  this.id = id
-  this.toDos = data;
-
+ /**
+  * update TodoList to nextData
+  * @param {Array} nextData 
+  */
   this.setState = (nextData) => {
     // 이전 ul 삭제
     const target = document.querySelector(`${this.id}`);
     target.removeChild(target.childNodes[0]);
-
     // 수정할 데이터로 ul 다시 생성
-    const element = document.createElement("ul")
-    addToDo(element, nextData)
-    document.querySelector(this.id).appendChild(element)
+    const element = document.createElement("ul");
+    addToDo(element, nextData);
+    document.querySelector(this.id).appendChild(element);
   }
 
-  this.render = (toDos) => {
-    const element = document.createElement("ul")
-    addToDo(element, toDos)
-    document.querySelector(this.id).appendChild(element)
+ /**
+  * create TodoList 
+  * @param {Array} data 
+  */
+  this.render = (data) => {
+    const element = document.createElement("ul");
+    addToDo(element, data);
+    document.querySelector(this.id).appendChild(element);
   }
 
-  checkData(this.toDos)
+  checkData(this.toDos);
   this.render(this.toDos);
-
 }
-      
 
 export default TodoList;
