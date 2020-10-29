1e2879e54d7e7269fe5ecc4077dc0cf016e58e8b
choon <chachooon@gmail.com>
2020-10-29 09:41:12 +0900

    feat: 보너스 구현사항 - 다중 컴포넌트 #57

--- mission1/9th/jeongyun/js/app.js
+++ mission1/9th/jeongyun/js/app.js
@@ -1,4 +1,6 @@
 import  TodoList  from './TodoList.js'
-import { data } from './data.js'
+import { data, data2, data3 } from './data.js'
 
 const todoList = new TodoList("#todo-list", data);
+const todoList2 = new TodoList("#todo-list2", data2);
+const todoList3 = new TodoList("#todo-list3", data3);
--- mission1/9th/jeongyun/js/data.js
+++ mission1/9th/jeongyun/js/data.js
@@ -1,10 +1,33 @@
 export const data = [
     {
-      text: 'JS 공부하기!!!'
+      text: 'JS 공부하기'
     },
     {
       text: 'JS 복습하기'
     }
   ],
-  data2 = [],
-  data3 = [];
+  data2 = [
+    {
+      text: '[mission1] TodoList 컴포넌트 작성'
+    },
+    {
+      text: '[mission1] 보너스 구현사항'
+    },
+    {
+      text: '[mission1] 보너스 구현사항 - 다중 컴포넌트'
+    },
+    {
+      text: '[mission1] 보너스 구현사항 - isCompleted 처리'
+    }
+  ],
+  data3 = [
+    {
+      text: '아침 먹기'
+    },
+    {
+      text: '점심 맛있게 먹기'
+    },
+    {
+      text: '저녁 굶고 야식 먹기'
+    }
+  ];
