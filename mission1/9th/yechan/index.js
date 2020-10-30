d265d697a8d45ed1d8df8929bb648f1cb640b539
박에찬 <yuchan0315@naver.com>
2020-10-30 17:42:30 +0900

    saving files before refreshing line endings

--- mission1/9th/yechan/index.js
+++ mission1/9th/yechan/index.js
@@ -33,9 +33,9 @@ const hobby_data = [
 //error data
 const error = {
     INVALID_DATA: "유효하지 않은 데이터입니다.",
-    NOTARRAY_DATA: "데이터 타입이 ARRAY가 아닙니다.",
+    NOT_ARRAY_DATA: "데이터 타입이 ARRAY가 아닙니다.",
     NOT_NEW: "인스턴스가 아닙니다. new 키워드를 사용해 생성자를 만들어주세요.",
-    NOt_POSSIBLE_DATA: "해당 값이 null이거나 undefined입니다. 유효한 값을 입력해 주세요."
+    NOT_POSSIBLE_DATA: "해당 값이 null이거나 undefined입니다. 유효한 값을 입력해 주세요."
 }
 
 // 이곳에서 코딩을 시작하세요!
@@ -43,19 +43,19 @@ const error = {
 //error check
 const dataValidCheck = (data) => {
     //배열여부 검사
-    if (Array.isArray(data) === false) throw new Error(error.NOTARRAY_DATA)
+    if (Array.isArray(data) === false) throw new Error(error.NOT_ARRAY_DATA)
     //text 데이터를 가지고 있는지 여부 검사
     data.forEach(data => {
         // console.log(data)
         if (!data.hasOwnProperty('text')) throw new Error(error.INVALID_DATA)
-        if (data.text === (null || undefined)) throw new Error(error.INVALID_DATA)
+        if (data.text === (null || undefined)) throw new Error(error.NOT_POSSIBLE_DATA)
     })
 }
 
 function TodoList(data, selector) {
     //new 키워드 사용여부 검사.
     if (!new.target) throw new Error(error.NOT_NEW)
-    const createHTMLString = ({ text, isCompleted }) => {
+    const createHTMLString = ( {text, isCompleted} ) => {
         return `<div>${isCompleted ? `<s>${text}</s>` : `${text}`}</div>`
     }
     this.setState = (nextData) => {
