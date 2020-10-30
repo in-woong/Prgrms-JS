7eb75b831b8047d109ec5b54e36d594fccbe697d
pjaeyoung <saiei2484@gmail.com>
2020-10-30 23:54:21 +0900

    refactor : checkTypeOfData 유틸함수화 및 Error메세지에 listId 구체적으로 표시, findListById 삭제

--- mission1/9th/dodo/modules/TodoList.mjs
+++ mission1/9th/dodo/modules/TodoList.mjs
@@ -1,46 +1,38 @@
+function checkTypeOfData(data, listId){
+    if(!Array.isArray(data)){
+        throw new Error('data is not array');
+    }
+
+    for(let i = 0; i < data.length; i+=1){
+        if(!("text" in data[i])){
+            throw new Error(`<listId : ${listId}> data[${i}] has no text property`);
+        }
+
+        if(typeof data[i].text !== "string"){
+            throw new Error(`<listId : ${listId}> data[${i}].text is not string`);
+        }
+
+        if(!("isCompleted" in data[i])){
+            throw new Error(`<listId : ${listId}> data[${i}] has no isCompleted property`);
+        }
+
+        if(typeof data[i].isCompleted !== "boolean"){
+            throw new Error(`<listId : ${listId}> data[${i}].isCompleted is not boolean`);
+        }
+    }
+}
+
 class TodoList {
 
-    static checkTypeOfData(data){
-        if(!Array.isArray(data)){
-            throw new Error('data is not array');
-        }
-
-        for(let i = 0; i < data.length; i+=1){
-            if(!("text" in data[i])){
-                throw new Error(`data[${i}] has no text property`);
-            }
-
-            if(typeof data[i].text !== "string"){
-                throw new Error(`data[${i}].text is not string`);
-            }
-
-            if(!("isCompleted" in data[i])){
-                throw new Error(`data[${i}] has no isCompleted property`);
-            }
-
-            if(typeof data[i].isCompleted !== "boolean"){
-                throw new Error(`data[${i}].isCompleted is not boolean`);
-            }
-        }
-    }
-
-    static findListById(id){
-        if(typeof id !== 'string'){
-            throw new Error('list id is not string');
-        }
-
-        const elList = document.getElementById(id);
-        if(elList === null){
-            throw new Error('list id does not exists');
-        }
-
-        return elList;
-    }
-
     constructor(data,listId){
-        TodoList.checkTypeOfData(data);
+        checkTypeOfData(data,listId);
         this.data = data;
-        this.$list = TodoList.findListById(listId);
+        this.$list = document.getElementById(listId);
+
+        if(!this.$list){
+            throw new Error('list id does not exists')
+        }
+
         this.render();
     }
 
@@ -52,7 +44,7 @@ class TodoList {
     }
 
     setState(nextData){
-        TodoList.checkTypeOfData(nextData);
+        checkTypeOfData(nextData,this.$list.id);
         this.data = nextData;
         this.render();
     }
