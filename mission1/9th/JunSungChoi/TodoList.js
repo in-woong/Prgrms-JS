0d4df847de2ee613f020e48c75d54a4f38ef3749
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-11-01 01:33:02 +0900

    #50 [미션1] 보너스 구현사항 구현

--- mission1/9th/JunSungChoi/TodoList.js
+++ mission1/9th/JunSungChoi/TodoList.js
@@ -1,6 +1,35 @@
+function isEmpty (data) {
+  if (data === null || data === undefined) {
+      return true
+    }
+  return false
+}
+
+function propertyChecker (data) {
+  let isValid = true
+
+  data.forEach(item => {
+    if(!item.hasOwnProperty('text')) {
+      isValid = false
+    }
+  })
+  return isValid
+}
+
 function TodoList (data) {
-  this.data = data;
-  this.render = function () {
-    document.querySelector('#todo-list').innerHTML = `${data.map(item => item.text)}`
+  if (isEmpty(data) || !Array.isArray(data) || !propertyChecker(data)) {
+    throw new Error ('잘못된 데이터 입니다.')
   }
+  
+
+  if (!new.target) {
+    throw new Error ('new 키워드를 사용해주세요.')
+  }
+
+  this.data = data
+  this.render = function () {
+    document.querySelector('#todo-list').innerHTML = data.map(item => `<p>${item.text}</p>`).join('')
+  }
+
+  this.render()
 }
