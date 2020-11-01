69b28642786513366acea0e8bee513bbdde483f8
박에찬 <yuchan0315@naver.com>
2020-11-01 20:09:58 +0900

    setState 파라미터 수정, caramel case 수정

--- mission1/9th/yechan/index.js
+++ mission1/9th/yechan/index.js
@@ -1,5 +1,5 @@
-//data
-const todo_data = [
+//DATA
+const TODO_DATA = [
     {
         text: 'JS 공부하기',
         isCompleted: true
@@ -9,17 +9,17 @@ const todo_data = [
         isCompleted: false
     }
 ]
-const song_data = [
+const SONG_DATA = [
     {
         text: 'Bluemoon - 창모&효린',
-        isCompleted: null
+        isCompleted: true
     },
     {
         text: 'Creep - Radiohead',
         isCompleted: true
     }
 ]
-const hobby_data = [
+const HOBBY_DATA = [
     {
         text: '잠자기',
         isCompleted: false
@@ -30,7 +30,6 @@ const hobby_data = [
     }
 ]
 
-//error data
 const ERROR_MESSAGES = {
     NOT_INCLUDE_DATA: "유효하지 않은 데이터입니다.",
     INVALIDS_ISCOMPLETED_DATATYPE : "isComplted 값이 올바르지 않습니다.",
@@ -45,34 +44,33 @@ const ERROR_MESSAGES = {
 const dataValidCheck = (data) => {
     //배열여부 검사
     if (!Array.isArray(data)) throw new Error(ERROR_MESSAGES.NOT_ARRAY_DATA)
-    //text 데이터를 가지고 있는지 여부 검사
     data.forEach(data => {
         // console.log(data)
-        if (!data.hasOwnProperty('text')) throw new Error(ERROR_MESSAGES.NOT_INCLUDE_DATA)
+        // if (!data.hasOwnProperty('text')) throw new Error(ERROR_MESSAGES.NOT_INCLUDE_DATA)
+        if(typeof data.text !=='string') throw new Error(ERROR_MESSAGES.NOT_INCLUDE_DATA)
         if (data.isCompleted !==true && data.isCompleted !==false) throw new Error(ERROR_MESSAGES.INVALIDS_ISCOMPLETED_DATATYPE)
         if (data.text === null ||data.text=== undefined) throw new Error(ERROR_MESSAGES.INVALID_TEXT_DATATYPE)
     })
 }
 
-function TodoList(data, selector) {
+
+function TodoList(data,selector) {
     //new 키워드 사용여부 검사.
     if (!new.target) throw new Error(error.NOT_NEW)
-    const createHTMLString = ( {text, isCompleted} ) => {
-        return `<div>${isCompleted ? `<s>${text}</s>` : `${text}`}</div>`
-    }
-    this.setState = (nextData,selector) => {
+    const createHTMLString = ( {text, isCompleted} ) => `<li>${isCompleted ? `<s>${text}</s>` : `${text}`}</li>`
+
+    this.setState = (nextData) => {
         dataValidCheck(nextData)
-        this.selector = selector;
         this.data = nextData;
         this.render()
     }
     this.render = function () {
         const todoHTMLString = this.data.map(createHTMLString).join('');
-        console.log("todoHTMLString : " + todoHTMLString)
+        // console.log("todoHTMLString : " + todoHTMLString)
         document.querySelector(selector).innerHTML = todoHTMLString
     }
 }
-const todoList = new TodoList(todo_data,"#todo-list");
-setTimeout(() => { todoList.setState(todo_data) }, 0)
-setTimeout(() => { todoList.setState(song_data) }, 3000)
-setTimeout(() => { todoList.setState(hobby_data) }, 6000)
+const todoList = new TodoList(TODO_DATA,"#todo-list");
+setTimeout(() => { todoList.setState(TODO_DATA) }, 0)
+setTimeout(() => { todoList.setState(SONG_DATA) }, 3000)
+setTimeout(() => { todoList.setState(HOBBY_DATA) }, 6000)
