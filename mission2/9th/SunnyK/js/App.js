6ef48d9d402ac8f56aba3aefa39d7c8b4b4d9c76
sunyoung <sunnyk5780@gmail.com>
2020-11-05 03:47:18 +0900

    [Mission2] #75 할 일 추가하기

--- mission2/9th/SunnyK/js/App.js
+++ mission2/9th/SunnyK/js/App.js
@@ -1,4 +1,28 @@
 import TodoList from './TodoList.js'
 import { data } from './data.js'
 
-new TodoList(data, 'todo-list')
+const todoList = new TodoList(data, 'todo-list')
+
+document.querySelector('#todo-text-input').addEventListener('keypress', (e) => {
+  const ENTER_KEY_CODE = 13
+  if (e.keyCode === ENTER_KEY_CODE) addTodo(e.target)
+})
+
+document
+  .querySelector('#todo-input-btn')
+  .addEventListener('click', (e) =>
+    addTodo(document.querySelector('#todo-text-input'))
+  )
+
+function addTodo($todoTextInput) {
+  todoList.setState([
+    ...todoList.data,
+    {
+      text: $todoTextInput.value,
+      isCompleted: false,
+    },
+  ])
+
+  $todoTextInput.value = ''
+  $todoTextInput.focus()
+}
--- mission2/9th/SunnyK/js/TodoList.js
+++ mission2/9th/SunnyK/js/TodoList.js
@@ -2,36 +2,36 @@ import { useNewKeyword, isArrayState, checkTypes } from './Validation.js'
 
 /**
  * TodoList 컴포넌트
- * @param {Array} data 
- * @param {String} targetId 
+ * @param {Array} todoData
+ * @param {String} targetId
  */
-export default function TodoList1(data, targetId) {
-
-  this.validData = (data) => {
+export default function TodoList1(todoData, targetId) {
+  this.validData = (todoData) => {
     useNewKeyword(this)
-    isArrayState(data)
+    isArrayState(todoData)
     checkTypes(
-      data, 
-      ({ text, isCompleted }) => 
+      todoData,
+      ({ text, isCompleted }) =>
         typeof text === 'string' && typeof isCompleted === 'boolean'
     )
   }
 
   this.setState = (nextData, targetId) => {
     this.validData(nextData)
-    this.data = nextData
-    if(targetId) this.$target = document.getElementById(targetId)
+    this.todoData = nextData
+    if (targetId) this.$target = document.getElementById(targetId)
     this.render()
   }
 
   this.render = () => {
-    const htmlString = this.data.map(({ text, isCompleted }) => 
-      (isCompleted) ? `<li>${text}</li>` : `<li><s>${text}</s></li>`
-    ).join('')
+    const htmlString = this.todoData
+      .map(({ text, isCompleted }) =>
+        isCompleted ? `<li><s>${text}</s></li>` : `<li>${text}</li>`
+      )
+      .join('')
 
     this.$target.innerHTML = `<ul>${htmlString}</ul>`
   }
 
-  this.setState(data, targetId)
+  this.setState(todoData, targetId)
 }
- 
