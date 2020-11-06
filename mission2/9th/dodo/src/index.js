01d650f68fd1093bee713ac99456b750e3cbf259
pjaeyoung <saiei2484@gmail.com>
2020-11-06 21:35:53 +0900

    feat : input에 입력한 값을 todo-list에 추가하기

--- mission2/9th/dodo/src/index.js
+++ mission2/9th/dodo/src/index.js
@@ -1,3 +1,13 @@
 import TodoList from './TodoList.js'
 
 const todoList = new TodoList({ target: document.getElementById('todo-list') })
+const elInput = document.getElementById('input-todo')
+elInput.addEventListener('keypress', (e) => {
+  if (e.key !== 'Enter') return
+  if (elInput.value.length === 0) return
+  todoList.setState([
+    ...todoList.state,
+    { text: elInput.value, isCompleted: false },
+  ])
+  elInput.value = ''
+})
