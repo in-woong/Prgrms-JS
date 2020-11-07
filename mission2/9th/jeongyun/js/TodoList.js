aab150c10187ce1e183613172cc03598e01e958b
choon <chachooon@gmail.com>
2020-11-07 20:12:39 +0900

    feat: TodoApp 업그레이드하기

--- mission2/9th/jeongyun/js/TodoList.js
+++ mission2/9th/jeongyun/js/TodoList.js
@@ -1,6 +1,6 @@
 /**
  * validate data
- * @param {Array} data 
+ * @param {Array} data
  */
 const checkData = (data) => {
   if(!data) {
@@ -25,43 +25,90 @@ const checkData = (data) => {
   })
 }
 
-
 /**
- * 
- * @param {string} id 
- * @param {Array} data 
+ *
+ * @param {string} id
+ * @param {Array} data
  */
-function TodoList(id, data) {
+export default function TodoList(id, data) {
   // 파라메터로 받은 data this.data에 할당
   this.id = id;
-  this.toDos = data;
-
+  this.data = data;
+  this.targetId = null;
+  this.targetElement = document.querySelector(`#${this.id}`);
   // new 키워드 안 붙이고 함수 실행 시 에러 처리
   if (!new.target) {
     throw new Error('not called with new');
-  } 
+  }
 
- /**
-  * update TodoList to nextData
-  * @param {Array} nextData 
-  */
+  // click 이벤트 등록
+  this.targetElement.addEventListener("click", (event) => {
+    if (event.target.id.indexOf("todo-item") > -1 || event.target.id.indexOf("done-item") > -1) {
+      this.data = this.data.map((item, index) => {
+        return (parseInt(event.target.id.split("-")[2]) === index) ? {
+          text: item.text,
+          isCompleted: !item.isCompleted
+        } : item;
+      })
+      this.render();
+    }
+    if (event.target.id.indexOf("remove-item") > -1) {
+      this.data = this.data.filter((item, index) => parseInt(event.target.id.split("-")[2]) !== index)
+      this.render();
+    }
+  });
+  // submit 이벤트 등록
+  this.targetElement.addEventListener('submit', (event) => {
+    event.preventDefault();
+    this.data = [...this.data, {
+      text: document.getElementById("todo-list-input").value,
+      isCompleted: false
+    }];
+    this.render();
+  })
+
+  /**
+   * update TodoList to nextData
+   * @param {Array} nextData
+   */
   this.setState = (nextData) => {
-    this.toDos = nextData;
+    this.data = [...this.data, ...nextData];
     this.render();
   }
 
- /**
-  * create TodoList 
-  */
+  /**
+   * create TodoList
+   */
   this.render = () => {
     // 데이터 검증
-    checkData(this.toDos);
-    // toDo 항목 생성
-    document.querySelector(this.id).innerHTML = `<ul>${this.toDos.map(item => 
-      item.isCompleted ? `<li><s>${item.text}</s><li>` : `<li>${item.text}<li>`).join('')}</ul>`
+    checkData(this.data);
+
+    // todo-input
+    const todoInput = `
+    <form>
+        <label for="todo-list-input">${this.id}: </label>
+        <span class="todo">
+            <input type="text" name="todo" id="todo-list-input" placeholder="할 일을 입력하세요."/>
+            <button type="reset">✕</button>
+        </span>
+        <button type="submit">Submit</button>
+    </form>`;
+
+    // todo-list
+    const todoList = `<ul>${this.data.map(({ text, isCompleted }, index) =>
+      `<li>
+        <span id="todo-item-${index}"> ${isCompleted ? `✔<del id="done-item-${index}">${text}</del>` : text}</span>
+        <button type="button" id="remove-item-${index}">✕</button>
+      <li>`
+    )
+      .join('')}</ul>`;
+
+    // todo-input, todo-list 생성
+    this.targetElement.innerHTML = todoInput + todoList;
+    // todo-input 포커스
+    document.querySelector(`#${this.id}-input`)
+      .focus();
   }
 
   this.render();
 }
-
-export default TodoList;
--- mission2/9th/jeongyun/js/app.js
+++ mission2/9th/jeongyun/js/app.js
@@ -1,15 +1,7 @@
 import  TodoList  from './TodoList.js'
-import { data, data2, data3 } from './data.js'
+import { data } from './data.js'
 
-new TodoList("#todo-list", data);
-new TodoList("#todo-list3", data3);
 
-const todoList = new TodoList("#todo-list2", data2);
+new TodoList("todo-list", data);
 
-todoList.setState ([
-    ...data2,
-    {
-        text: '[mission1] 보너스 구현사항 - setState',
-        isCompleted: false
-    }
-]);
+
