b9724734ff5fd3294697c6abdec4cf29adc7e53a
sunyoung <sunnyk5780@gmail.com>
2020-11-08 08:15:48 +0900

    [Mission2] TodoList 내에서 쓰이는 클래스이름 상수로 모음

--- mission2/9th/SunnyK/js/TodoList.js
+++ mission2/9th/SunnyK/js/TodoList.js
@@ -1,5 +1,12 @@
 import { useNewKeyword } from './Validation.js'
 
+const TODOLIST_CALSS_NAMES = {
+  todoDeleteBtn: 'todo-delete-btn',
+  todoLabel: 'todo-label',
+  todoCheckbox: 'todo-checkbox',
+  todoText: 'todo-text',
+}
+
 /**
  * TodoList 컴포넌트
  * @param {Array} todoData
@@ -18,17 +25,15 @@ export default function TodoList({ todoData, $target }) {
   }
 
   this.createTodoHTMLString = ({ text, isCompleted }, index) => {
-    const DELETE_BTN_HTML_STRING = '<button class="todo-delete-btn">X</button>'
     return `
       <li data-id=${index}>
-        <label class="todo-label">
-          <input class="todo-checkbox" type="checkbox" ${
-            isCompleted ? 'checked' : ''
-          }/>
-          <span class="todo-text">${text}</span>
+        <label class="${TODOLIST_CALSS_NAMES.todoLabel}">
+          <input class="${TODOLIST_CALSS_NAMES.todoCheckbox}" 
+            type="checkbox" ${isCompleted ? 'checked' : ''}/>
+          <span class="${TODOLIST_CALSS_NAMES.todoText}">${text}</span>
         </label>
         
-        ${DELETE_BTN_HTML_STRING}
+        <button class="${TODOLIST_CALSS_NAMES.todoDeleteBtn}">X</button>
       </li>
     `
   }
@@ -42,14 +47,17 @@ export default function TodoList({ todoData, $target }) {
   }
 
   this.deleteTodoEvent = $target.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
+    if (e.target.className === TODOLIST_CALSS_NAMES.todoDeleteBtn) {
       this.deleteTodo(e.target.parentNode.dataset.id)
     }
   })
 
   this.toggleTodoEvent = $target.addEventListener('click', (e) => {
     if (
-      e.target.className === ('todo-label' && 'todo-text' && 'todo-checkbox')
+      e.target.className ===
+      (TODOLIST_CALSS_NAMES.todoLabel &&
+        TODOLIST_CALSS_NAMES.todoText &&
+        TODOLIST_CALSS_NAMES.todoCheckbox)
     ) {
       // Internet Explorer는 closest() 지원 안함
       this.toggleTodo(e.target.closest('li').dataset.id)
