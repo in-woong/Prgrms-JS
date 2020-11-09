df7d40a7bb4874cfd99d6d681d23581d71bba55b
seulgiyoon <sgyoon.info@gmail.com>
2020-11-10 00:38:35 +0900

    Add 데이터를 localStorage에 저장하도록 변경 및 커스텀 이벤트 추가

--- mission2/9th/seulgiyoon/App.js
+++ mission2/9th/seulgiyoon/App.js
@@ -1,50 +1,65 @@
 import TodoList from './TodoList.js'
 import TodoInput from './TodoInput.js'
 import TodoCount from './TodoCount.js'
-import Data from './Data.js'
 
 export default class App {
-  constructor() {
+  constructor(el) {
+    this.el = document.getElementById(el)
+    this.data = JSON.parse(localStorage.getItem('data')) || []
     this.input = new TodoInput('todo-form')
-    this.list = new TodoList('todo-list', Data.todos)
-    this.counter = new TodoCount('todo-counter', Data.todos)
+    this.list = new TodoList('todo-list', this.data)
+    this.counter = new TodoCount('todo-counter', this.data)
+    this.init()
+  }
+
+  init() {
     this.input.addSubmitEvent(this.addNewItem.bind(this))
     this.addListClickEvent()
   }
 
-  reRender() {
-    this.list.setState(Data.todos)
-    this.counter.setState(Data.todos)
+  reRender(data) {
+    this.data = data
+    this.list.setState(data)
+    this.counter.setState(data)
+    localStorage.setItem('data', JSON.stringify(data))
   }
 
   addNewItem(value) {
-    Data.todos = [{ text: value, isCompleted: false }, ...Data.todos]
-    this.reRender()
+    const newData = [{ text: value, isCompleted: false }, ...this.data]
+    this.reRender(newData)
   }
 
   removeItem(tartgetText) {
-    Data.todos = Data.todos.filter(
+    const newData = this.data.filter(
       (item) => item.text.replaceAll(' ', '') !== tartgetText
     )
-    this.reRender()
+    this.reRender(newData)
   }
 
   completeItem(targetText) {
-    Data.todos = Data.todos.map((item) =>
+    const newData = this.data.map((item) =>
       item.text.replaceAll(' ', '') === targetText
         ? { ...item, isCompleted: !item.isCompleted }
         : item
     )
-    this.reRender()
+    this.reRender(newData)
   }
 
   addListClickEvent() {
-    document.querySelector('ul').addEventListener('click', (e) => {
+    const todoListElement = document.querySelector('ul')
+    todoListElement.addEventListener('click', (e) => {
       if (e.target.className === 'close_btn') {
         this.removeItem(e.target.parentNode.dataset.text)
       } else if (e.target.className === 'text' || 'text_complete') {
         this.completeItem(e.target.parentNode.dataset.text)
       }
+    })
+  }
+
+  addCustomResetEvent(event) {
+    this.el.addEventListener(event, () => {
+      const newData = []
+      this.reRender(newData)
     })
   }
 }
--- mission2/9th/seulgiyoon/main.js
+++ mission2/9th/seulgiyoon/main.js
@@ -1,5 +1,11 @@
 import App from './App.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  new App()
+  const app = new App('app')
+
+  // 커스텀 이벤트
+  app.addCustomResetEvent('@reset')
+  document.getElementById('todo-reset').addEventListener('click', () => {
+    app.el.dispatchEvent(new CustomEvent('@reset'))
+  })
 })
