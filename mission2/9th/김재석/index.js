b0419e9a3524f11cb270453dc81b74b1879aafbe
fvg256 <39579423+fvg256@users.noreply.github.com>
2020-11-10 03:30:28 +0900

    [Mission2] 김재석 #75 구현

---
+++ mission2/9th/김재석/index.js
@@ -0,0 +1,13 @@
+var data = [
+    {
+      text: 'JS 공부하기',
+      isCompleted : true
+    },
+    {
+      text: 'JS 복습하기',
+      isCompleted : false
+    }
+  ]
+
+
+ new TodoList(data, "#todo-list")
