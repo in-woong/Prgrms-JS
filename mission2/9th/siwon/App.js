1d28d14ff7172fa4af282846e26a1077bb5e0b26
blacksiwon <cool1726@naver.com>
2020-11-10 16:19:21 +0900

    [Mission2] siwon - #91 localStorage 구현

--- mission2/9th/siwon/App.js
+++ mission2/9th/siwon/App.js
@@ -3,8 +3,12 @@ import TodoList from './TodoList.js';
 import TodoCount from './TodoCount.js';
 
 // TodoInput과 TodoList를 관리하는 APP Component
-export default function App(initialData) {
-    let data = initialData;
+export default function App() {
+    const todoStorage = window.localStorage;
+    let data = []
+    if (todoStorage.length !== 0) {
+        data = JSON.parse(localStorage.getItem('todo'));
+    }
 
     const $todoInput = document.querySelector("#todo-input");
     const $todoList = document.querySelector("#todo-list");
@@ -14,26 +18,30 @@ export default function App(initialData) {
     // input으로 입력한 todo를 추가하는 함수
     this.addTodo = (newTodo) => {
         data.push({ text: newTodo, isCompleted: false });
-        this.todoList.setState(data);
-        this.todoCount.countCompleted(data);
+        this.changeState();
     }
 
     // 특정 index의 todo를 삭제하는 함수
     this.deleteTodo = (index) => {
         data.splice(index, 1);
-        this.todoList.setState(data);
-        this.todoCount.countCompleted(data);
+        this.changeState();
     }
 
     // 특정 index의 todo의 완료여부를 변경하는 함수
     this.toggleTodo = (index) => {
         data[index].isCompleted = !data[index].isCompleted;
+        this.changeState();
+    }
+
+    this.changeState = () => {
+        todoStorage.setItem('todo', JSON.stringify(data));
         this.todoList.setState(data);
         this.todoCount.countCompleted(data);
     }
 
     this.clearTodo = () => {
         data.splice(0, data.length);
+        todoStorage.clear();
         this.todoList.setState(data);
         this.todoCount.countCompleted(data);
     }
--- mission2/9th/siwon/index.js
+++ mission2/9th/siwon/index.js
@@ -1,23 +1,3 @@
 import App from './App.js';
-import TodoList from './TodoList.js';
 
-const data = [
-    {
-        text: 'JS 공부하기',
-        isCompleted: true
-    },
-    {
-        text: 'JS 복습하기',
-        isCompleted: true
-    },
-    {
-        text: '낮잠자기',
-        isCompleted: false
-    },
-    {
-        text: '텀프 제출하기',
-        isCompleted: true
-    },
-];
-
-const todoApp = new App(data);
+new App();
