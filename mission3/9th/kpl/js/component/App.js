ab8de81565758940248e1194ebbfae91b1444666
aB <rkdvnfma90@hanmail.net>
2020-11-12 14:19:29 +0900

    [Mission3] 강푸름 - setState 위치변경

--- mission3/9th/kpl/js/component/App.js
+++ mission3/9th/kpl/js/component/App.js
@@ -4,24 +4,23 @@ import SearchResult from './SearchResult.js'
 function App({$app}) {
     this.$app = $app;
     this.data = [];
-    this.components = [
-        new SearchKeyword({
-            $app,
-            onSearchResult: this.setState
-            
-        }),
-        new SearchResult({
-            $app,
-            data: this.data
-        })
-    ];
-
     this.setState = (searchResultData) => {
         this.data = searchResultData;
         this.components.forEach((component) => {
             component.setState && component.setState(this.data);
         });
     };
+
+    this.components = [
+        new SearchKeyword({
+            $app,
+            onSearchResult: this.setState
+        }),
+        new SearchResult({
+            $app,
+            data: this.data
+        })
+    ];
 }
 
 export default App
