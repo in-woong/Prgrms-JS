d10262ad56c2c8392eac1630ff02796f3b96803f
aB <rkdvnfma90@hanmail.net>
2020-11-15 08:42:41 +0900

    [Mission3] 강푸름 - contant.js 주석추가

--- mission3/9th/kpl/js/constant/constant.js
+++ mission3/9th/kpl/js/constant/constant.js
@@ -8,4 +8,7 @@ export const JJALBOT_API_URL = 'https://jjalbot.com/api/jjals?text';
  */
 export const JJALBOT_DELAY_TIME = 500;
 
+/**
+ * 짤방 검색기록을 사용하기 위한 localStorage 키
+ */
 export const JJALBOT_HISTORY_STORAGE_KEY = 'jjalbotStorageKey';
