740859827edef3578d78313bb8fab3c3f99296eb
aB <rkdvnfma90@hanmail.net>
2020-11-15 08:41:07 +0900

    [Mission3] 강푸름 - util 수정

--- mission3/9th/kpl/js/util/util.js
+++ mission3/9th/kpl/js/util/util.js
@@ -29,38 +29,12 @@ export const onDebounce = function({inputData, fetchFunction, delay}) {
 
 /**
  * 
- * @param {parseData} string 
- * JSON parse를 실행한다.
- */
-export const jsonParse = (parseData) => {
-    try {
-        return JSON.parse(parseData);
-    } catch (error) {
-        throw new Error('JSON parse시 오류가 발생했습니다.');
-    }
-}
-
-/**
- * 
- * @param {jsonData} json 
- * JSON stringify를 실행한다.
- */
-export const jsonStringify = (jsonData) => {
-    try{
-        return JSON.stringify(jsonData);
-    }catch(error) {
-        throw new Error('JSON stringify시 오류가 발생했습니다.');
-    }
-}
-
-/**
- * 
  * @param {localStorageKey, localStorageData} 
  * 로컬스토리지에서 아이템을 저장한다.
  */
 export const setItemLocalStorage = (localStorageKey, localStorageData) => {
     try {
-        localStorage.setItem(localStorageKey, jsonStringify(localStorageData));
+        localStorage.setItem(localStorageKey, JSON.stringify(localStorageData));
     } catch(error) {
         throw new Error(`localStorage에 항목추가시 에러가 발생하였습니다. ${error}`);
     }
@@ -73,7 +47,7 @@ export const setItemLocalStorage = (localStorageKey, localStorageData) => {
  */
 export const getItemLocalStorage = (localStorageKey) => {
     try {
-        return jsonParse(localStorage.getItem(localStorageKey));
+        return JSON.parse(localStorage.getItem(localStorageKey));
     } catch(error) {
         throw new Error(`localStorage 초기화시 에러가 발생하였습니다. ${error}`);
     }
