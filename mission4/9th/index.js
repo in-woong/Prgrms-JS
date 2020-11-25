ca8cb2ce4dd05c2a9d2b2c8f2d9a620e26ab3a8c
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2020-11-26 01:02:31 +0900

    필수구현사항

---
+++ mission4/9th/index.js
@@ -0,0 +1,6 @@
+import App from './App.js'
+import TodoList from './TodoList.js'
+
+const $app = document.querySelector('#app')
+
+new App({$app})
---
+++ mission4/9th/validation.js
@@ -0,0 +1,23 @@
+export const useNewKeyword = (context) => {
+    if (context === window) {
+      throw new Error('new 키워드가 없음')
+    }
+  }
+  
+export const useArrayState = (state) => {
+    if (!Array.isArray(state)) {
+        throw new Error('잘못된 state 입니다!')
+    }
+}
+  
+export const checkTarget = ($target) => {
+    if (!$target) {
+      throw new Error('target이 올바르지 않습니다.')
+    }
+}
+  
+export const checkTypes = (state, checkCallback) => {
+    if (!state.every(checkCallback)) {
+      throw new Error('state가 맞지 않습니다.')
+    }
+}
