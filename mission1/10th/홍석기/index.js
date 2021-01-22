eff40a27524f25413a6bb263b141e2addd6bb2fd
Derek-94 <ghdtjrrl94@gmail.com>
2021-01-22 16:30:24 +0900

    feat(validation): Update data validation func.
    - Go through data validation before perform rendering.
    - If validation fails, message alerts.
    - Update README.md file.

--- mission1/10th/홍석기/index.js
+++ mission1/10th/홍석기/index.js
@@ -1,4 +1,5 @@
 import {RawData} from "./data/todoRawDatas.js";
+import {message} from "./constantValue/message.js";
 
 class controlToDoList {
     constructor(data) {
@@ -10,13 +11,15 @@ class controlToDoList {
         // initialize local Storage ... later.
     }
 
+    dataValidation() {
+        return Array.isArray(this.toDoData) && (typeof(this.toDoData) !== undefined) && (typeof(this.toDoData) !== null) ? true : false;
+    }
+
     render() {
         const todoLists = document.querySelector("#todo-list");
-        this.toDoData.forEach((rawData, index) => {
-            todoLists.innerHTML += `${index + 1}. ${rawData.text}</br>`;
-        })
+        this.toDoData.forEach((rawData, index) => todoLists.innerHTML += `${index + 1}. ${rawData.text}</br>`);     
     }
 }
 
 const toDoObj = new controlToDoList(RawData);
-toDoObj.render();
+toDoObj.dataValidation() ? toDoObj.render() : alert(message.RAW_DATA_TYPE_INAPPROPRIATE);
