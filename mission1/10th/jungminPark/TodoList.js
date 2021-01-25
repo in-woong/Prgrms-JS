755822e43c29c50b01ee926eb07b1e0e73a08ea5
kmujm <pmini09@kookmin.ac.kr>
2021-01-25 18:49:16 +0900

    [Mission1]박정민 - 필수&보너스 구현

---
+++ mission1/10th/jungminPark/TodoList.js
@@ -0,0 +1,31 @@
+class TodoList{
+    constructor(data){
+        this.data = data;
+        this.validator();
+        this.render();
+    }
+
+    //data의 유효성 검사
+    validator(){
+        try{
+            if(this.data === undefined || this.data === null){
+                throw new Error(ERROR_MESSAGE.IS_UNDEFINED_OR_NULL)
+            }
+            if(!Array.isArray(this.data)){
+                throw new Error(ERROR_MESSAGE.IS_NOT_ARRAY)
+            }
+        }
+        //그 외의 에러
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    render(){
+        let text = '';
+        this.data.map((item) => {
+            item.isCompleted == true ? text += `<div><s>${item.text}</s></div>` : text += `<div>${item.text}</div>`
+        })
+        document.querySelector('#todo-list').innerHTML += text;
+    }
+}
