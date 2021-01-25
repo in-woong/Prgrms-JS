fcf403497667d9027f48159ffa942656440e9589
곽경진 <kgj1212_@naver.com>
2021-01-25 20:41:28 +0900

    [Mission1] 곽경진 - fix 코드리뷰 내용 수정
    1. `index.html` 파일 내 script 위치변경
    2.  EOL
    3. map -> some / map -> forEach()
    4. 렌더링 될때마다 DOM Search 현상 개선
    5. innerHTML 여러번 -> 한번만 선언
    6. 인스턴스의 setState() 제거
    7. 에러메시지 상수로 분리
    8. 변수명 구체적으로 변경
    9. prettier 적용
    10. review.md 파일 추가

--- mission1/10th/gyeongjin/js/main.js
+++ mission1/10th/gyeongjin/js/main.js
@@ -1,47 +1,73 @@
 import { dataStudy, dataExercise, dataPlay } from './data.js';
 
+const ERROR_MESSAGE = {
+  INVALID: 'Invalid parameter',
+  IS_NOT_ARRAY: 'Data is not array',
+  IS_NOT_STRING: 'Title is must be type of string',
+  EMPTY_TEXT: 'Some Data does not have any text',
+};
+
 class TodoList {
-    constructor(title) {
-        this.title = title;
+  constructor(listTitle, nextData) {
+    // review4 : 렌더링 될떄마다 불필요한 DOM Search가 계속되는 문제해결
+    const $app = document.querySelector('#app');
+    const $listBox = document.createElement('div');
+    this.listBox = $listBox;
+    this.listTitle = listTitle;
+    this.nextData = nextData;
+
+    $app.appendChild($listBox);
+    $listBox.classList.add(listTitle);
+    this.setState(listTitle, nextData);
+  }
+
+  setState = (listTitle, nextData) => {
+    this.validator(listTitle, nextData);
+    this.render();
+  };
+
+  validator = (listTitle, listData) => {
+    if (
+      listData === null ||
+      listData === undefined ||
+      listTitle === null ||
+      listTitle === undefined
+    ) {
+      throw new Error(ERROR_MESSAGE.INVALID);
     }
+    if (!Array.isArray(listData)) {
+      throw new Error(ERROR_MESSAGE.IS_NOT_ARRAY);
+    }
+    if (typeof listTitle !== 'string') {
+      throw new Error(ERROR_MESSAGE.IS_NOT_STRING);
+    }
+    // review3 : immutable array 가 아니면 forEach / some 사용
+    listData.some((item) => {
+      if (item.text.length < 1) {
+        throw new Error(ERROR_MESSAGE.EMPTY_TEXT);
+      }
+    });
+  };
 
-    setState = ( nextData ) => {
-        this.nextData = nextData;
-        this.validator(this.title, nextData);
-        this.render();
-    };
+  render = () => {
+    // review5 : map은 immutable array를 새롭게 만들어낼때 사용,
+    // 업데이트시 reflow(리렌더링)을 야기시키는 innerHTML은 한번만 사용
+    let listTitleString = '';
+    let listItemString = '';
 
-    validator = (title, data) => {
-        if(data === null || data === undefined || title === null || title === undefined) { 
-            throw new Error("Invalid parameter")
-        };
+    listTitleString = `<strong class="title">${this.listTitle}</strong>`;
+    this.nextData.forEach((item) => {
+      item.isCompleted
+        ? (listItemString += `<div><s>${item.text}</s></div>`)
+        : (listItemString += `<div>${item.text}</div>`);
+    });
 
-        if(!Array.isArray(data)) {throw new Error("Data is not array")};
-
-        if(typeof title !== 'string') {throw new Error("Title is must be type of string")};
-
-        data.map((item) => {
-            if(item.text.length < 1) {
-                throw new Error("Data does not have any text")
-            }
-        });
-    };
-
-    render = () => {
-        const app = document.querySelector('#app');
-        const container = document.createElement('div');
-
-        container.classList.add(this.title);
-        app.appendChild(container);
-        container.innerHTML += `<strong class="title">${this.title}</strong>`;
-        this.nextData.map(item => { 
-           item.isCompleted ? 
-           container.innerHTML += `<div><s>${item.text}</s></div` : 
-           container.innerHTML += `<div>${item.text}</div>`
-        });
-    };
+    this.listBox.innerHTML = listTitleString + listItemString;
+  };
 }
 
-new TodoList('todo-list').setState(dataStudy);
-new TodoList('exercise-list').setState(dataExercise);
-new TodoList('play-list').setState(dataPlay);
+// review6 : 따로 인스턴스화 할 때 setState()를 하지 않도록 변경
+new TodoList('todo-list', dataStudy);
+new TodoList('exercise-list', dataExercise);
+new TodoList('play-list', dataPlay);
+// review2 : EOL
