491e2605af5fa5fbe46601d134208acab15c7252
Derek-94 <ghdtjrrl94@gmail.com>
2021-01-29 11:27:06 +0900

    refactor: Applied feed back
    - Change file name same as class name.
    - Put data validation and render function to contructor.
    - Make validation function more concise.
    - Update setState function to work asyn, after 3 seconds.

--- mission1/10th/홍석기/index.js
+++ mission1/10th/홍석기/index.js
@@ -1,13 +1,11 @@
 import {rawData, derekDoneRawData, derekToDoRawData} from "./data/todoRawDatas.js";
-import {message} from "./constantValue/message.js";
-import {ControlToDoList} from "./class/classHandler.js";
+import {ControlToDoList} from "./class/ControlToDoList.js";
 
 const toDoObj = new ControlToDoList(rawData, "todo-list", "TO Do List");
-toDoObj.checkDataForm() ? toDoObj.render() : (function() {throw new Error(message.RAW_DATA_TYPE_INAPPROPRIATE)})
-toDoObj.setState([{text: "앞구르기", isCompleted: true}, {text: "뒷구르기", isCompleted: false}]);
+
+setTimeout(() => {
+    toDoObj.setState([{text: "앞구르기", isCompleted: true}, {text: "뒷구르기", isCompleted: false}]);
+}, 3000);
 
 const derekToDoObj = new ControlToDoList(derekToDoRawData, "todo-list-derek", "Derek To Do List");
-derekToDoObj.checkDataForm() ? derekToDoObj.render() :  (function() {throw new Error(message.RAW_DATA_TYPE_INAPPROPRIATE)})
-
 const derekDoneObj = new ControlToDoList(derekDoneRawData, "done-list-derek", "Derek Done List");
-derekDoneObj.checkDataForm() ? derekDoneObj.render() : (function() {throw new Error(message.RAW_DATA_TYPE_INAPPROPRIATE)})
