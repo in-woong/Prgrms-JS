050935f2c1f6a08b9622d3b885679c32b46d5468
tiaz0128 <tiaz0128.dev@gmail.com>
2021-01-30 22:29:59 +0900

    [Mission2]통붕이 - #75 #76 #77 #81 #90 #91
    필수 구현사항 - TodoApp 업그레이드하기
    보너스 구현사항 - input 컴포넌트화 하기
    보너스 구현 사항 - TodoCount 컴포넌트
    보너스 구현사항 - Event delegate
    보너스 구현 사항 - 커스텀 이벤트
    보너스 구현사항 - localStorage

--- mission2/10th/tongbg/index.mjs
+++ mission2/10th/tongbg/index.mjs
@@ -1,53 +1,19 @@
-import TodoList from './TodList.js'
+import TodoApp from './asset/js/TodoApp.js'
 
-const todoData = [
-  {
-    text: 'JS 공부하기',
-    isCompleted: true,
-  },
-  {
-    text: 'JS 복습하기',
-    isCompleted: false,
-  },
-]
+// localStorage.setItem(
+//   'todoData',
+//   JSON.stringify([
+//     {
+//       text: 'JS 공부하기',
+//       isCompleted: true,
+//     },
+//     {
+//       text: 'JS 복습하기',
+//       isCompleted: false,
+//     },
+//   ])
+// )
+//localStorage.clear()
+const todoData = JSON.parse(localStorage.getItem('todoData'))
 
-const taskData = [
-  {
-    text: '라벨지 수정하기',
-    isCompleted: true,
-  },
-  {
-    text: 'BUSTSP01 배포하기',
-    isCompleted: false,
-  },
-]
-
-const shoppingData = [
-  {
-    text: '애호박',
-    isCompleted: true,
-  },
-  {
-    text: '짜요짜요',
-    isCompleted: false,
-  },
-]
-
-const todoList = new TodoList(todoData, 'todo-list')
-new TodoList(taskData, 'task-list')
-new TodoList(shoppingData, 'shopping-list')
-
-// 변경할 데이터
-const nextData = [
-  {
-    text: 'TypeScript 공부하기',
-    isCompleted: true,
-  },
-  {
-    text: 'TypeScript 복습하기',
-    isCompleted: false,
-  },
-]
-
-// 데이터변경 하면서 render
-todoList.setState(nextData)
+new TodoApp(todoData, document.querySelector('#todo-list'), document.querySelector('#todo-input'), document.querySelector('#todo-count'), document.querySelector('#todo-removeAll'))
