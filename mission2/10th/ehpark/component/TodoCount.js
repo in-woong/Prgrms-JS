2d3bcb5a06c792fbc47d50e24dc6e090a7860762
renee <renee13wi@gmail.com>
2021-02-01 14:34:37 +0900

    Modify code indent

--- mission2/10th/ehpark/component/TodoCount.js
+++ mission2/10th/ehpark/component/TodoCount.js
@@ -1,31 +1,31 @@
 export default function TodoCount($target, data, todoCountSelector) {
-	this.$target = $target
-	this.data = data
-	this.$todoCount = $target.querySelector(todoCountSelector)
+  this.$target = $target
+  this.data = data
+  this.$todoCount = $target.querySelector(todoCountSelector)
 
-	this.getTotalCount = () => {
-		return this.data.length
-	}
+  this.getTotalCount = () => {
+    return this.data.length
+  }
 
-	this.getCompletedCount = () => {
-		let completedInit = 0
-		this.data.forEach(item => {
-			if(item.isCompleted) {
-				completedInit += 1
-			}
-		})
-		return completedInit
-	}
+  this.getCompletedCount = () => {
+    let completedInit = 0
+    this.data.forEach(item => {
+      if(item.isCompleted) {
+        completedInit += 1
+      }
+    })
+    return completedInit
+  }
 
-	this.render = () => {
-		this.$todoCount.innerHTML = `<span class="unit-count">총 Todo의 갯수 : ${this.getTotalCount()}</span>
-																 <span class="unit-count">완료처리된 Todo의 갯수 : ${this.getCompletedCount()}</span>`
-	}
+  this.render = () => {
+    this.$todoCount.innerHTML = `<span class="unit-count">총 Todo의 갯수 : ${this.getTotalCount()}</span>
+                                 <span class="unit-count">완료처리된 Todo의 갯수 : ${this.getCompletedCount()}</span>`
+  }
 
-	this.setState = (nextState) => {
-		this.data = nextState
-		this.render()
-	}
+  this.setState = (nextState) => {
+    this.data = nextState
+    this.render()
+  }
 
-	this.render()
+  this.render()
 }
--- mission2/10th/ehpark/component/TodoInput.js
+++ mission2/10th/ehpark/component/TodoInput.js
@@ -1,28 +1,28 @@
 export default function TodoInput($target, todoInputBoxSelector, handlerAddTodo) {
-	this.$target = $target
-	this.$todoInputBox = this.$target.querySelector(todoInputBoxSelector)
-	this.handlerAddTodo = handlerAddTodo
+  this.$target = $target
+  this.$todoInputBox = this.$target.querySelector(todoInputBoxSelector)
+  this.handlerAddTodo = handlerAddTodo
 
-	this.render = () => {
-		this.todoInput = document.createElement('input')
-		this.todoInput.className = 'input-todo'
-		this.todoInput.type = 'text'
-		this.$todoInputBox.appendChild(this.todoInput)
+  this.render = () => {
+    this.todoInput = document.createElement('input')
+    this.todoInput.className = 'input-todo'
+    this.todoInput.type = 'text'
+    this.$todoInputBox.appendChild(this.todoInput)
 
-		this.btnTodoAdd = document.createElement('button')
-		this.btnTodoAdd.className = 'btn-todo-add'
-		this.btnTodoAdd.innerHTML = '추가'
-		this.$todoInputBox.appendChild(this.btnTodoAdd)
-	}
-	
-	this.eventAddTodo = () => {
-		this.btnTodoAdd.addEventListener('click', () => {
-				this.handlerAddTodo(this.todoInput.value)
-				this.todoInput.value =''
-				this.todoInput.focus()
-		})
-	}
+    this.btnTodoAdd = document.createElement('button')
+    this.btnTodoAdd.className = 'btn-todo-add'
+    this.btnTodoAdd.innerHTML = '추가'
+    this.$todoInputBox.appendChild(this.btnTodoAdd)
+  }
+  
+  this.eventAddTodo = () => {
+    this.btnTodoAdd.addEventListener('click', () => {
+        this.handlerAddTodo(this.todoInput.value)
+        this.todoInput.value =''
+        this.todoInput.focus()
+    })
+  }
 
-	this.render()
-	this.eventAddTodo()
+  this.render()
+  this.eventAddTodo()
 }
