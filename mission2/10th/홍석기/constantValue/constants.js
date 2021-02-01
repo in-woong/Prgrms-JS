15743af5f9deb74200fdb4222bfe94c9d91b0d58
Derek-94 <ghdtjrrl94@gmail.com>
2021-02-01 11:23:39 +0900

    feat(constants): Add contant values.
    - Add constants that is used when user input todos.
    - Add message that pops when user put blank todo.

--- mission2/10th/홍석기/constantValue/constants.js
+++ mission2/10th/홍석기/constantValue/constants.js
@@ -1,5 +1,8 @@
 const constants = {
-    LOCALSTORAGE_TO_DO_KEY : "todoElement"
+    LOCALSTORAGE_TO_DO_KEY : "todoElement",
+    TAG_INCLUDING_TODO: "li",
+    INDEX_NTH_IN_CHILDNODES: 0,
+    DONE_CLASS : "done"
 }
 
 export {constants};
