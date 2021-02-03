ec6584e645cfbbcf4c4da98df90e8110a78b632a
이동명 <73331335+dongmyoungLee@users.noreply.github.com>
2021-02-03 12:49:19 +0900

    Update TodoList.js

--- mission2/10th/dongmyoung/TodoList.js
+++ mission2/10th/dongmyoung/TodoList.js
@@ -22,7 +22,7 @@ function TodoList(initialState, $target) {
   }
 
   this.render = () => {
-      $target.innerHTML = this.state.filter(state => state.visible === true).map(state => state.isCompleted ? `<div style="display: flex;"><div onclick="isCompletedChange(${state.id})"><s>(완료) ${state.text}</s></div><button onclick="Delete(${state.id})">삭제</button></div>` :  `<div style="display: flex;"><div onclick="isCompletedChange(${state.id})">${state.text}</div><button onclick="Delete(${state.id})">삭제</button></div>`).join('')
+      $target.innerHTML = this.state.filter(state => state.visible === true).map(state => state.isCompleted ? `<div style="display: flex; margin-bottom: 10px; cursor: pointer;"><div onclick="isCompletedChange(${state.countData})"><s>(완료) ${state.text}</s></div><button style="margin-left: 10px;" onclick="Delete(${state.countData})">삭제</button></div>` :  `<div style="display: flex; margin-bottom: 10px; cursor: pointer;"><div onclick="isCompletedChange(${state.countData})">${state.text}</div><button style="margin-left: 10px;" onclick="Delete(${state.countData})">삭제</button></div>`).join('')
   }
   
 
@@ -36,17 +36,60 @@ function TodoList(initialState, $target) {
   this.render()
 }
 
-function Delete(stateId) {
-  studyData[stateId-1].visible = false
-  new TodoList(studyData, document.querySelector('#study-list'))
+function AddTodoBtn($addTarget, $id) {
+    document.getElementById('add_Btn').addEventListener('click', (evt) => {
+      inputValue = $addTarget.value
+      if(inputValue.length === 0) {
+        alert('할 일을 입력하고 버튼을 눌러주세요.')
+      }else{
+        studyData.push({
+          countData : studyData.length + 1,
+          text : `${inputValue}`,
+          isCompleted : false,
+          visible : true,
+        })
+
+        new TodoList(studyData, $target)
+        $addTarget.value = ''
+      }
+    }) 
+}
+
+function AddTodoEnter($addTarget, $id) {
+  document.getElementById('addTodo').addEventListener('keypress', function(key) {
+    if(key.key === 'Enter') {
+      inputValue = $addTarget.value
+      if(inputValue.length === 0) {
+        alert('할 일을 입력하세요.')
+      }else{
+        studyData.push({
+          countData : studyData.length + 1,
+          text : `${inputValue}`,
+          isCompleted : false,
+          visible : true,
+        })
+
+        new TodoList(studyData, $target)
+        $addTarget.value = ''
+      }
+    }
+    
+  
+  }) 
 }
 
 
-function isCompletedChange(stateId) {
-  if(studyData[stateId-1].isCompleted){
-    studyData[stateId-1].isCompleted = false
+function Delete(stateCount) {
+  studyData[stateCount-1].visible = false
+  new TodoList(studyData, $target)
+}
+
+
+function isCompletedChange(stateCount) {
+  if(studyData[stateCount-1].isCompleted){
+    studyData[stateCount-1].isCompleted = false
   }else{
-    studyData[stateId-1].isCompleted = true
+    studyData[stateCount-1].isCompleted = true
   }
-  new TodoList(studyData, document.querySelector('#study-list'))
+  new TodoList(studyData, $target)
 }
