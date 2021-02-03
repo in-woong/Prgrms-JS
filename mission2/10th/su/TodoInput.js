a27660528139a385b5d6c7272eae03ff121c27fa
suyeon <suyeon.jeong@atto-research.com>
2021-02-03 16:26:15 +0900

    feat: 할일 추가 후 입력 초기화

--- mission2/10th/su/TodoInput.js
+++ mission2/10th/su/TodoInput.js
@@ -4,38 +4,17 @@ function TodoInput(selector) {
   this.selector = selector;
   this.target = document.querySelector(this.selector);
 
-  this.validation = function() {
-    if(!data) throw new Error("data is error");
-    if(!selector) throw new Error("selector is error");
-    if(!(this instanceof TodoInput)) throw new Error("Add new keyword");
-  }
-
-  this.render = function() {
-    // this.validation();
-  }
-
   this.addEvent = function(callback) {
     this.target.addEventListener('keyup', (event) => {
       const ENTERKEY = 13;
       if(event.keyCode === ENTERKEY) {
         if(typeof callback == 'function') {
-          console.log("keyup")
           callback(event.target.value);
+          this.target.value = '';
         }
       }
     });
   }
-
-  this.removeEvent = function() {
-
-  }
-
-  this.setState = function(nextData) {
-    this.data = nextData;
-    this.render();
-  }
-
-  this.render();
 }
 
 export default TodoInput;
