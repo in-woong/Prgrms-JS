56e441d4133ab6d8aaf474469bd300b48749dd90
mc2918 <tlsdud1375@gmail.com>
2021-02-05 01:39:33 +0900

    Add todoCount component

--- mission2/10th/shinyoungkim/src/App.js
+++ mission2/10th/shinyoungkim/src/App.js
@@ -1,16 +1,18 @@
 import TodoList from './components/TodoList.js'
 import TodoInput from './components/TodoInput.js'
+import TodoCount from './components/TodoCount.js'
 
 export default function App( $app, initialState ) { 
 
   this.state = initialState
   
   this.init = () => {
-    this.todoInput = new TodoInput({$app, onAddTodoItem})
-    this.todoList = new TodoList({$app, initialState: this.state})
+    this.todoCount = new TodoCount({ $app, initialState })
+    this.todoInput = new TodoInput({ $app, onChange })
+    this.todoList = new TodoList({ $app, initialState, onRemove, onToggle })
   }
 
-  const onAddTodoItem = (text) => {
+  const onChange = (text) => {
     const nextSate = [
       ...this.state, 
       {
@@ -18,13 +20,30 @@ export default function App( $app, initialState ) {
         isCompleted: false
       }
     ]
+
     this.setState(nextSate)
+  }
+
+  const onRemove = (index) => {
+    this.state.splice(index, 1)
+    this.setState(this.state)
+  }
+
+  const onToggle = (index) => {
+    const nextState = [...this.state]
+    nextState[index] = {
+      text: nextState[index].text,
+      isCompleted: !nextState[index].isCompleted
+    }
+
+    this.setState(nextState)
   }
 
 
   this.setState = (nextData) => {
     this.state = nextData
     this.todoList.setState(this.state)
+    this.todoCount.setState(this.state)
   }
 
   this.init()
