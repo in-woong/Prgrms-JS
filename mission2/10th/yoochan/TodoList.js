712d824e6c6eaa9ec01991772c6eacbe84f5bfa2
cychann <skcbdlhc129@naver.com>
2021-02-04 09:47:09 +0900

    change template

--- mission2/10th/yoochan/TodoList.js
+++ mission2/10th/yoochan/TodoList.js
@@ -1,43 +1,18 @@
-function TodoList(dataList, targetId) {
-    this.state = dataList
 
-    this.validation = (state) => {
-        if(!new.target) {
-        throw new Error("no use new keword");
-        }
-
-        errorTest(this.state)
+function TodoList($target, data) {
+    this.$target = $target
+    this.data = data
+  
+    this.render = function() {
+      this.$target.innerHTML = this.data
+        .map(todo => `<div>${todo.text}</div>`)
+        .join('')
     }
-
-    this.render = () => {
-        const todoList = document.querySelector(targetId);
-        todoList.innerHTML = this.state.map(({ text, isCompleted }) => (isCompleted ? `<div>${text}</div>` : `<div><s>${text}</s></div>`)).join('')
+  
+    this.setState = function(nextData) {
+      this.data = nextData
+      this.render()
     }
-
-    this.setState = (nextState) => {
-        errorTest(nextState)
-        this.state = nextState
-        this.render()
-    }
-
-    this.validation();
-    this.render();
-    }
-
-function errorTest(data) {
-    if(data === null || data === undefined) {
-        throw new Error("Data is empty");
-    }
-    if(!Array.isArray(data)) {
-        throw new Error("Data is not array");
-    }
-
-        data.forEach((value) => {
-        if (typeof value.text !== 'string'){
-            throw new Error("The content of the data is strange");
-        }
-        if(value == null || value == undefined) {
-            throw new Error("data value is empty");
-        }
-        })
-    }
+  
+    this.render()
+  }
--- mission2/10th/yoochan/index.js
+++ mission2/10th/yoochan/index.js
@@ -1,34 +1,12 @@
-const todoList = [
-{
-    text: 'JS 공부하기',
-    isCompleted: true
-},
-{
-    text: 'JS 복습하기',
-    isCompleted: false
-}
-]
-const exerciseList = [
-{
-    text: '일찍 일어나기',
-    isCompleted: true
-},
-{
-    text: '운동하기',
-    isCompleted: true
-}
-]
-const playList = [
-{
-    text: '영화 보기',
-    isCompleted: false
-},
-{
-    text: '롤하기',
-    isCompleted: false
-}
-]
 
-const todo1 = new TodoList(todoList, '#todo-list');
-const todo2 = new TodoList(exerciseList, '#exercise-list');
-const todo3 = new TodoList(playList, '#play-list');
+var data = [
+    {
+      text: 'JS 공부하기',
+    },
+    {
+      text: 'JS 복습하기',
+    },
+  ]
+  
+  const $target = document.querySelector('#todo-list')
+  const todoList = new TodoList($target, data)
