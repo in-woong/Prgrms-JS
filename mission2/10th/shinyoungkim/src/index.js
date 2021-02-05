0b90644c10058a5058bff09e3efded5038e32cfb
mc2918 <tlsdud1375@gmail.com>
2021-02-05 17:18:01 +0900

    Remove initial data

--- mission2/10th/shinyoungkim/src/index.js
+++ mission2/10th/shinyoungkim/src/index.js
@@ -1,16 +1,5 @@
 import App from './App.js'
 
-const initialState = [
-  {
-    text: '',
-    isCompleted: false
-  },
-  {
-    text: 'data2',
-    isCompleted: true
-  }
-]
-
 const $app = document.querySelector('#app')
 
-new App($app, initialState)
+new App({ $app })
