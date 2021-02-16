02a0e29b480abd5cb6bf022d90fa41e534c92602
kmujm <pmini09@kookmin.ac.kr>
2021-02-16 23:38:24 +0900

    add 버튼 추가

--- mission2/10th/jungminPark/TodoInput.js
+++ mission2/10th/jungminPark/TodoInput.js
@@ -4,7 +4,11 @@ function TodoInput({$app, onTodoInput}){
     const $todoInput = document.createElement('input')
     $todoInput.placeholder = 'Enter the work!'
 
+    const $addButton = document.createElement('button')
+    $addButton.textContent = 'ADD'
+    
         $app.appendChild($todoInput)
+        $app.appendChild($addButton)
 
         $todoInput.addEventListener('keydown', (e) => {
             if(e.keyCode === ENTER_KEY_CODE){
