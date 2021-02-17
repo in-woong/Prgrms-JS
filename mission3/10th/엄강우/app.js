46e7d832fff399f0baf1646795815a6a8fa959ef
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2021-02-17 20:36:23 +0900

    [Mission 3] bonus - make search history

--- mission3/10th/엄강우/app.js
+++ mission3/10th/엄강우/app.js
@@ -1,13 +1,15 @@
 import SearchInput from "./searchInput.js";
 import SearchResult from "./searchResult.js";
+import SearchHistory from "./searchHistory.js";
 
 export default function App($app, initialState) {
   this.$app = $app
   
   this.state = initialState
   
-  this.setState = (newData) => {
-    this.state = newData
+  this.setState = (newData, keyword) => {
+    this.state.imageData = newData
+    this.state.searchHistory.push(keyword)
     this.render()
   }
 
@@ -19,18 +21,19 @@ export default function App($app, initialState) {
 
   const handleInput = async (event) => {
     const newData = await this.fetchJjalImage(event.target.value)
-    console.log(newData)
-    this.setState(newData)
+    this.setState(newData, event.target.value)
   }
 
   
   this.render = () => {    
     searchInput.render()
-    searchResult.render(this.state)
+    searchResult.render(this.state.imageData)
+    searchHistory.render(this.state.searchHistory)
   }
 
   const searchInput = new SearchInput(document.querySelector("#search-keyword"), handleInput);
-  const searchResult = new SearchResult(document.querySelector("#search-result"), this.state);
+  const searchResult = new SearchResult(document.querySelector("#search-result"));
+  const searchHistory = new SearchHistory(document.querySelector("#search-history"));
 
   this.render()
 }
--- mission3/10th/엄강우/searchResult.js
+++ mission3/10th/엄강우/searchResult.js
@@ -2,6 +2,6 @@ export default function SearchResult($searchResult) {
   this.$searchResult = $searchResult
   
   this.render = (data) => {
-    this.$searchResult.innerHTML = data.map((image) => `<li><<image src="${image.imageUrl}"><image></li>`).join("")
+    this.$searchResult.innerHTML = data.map((image) => `<li><image src="${image.imageUrl}"><image></li>`).join("")
   }
 }
