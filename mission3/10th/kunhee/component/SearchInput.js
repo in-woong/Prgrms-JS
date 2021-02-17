d49048c2e886f7b04a9dc896bd126811a12a038a
shine784 <shine7884@gmail.com>
2021-02-18 00:19:09 +0900

    [feat] test debounce

--- mission3/10th/kunhee/component/SearchInput.js
+++ mission3/10th/kunhee/component/SearchInput.js
@@ -1,11 +1,4 @@
 import debounce from '../debounce.js'
-let timer = null;
-const debounce2 = (func, wait) => {
-  if (timer) {
-    clearTimeout(timer)
-  }
-  timer = setTimeout(func, wait)
-}
 export default function SearchInput({$sec01,initialState,onSearch}) {
     this.state = initialState;
     $sec01.innerHTML = `<input id="search-keyword" /><div id="search-result"></div>`
