6af1b786dc7ad2a6fffc562c7c2ace449da5f1d4
Inkyu Kim <inkyu0103@gmail.com>
2021-05-26 22:45:17 +0900

    [Mission 2] 10기 김인규  필수 구현사항 (#75) , 보너스 구현사항 ( #76,#77,#81,#90,#91) (#506)
    * [Mission 2] 10기 김인규 #75
    
    #75
    
    * [10기] 김인규 필수 구현사항 (#75) , 보너스 구현사항(#76,#77,#81,#90,#91)
    
    #75
    #76
    #81
    #90
    #91

---
+++ mission2/10th/[10기] 김인규/index.js
@@ -0,0 +1,9 @@
+const ERR_MSG ={
+    NO_NEW_KEYWORD : "new 키워드가 없습니다",
+    NOT_ARRAY : "배열이 아닙니다.",
+    NO_TEXT_PROP : "text항목이 존재하지 않습니다.",
+    NO_ISCOMPLETE_PROP : "isCompleted 항목이 존재하지 않습니다.",
+    TEXT_TYPE_ERR : "text의 값이 string이 아닙니다.",
+    INPUT_ONLY_BLANK : "입력된 내용이 없습니다."
+}
+new App(document.querySelector("#App"))
