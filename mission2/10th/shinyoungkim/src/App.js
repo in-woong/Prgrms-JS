511dab300818283aac37fac351ce7cb98fa024b2
SHIN-YOUNG KIM <tlsdud1375@gmail.com>
2021-05-26 22:45:29 +0900

    [Mission2] 김신영 (#509)
    * Add mission1 index.html
    
    * Add TodoList checklist
    
    * Add form input and root app
    
    * Move validation code index.html from validation.js
    
    * Remove Unnecessary code in checkTypes
    
    * Add checkTarget Error
    
    * Fix @param type
    
    * Remove input form and Update html structure
    
    * Test initial data
    
    * Add todoInput, todoList and onAddTodoItem
    
    * Update checkValidate in TodoList
    
    * Add TodoInput component
    
    * Add list style reset
    
    * Fix css link path
    
    * Add checkInputValue When use to check input null, empty value
    
    * Refactor addEventListener for simplified code and  Add handleEventInput function
    
    * Add render - conditional sentence of state length
    
    * Fix render - conditional sentence false value
    
    * Add handleClick Event
    
    * Add todo component style
    
    * Add remove vlaue whiteSpace
    
    * Add todoCount component
    
    * Add alert to checkInputValue
    
    * Add todoCount component
    
    * Add localStorage data
    
    * Add todo list Checklist
    
    * Add todo-list item style
    
    * Add code convention
    
    * Remove initial data
    
    * Add code convention
    
    * Fix checkbox status
    
    * Add remove component
    
    * Add localStorage

---
+++ mission2/10th/shinyoungkim/src/App.js
@@ -0,0 +1,69 @@
+import TodoList from './components/TodoList.js'
+import TodoInput from './components/TodoInput.js'
+import TodoCount from './components/TodoCount.js'
+import TodoRemove from './components/TodoRemove.js'
+
+import { getItem , setItem } from './utils/localStorage.js'
+
+const TODO_STORAGE_KEY = 'todolocalStorege'
+
+export default function App({$app}) { 
+  
+  this.init = () => {
+
+    this.state = getItem(TODO_STORAGE_KEY) || [] 
+
+    this.todoCount = new TodoCount({ $app, initialState: this.state })
+    this.todoInput = new TodoInput({ $app, onChange })
+    this.todoRemove = new TodoRemove({ $app, onRemove })
+    this.todoList = new TodoList({ $app, initialState: this.state , onRemove, onToggle })
+
+    this.removeAllButton()
+  }
+
+  const onChange = (text) => {
+    const nextSate = [
+      ...this.state, 
+      {
+        text: text,
+        isCompleted: false
+      }
+    ]
+
+    this.setState(nextSate)
+  }
+
+  const onRemove = (index) => {
+    this.state.splice(index, 1)
+    this.setState(this.state)
+  }
+
+  const onToggle = (index) => {
+    const nextState = [...this.state]
+    nextState[index] = {
+      text: nextState[index].text,
+      isCompleted: !nextState[index].isCompleted
+    }
+
+    this.setState(nextState)
+  }
+
+  this.removeAllButton = () => {
+    const removeAllBtn = this.todoRemove.$target
+
+    removeAllBtn.addEventListener('remove-all', event => {
+      localStorage.clear()
+      this.state = []
+      this.todoList.setState(this.state)
+    })
+  }
+
+  this.setState = (nextState) => {
+    this.state = nextState
+    setItem(TODO_STORAGE_KEY, nextState)
+    this.todoList.setState(this.state)
+    this.todoCount.setState(this.state)
+  }
+
+  this.init()
+}
