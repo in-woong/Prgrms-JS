2d9ff7aca234e519557632f379a2db80254218df
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2021-05-26 21:32:10 +0900

    [Mission 2] 엄강우 - 필수 구현 사항 및 보너스 구현 사항 (#497)
    * 필수구현사항 (#461)
    
    * [Misson 2] mission 1 - 복습
    
    * Update index.html
    
    * [Mission 2] mission 1 - fix
    
    * [Mission 2] 필수구현사항
    
    * [Misson 2] 필수구현사항 - 피드백 정독 후 개선
    
    * [Mission 2] Bonus - Input 컴포넌트 분리하기
    
    * [Misson 2] Bonus - input 컴포넌트화 하기
    
    * [Misson 2] bonus - input 컴포넌트화 fix
    
    * [Misson 2] 보너스 미션 구현
    
    Co-authored-by: Boeun05 <71836751+Boeun05@users.noreply.github.com>

---
+++ mission2/10th/엄강우/src/Components/todoList.js
@@ -0,0 +1,19 @@
+export default function TodoList($target, todos) { 
+  try {
+    if(!new.target) {
+      throw new error("you need new keyword")
+    }
+    this.$target = $target
+    this.render = () => {
+      this.$target.innerHTML = todos.map(({text, isCompleted}, index) => isCompleted ? 
+      `<div><s><span id="todo" data-index="${index}">${text}</span></s><button id="todo-button" data-index="${index}">지우기</button></div>` 
+      : `<div><span id="todo" data-index="${index}">${text}</span><button id="todo-button" data-index="${index}">지우기</button></div>`).join('')     
+    }
+    this.render()
+  } catch (e) {
+    alert(e.message)
+  }
+  
+}
+
+
