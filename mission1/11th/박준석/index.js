efc69c06cf1eed368997ad23d5504adc89698f7c
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-05-27 22:04:42 +0900

    [mission1] Suppplier 보너스 구현사항 - isCompleted 처리

---
+++ mission1/11th/박준석/index.js
@@ -0,0 +1,60 @@
+import { addNewDiv } from "./utils.js"
+import { TodoList } from "./mission1.js"
+
+const data = [
+    {
+        text: 'JS 공부하기',
+        isCompleted: false
+    },
+    {
+        text: 'JS 복습하기',
+        isCompleted: false
+    },
+    {
+        text : "                   JS 과제하기    ",
+        isCompleted: true
+    }
+]
+
+const data2 = [
+    {
+        text: '게임하기',
+        isCompleted: true
+    },
+    {
+        text: '축구하기',
+        isCompleted: false
+    },
+    {
+        text : "독서하기",
+        isCompleted: true
+    }
+]
+
+const data3 = [
+    {
+        text: '밥',
+        isCompleted: false
+    },
+    {
+        text: '라면',
+        isCompleted: true
+    },
+    {
+        text : "치킨",
+        isCompleted: false
+    }
+]
+
+
+addNewDiv("todo-list");
+addNewDiv("play-list");
+addNewDiv("food-list");
+
+let test = new TodoList(data, "#todo-list");
+let test2 = new TodoList(data2, "#play-list");
+let test3 = new TodoList(data3, "#food-list");
+
+test.render();
+test2.render();
+test3.render();
--- mission1/11th/박준석/mission1.js
+++ mission1/11th/박준석/mission1.js
@@ -1,43 +1,52 @@
 import { $, TEXTUNDEFINED, DATAISNOTARY, VALISNOTOBJ, ISNOTNEW,
-    isAvilable, addNewDiv } from "./utils.js"
+    isAvilable} from "./utils.js"
 
 const data = [
     {
-        text: 'JS 공부하기'
+        text: 'JS 공부하기',
+        isCompleted: false
     },
     {
-        text: 'JS 복습하기'
+        text: 'JS 복습하기',
+        isCompleted: false
     },
     {
-        text : "                   d    "
+        text : "                   d    ",
+        isCompleted: true
     }
 ]
 
 const data2 = [
     {
-        text: '게임하기'
+        text: '게임하기',
+        isCompleted: true
     },
     {
-        text: '축구하기'
+        text: '축구하기',
+        isCompleted: false
     },
     {
-        text : "독서하기"
+        text : "독서하기",
+        isCompleted: true
     }
 ]
 
 const data3 = [
     {
-        text: '밥'
+        text: '밥',
+        isCompleted: false
     },
     {
-        text: '라면'
+        text: '라면',
+        isCompleted: true
     },
     {
-        text : "치킨"
+        text : "치킨",
+        isCompleted: false
     }
 ]
 
-function TodoList(data, id) {
+export function TodoList(data, id) {
     if (!(this instanceof TodoList))
         throw new Error(ISNOTNEW);
 
@@ -57,7 +66,13 @@ function TodoList(data, id) {
 
     this.render = () => {
         this.state.forEach((val) => {
-            $List.insertAdjacentHTML("beforeend", `<li>${val.text}</li>`)
+            let str;
+            console.log(val.isCompleted);
+            if (val.isCompleted)
+                str = `<li><s>${val.text}</s></li>`;
+            else
+                str = `<li>${val.text}</li>`;
+            $List.insertAdjacentHTML("beforeend", str);
         })
     }
 
@@ -72,15 +87,4 @@ function TodoList(data, id) {
 
 
 
-addNewDiv("todo-list");
-addNewDiv("play-list");
-addNewDiv("food-list");
-
-let test = new TodoList(data, "#todo-list");
-let test2 = new TodoList(data2, "#play-list");
-let test3 = new TodoList(data3, "#food-list");
-
-test.render();
-test2.render();
-test3.render();
 
