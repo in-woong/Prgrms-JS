1f67420e2ac2aa51c4486ca7a26b4e97814b5e1f
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-05-27 20:55:34 +0900

    [mission1] Suppplier 보너스 구현사항 - 인스턴스 생성 체크

--- mission1/11th/박준석/mission1.js
+++ mission1/11th/박준석/mission1.js
@@ -1,4 +1,5 @@
-import { $ } from "./utils.js"
+import { $, TEXTUNDEFINED, DATAISNOTARY, VALISNOTOBJ, ISNOTNEW,
+            isAvilable } from "./utils.js"
 
 const $todoList = $("#todo-list");
 
@@ -9,17 +10,39 @@ const data = [
     {
       text: 'JS 복습하기'
     },
+    {
+      text : "                   d    "
+    }
 ]
 
+
+
 function TodoList(data) {
+    if (!(this instanceof TodoList))
+        throw new Error(ISNOTNEW);
+
+    this.check = (data) => {
+        if (!Array.isArray(data))
+            throw new Error(DATAISNOTARY)
+        data.forEach((val) => {
+            if (val.constructor !== Object)
+                throw new Error(VALISNOTOBJ)
+            if (isAvilable(val.text))
+                throw new Error(TEXTUNDEFINED)
+        })
+    }
 
     this.render = () => {
         data.forEach((val) => {
-            $todoList.insertAdjacentHTML("beforeend", `<div> - ${val.text}</div>`)
+            $todoList.insertAdjacentHTML("beforeend", `<li>${val.text}</li>`)
         })
     }
+
+    this.check(data);
 }
 
 let test = new TodoList(data);
 
+
 test.render();
+
--- mission1/11th/박준석/utils.js
+++ mission1/11th/박준석/utils.js
@@ -1,1 +1,22 @@
 export const $ = (ele, dom = document) => dom.querySelector(ele);
+
+export const ISNOTNEW = "new 키워드로 생성된 인스턴스가 아닙니다.";
+export const DATAISNOTARY = "데이터의 형태가 배열이 아닙니다.";
+export const VALISNOTOBJ = "데이터의 값 중 객체가 아닌 값이 있습니다.";
+export const TEXTUNDEFINED = "데이터의 text들 중 적합하지 않은 값이 있습니다.";
+
+const isOnlySpace = (str) => {
+    for (let c of str){
+        if (c != ' ')
+            return (false)
+    }
+    return (true);
+}  
+
+export const isAvilable = (str) => {
+    if (str == undefined || isOnlySpace(str) || typeof str !== 'string')
+        return (true);
+    else
+        return (false);
+}
+
