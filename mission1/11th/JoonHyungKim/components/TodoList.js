13cc9d5024c15c08c098765aca487b87ca927ba2
JoonHyungKim <asy8000@naver.com>
2021-05-28 00:25:32 +0000

    [feat] Mission01 보너스 구현사항 에러 처리 구현(#50)

--- mission1/11th/JoonHyungKim/components/TodoList.js
+++ mission1/11th/JoonHyungKim/components/TodoList.js
@@ -1,6 +1,13 @@
+const throwError = (msg) => { throw new Error(msg) };
+
 export class TodoList {
     constructor(el, data) {
         this.el = el;
+
+        !data && throwError('빈 데이터입니다.');
+        !Array.isArray && throwError('배열 값만 사용할 수 있습니다.');
+        !data.every(e => e.text && typeof e.text === 'string') && throwError('잘못된 데이터입니다.');
+
         this.data = data;
     }
 
