b71c1d518f8850b6f8efa4d7f7659759e330e062
JoonHyungKim <asy8000@naver.com>
2021-05-28 00:27:50 +0000

    [feat] Mission01 보너스 구현 사항 todolist 2개 추가(#57)

--- mission1/11th/JoonHyungKim/index.js
+++ mission1/11th/JoonHyungKim/index.js
@@ -10,6 +10,32 @@ const data = [{
     },
 ]
 
+const today_todo = [{
+        text: '출근'
+    },
+    {
+        text: '일하기'
+    },
+    {
+        text: '퇴근하기'
+    }
+]
+
+const tomorrow_todo = [{
+        text: '늦잠자기'
+    },
+    {
+        text: '방청소하기'
+    },
+    {
+        text: '출근 안하기'
+    }
+]
+
 const todoList = new TodoList(document.getElementById('todo-list'), data);
+const todayTodoList = new TodoList(document.getElementById('todo-list-2'), today_todo);
+const tomorrowTodoList = new TodoList(document.getElementById('todo-list-3'), tomorrow_todo);
 
 todoList.render();
+todayTodoList.render();
+tomorrowTodoList.render();
