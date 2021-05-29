d0bdb26c80599037cd7ac8c553e8259eea135180
younoah <kr_uno_10@naver.com>
2021-05-29 16:24:31 +0900

    feat : 데이터 유효성 검사 함수 구현

---
+++ mission1/11th/정윤호/validators/checkTodoItemData.js
@@ -0,0 +1,31 @@
+'use strict';
+
+export const checkTodoItemData = (data) => {
+	if (data === null) {
+		throw new Error('todo item data is null');
+	}
+
+	if (data === undefined) {
+		throw new Error('todo item data is undefined');
+	}
+
+	if (!Array.isArray(data)) {
+		throw new Error('todo item data is not Array');
+	}
+
+	data.forEach(todoItem => {
+		if (typeof todoItem !== 'object') {
+			throw new Error('todo item is not object');
+		}
+
+		if (!todoItem.hasOwnProperty('text')) {
+			throw new Error('todo item has not "text" property');
+		}
+
+		if (!todoItem.hasOwnProperty('isCompleted')) {
+			throw new Error('todo item has not "isCompleted" property');
+		}
+	});
+}
+
+export default checkTodoItemData;
