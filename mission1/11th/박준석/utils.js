b1b9248e00b0d5aa705ca9fe5be5d85d722577fb
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-05-29 18:10:33 +0900

    [mission1] 박준석 - ERROR_MASSAGE minor fix

--- mission1/11th/박준석/utils.js
+++ mission1/11th/박준석/utils.js
@@ -2,9 +2,9 @@ export const $ = (ele, dom = document) => dom.querySelector(ele);
 
 export const ERROR_MSSAGE = {
     IS_NOT_NEW : "new 키워드로 생성된 인스턴스가 아닙니다.",
-    DATA_IS_NOT_ARRAY = "데이터의 형태가 배열이 아닙니다.",
-    VALUE_IS_NOT_OBJECT = "데이터의 값 중 객체가 아닌 값이 있습니다.",
-    TEXT_UNDEFINED = "데이터의 text들 중 적합하지 않은 값이 있습니다."
+    DATA_IS_NOT_ARRAY : "데이터의 형태가 배열이 아닙니다.",
+    VALUE_IS_NOT_OBJECT : "데이터의 값 중 객체가 아닌 값이 있습니다.",
+    TEXT_UNDEFINED : "데이터의 text들 중 적합하지 않은 값이 있습니다."
 }
 
 const TodoListDivDOM = (id) => `
