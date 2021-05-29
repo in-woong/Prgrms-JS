350a02de23830e6b48817cd64c84d291c8954d2c
younoah <kr_uno_10@naver.com>
2021-05-29 18:06:50 +0900

    refactor : todo item 템플릿 함수 간결하게 수정

--- mission1/11th/정윤호/layouts/todoItemTemplate.js
+++ mission1/11th/정윤호/layouts/todoItemTemplate.js
@@ -1,13 +1,7 @@
 'use strict';
 
-export const todoItemTemplate = (todoData) => {
-	const {text, isCompleted} = todoData;
-	const template = {
-		'completed': `<s><p>${text}</p></s>`,
-		'unCompleted': `<p>${text}</p>`,
-	}
-
-	return template[isCompleted ? 'completed' : 'unCompleted'];
+export const todoItemTemplate = ({text, isCompleted}) => {
+	return isCompleted ? `<s><p>${text}</p></s>` : `<p>${text}</p>`;
 };
 
 export default todoItemTemplate;
