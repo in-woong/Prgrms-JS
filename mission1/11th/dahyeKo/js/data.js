f7cc6aa6d0b02f4a6e7ef8a07bceb261cb2c336b
hyeda1103 <dalgona92@gmail.com>
2021-05-29 21:25:44 +0900

    [Mission1] 고다혜 - 원본 데이터(할일목록) 파일

--- mission1/11th/dahyeKo/js/data.js
+++ mission1/11th/dahyeKo/js/data.js
@@ -9,31 +9,5 @@ let data = [
   }
 ];
 
-let data1 = [
-  { 
-    text: '양말접기',
-    isCompleted: true
-  },
-  {
-    text: '돌돌이로 머리카락 정리하기',
-    isCompleted: false
-  }
-];
 
-let data2 = [
-  { 
-    text: '명상하기',
-    isCompleted: true
-  },
-  { 
-    text: '요가하기',
-    isCompleted: true
-  },
-  {
-    text: '산책하기',
-    isCompleted: false
-  }
-];
-
-
-export { data, data1, data2 }
+export { data }
