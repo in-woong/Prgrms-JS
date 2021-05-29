98d5075f8fb5a6557df3fb940b73a371a8567fb7
hyeda1103 <dalgona92@gmail.com>
2021-05-29 21:03:06 +0900

    [Mission1] 고다혜 - TodoList 데이터

---
+++ mission1/11th/dahyeKo/js/data.js
@@ -0,0 +1,39 @@
+let data = [
+  { 
+    text: '코딩하기',
+    isCompleted: true
+  },
+  {
+    text: '집안 청소하기',
+    isCompleted: false
+  }
+];
+
+let data1 = [
+  { 
+    text: '양말접기',
+    isCompleted: true
+  },
+  {
+    text: '돌돌이로 머리카락 정리하기',
+    isCompleted: false
+  }
+];
+
+let data2 = [
+  { 
+    text: '명상하기',
+    isCompleted: true
+  },
+  { 
+    text: '요가하기',
+    isCompleted: true
+  },
+  {
+    text: '산책하기',
+    isCompleted: false
+  }
+];
+
+
+export { data, data1, data2 }
