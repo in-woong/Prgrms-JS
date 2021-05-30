0e7f0fb7c65cd32ebdc77f441fc50a7b3e48907c
hyeda1103 <dalgona92@gmail.com>
2021-05-30 18:50:40 +0900

    [Mission1] 고다혜 - 테스트용 데이터 및 EOL 추가

--- mission1/11th/dahyeKo/js/data.js
+++ mission1/11th/dahyeKo/js/data.js
@@ -1,39 +1,45 @@
-let data = [
-  { 
-    text: '코딩하기',
-    isCompleted: true
-  },
-  {
-    text: '집안 청소하기',
-    isCompleted: false
-  }
-];
+const data1 = [
+    {
+        text: "코딩하기",
+        isCompleted: true,
+    },
+    {
+        text: "집안 청소하기",
+        isCompleted: false,
+    }]
 
-let data1 = [
-  { 
-    text: '양말접기',
-    isCompleted: true
-  },
-  {
-    text: '돌돌이로 머리카락 정리하기',
-    isCompleted: false
-  }
-];
+const data2 = [
+    {
+        text: "양말접기",
+        isCompleted: true,
+    },
+    {
+        text: "돌돌이로 머리카락 정리하기",
+        isCompleted: false,
+    }]
 
-let data2 = [
-  { 
-    text: '명상하기',
-    isCompleted: true
-  },
-  { 
-    text: '요가하기',
-    isCompleted: true
-  },
-  {
-    text: '산책하기',
-    isCompleted: false
-  }
-];
+const data3 = [
+    {
+        text: "명상하기",
+        isCompleted: true,
+    },
+    {
+        text: "요가하기",
+        isCompleted: true,
+    },
+    {
+        text: "산책하기",
+        isCompleted: false,
+    }]
 
+const data4 = [
+    {
+        text: "요리하기",
+        isCompleted: true,
+    },
+    {
+        text: "춤추기",
+        isCompleted: true,
+    }]
 
-export { data, data1, data2 }
+export { data1, data2, data3, data4 }
