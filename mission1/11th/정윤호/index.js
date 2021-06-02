96caa436283416b2634a7f35227d5395c3b2a849
younoah <kr_uno_10@naver.com>
2021-06-02 11:41:35 +0900

    refactor : 모든 js파일에 prettier 적용(re)

--- mission1/11th/정윤호/index.js
+++ mission1/11th/정윤호/index.js
@@ -1,56 +1,58 @@
-import {$} from '../utils/dom.js'
+import { $ } from '../utils/dom.js'
 import TodoList from './components/todoList.js'
 
 const data1 = [
-	{
-		text: 'JS 공부하기',
-		isCompleted: true,
-	},
-	{
-		text: 'JS 복습하기',
-		isCompleted: false,
-	},
-];
-
-const data2 = [
-	{
-		text: '머리 흰색으로 염색하기',
-		isCompleted: true,
-	},
-	{
-		text: '기타 배우기',
-		isCompleted: true,
-	},
-];
-
-const data3 = [
-	{
-		text: '안드로이화 하기',
-		isCompleted: true,
-	},
-	{
-		text: '굽신굽신 하기',
-		isCompleted: true,
-	},
-	{
-		text: '팀장님 존경하기',
-		isCompleted: false,
-	},
-];
-
-const newData = [
-	{
-		text: '수정하기',
-		isCompleted: false,
-	},
+  {
+    text: 'JS 공부하기',
+    isCompleted: true,
+  },
+  {
+    text: 'JS 복습하기',
+    isCompleted: false,
+  },
 ]
 
-const todoList1 = new TodoList(data1, $('#todo-list1'));
-const todoList2 = new TodoList(data2, $('#todo-list2'));
-const todoList3 = new TodoList(data3, $('#todo-list3'));
+const data2 = [
+  {
+    text: '머리 흰색으로 염색하기',
+    isCompleted: true,
+  },
+  {
+    text: '기타 배우기',
+    isCompleted: true,
+  },
+]
 
-todoList1.render();
-todoList2.render();
-todoList3.render();
+const data3 = [
+  {
+    text: '안드로이화 하기',
+    isCompleted: true,
+  },
+  {
+    text: '굽신굽신 하기',
+    isCompleted: true,
+  },
+  {
+    text: '팀장님 존경하기',
+    isCompleted: false,
+  },
+]
 
-setTimeout(() => {todoList3.setState(newData)}, 3000);
+const newData = [
+  {
+    text: '수정하기',
+    isCompleted: false,
+  },
+]
+
+const todoList1 = new TodoList(data1, $('#todo-list1'))
+const todoList2 = new TodoList(data2, $('#todo-list2'))
+const todoList3 = new TodoList(data3, $('#todo-list3'))
+
+todoList1.render()
+todoList2.render()
+todoList3.render()
+
+setTimeout(() => {
+  todoList3.setState(newData)
+}, 3000)
--- mission1/11th/정윤호/validators/checkTodoItemData.js
+++ mission1/11th/정윤호/validators/checkTodoItemData.js
@@ -1,31 +1,31 @@
 import { NULL, UNDEFINED, NOT_ARRAY, HAS_NOT_OBJECT, HAS_NOT_TEXT, HAS_NOT_ISCOMPLETED } from '../utils/messages.js'
 
 const checkTodoItemData = (data) => {
-	if (data === null) {
-		throw new Error(NULL);
-	}
+  if (data === null) {
+    throw new Error(NULL)
+  }
 
-	if (data === undefined) {
-		throw new Error(UNDEFINED);
-	}
+  if (data === undefined) {
+    throw new Error(UNDEFINED)
+  }
 
-	if (!Array.isArray(data)) {
-		throw new Error(NOT_ARRAY);
-	}
+  if (!Array.isArray(data)) {
+    throw new Error(NOT_ARRAY)
+  }
 
-	data.forEach(todoItem => {
-		if (typeof todoItem !== 'object') {
-			throw new Error(HAS_NOT_OBJECT);
-		}
+  data.forEach((todoItem) => {
+    if (typeof todoItem !== 'object') {
+      throw new Error(HAS_NOT_OBJECT)
+    }
 
-		if (!todoItem.hasOwnProperty('text')) {
-			throw new Error(HAS_NOT_TEXT);
-		}
+    if (!todoItem.hasOwnProperty('text')) {
+      throw new Error(HAS_NOT_TEXT)
+    }
 
-		if (!todoItem.hasOwnProperty('isCompleted')) {
-			throw new Error(HAS_NOT_ISCOMPLETED);
-		}
-	});
+    if (!todoItem.hasOwnProperty('isCompleted')) {
+      throw new Error(HAS_NOT_ISCOMPLETED)
+    }
+  })
 }
 
-export default checkTodoItemData;
+export default checkTodoItemData
