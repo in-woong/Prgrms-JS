19927a4f5172bf2a1f9cb8b159a23b0d4e90acd1
younoah <kr_uno_10@naver.com>
2021-06-03 12:18:22 +0900

    refactor : 에러 메세지 객체 상수화

--- mission1/11th/정윤호/utils/messages.js
+++ mission1/11th/정윤호/utils/messages.js
@@ -1,6 +1,14 @@
 const dataFormErrorMessage = (keyword) => `todo item data is not ${keyword}`
 const dataProperyErrorMessage = (keyword) => `todo item has not ${keyword}`
 
+export const ERROR_MESSAGE = Object.freeze({
+  NULL: dataFormErrorMessage('exist (null)'),
+  UNDEFINED: dataFormErrorMessage('exist (undefined)'),
+  NOT_ARRAY: dataFormErrorMessage('Array'),
+  HAS_NOT_OBJECT: dataProperyErrorMessage('Object'),
+  HAS_NOT_TEXT: dataProperyErrorMessage('"text"'),
+  HAS_NOT_ISCOMPLETED: dataProperyErrorMessage('"isCompleted"'),
+})
 export const NULL = dataFormErrorMessage('exist (null)')
 export const UNDEFINED = dataFormErrorMessage('exist (undefined)')
 export const NOT_ARRAY = dataFormErrorMessage('Array')
