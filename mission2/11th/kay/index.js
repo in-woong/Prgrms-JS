fe2c3b32aff6257d4a3df3a39fe0131c58327e48
DESKTOP-6VLFG9G\User <asterisking@kbsmedia.co.kr>
2021-06-04 14:27:35 +0900

    [Mission2] 케이 필수구현사항

--- mission2/11th/kay/index.js
+++ mission2/11th/kay/index.js
@@ -1,4 +1,4 @@
-import TodoList from './TodoList.js';
+// import TodoList from './TodoList.js';
 
 const data = [
     {
@@ -45,7 +45,7 @@ const data3 = [
 const data4 = [
     {
     text: '잠이나 퍼자기',
-    isCompleted: true
+    isCompleted: false
     }
 ]
 
@@ -56,10 +56,8 @@ const todoList3 = new TodoList(data3, '#todo-list3');
 todoList.render();
 todoList2.render();
 todoList3.render();
-todoList.setState(data4);
+setTimeout(() => {todoList.setState(data4)}, 2000);
 
-const newTxt = document.querySelector('#inpt').value;
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', function() {
-    data4.push(`{text: ${newTxt}}`);
-})
+
+
+
