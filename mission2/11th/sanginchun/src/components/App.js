28c91b10203938b6e6240af2fe5bf79d2103de31
Sang In Chun <sanginchun91@gmail.com>
2021-06-05 01:14:15 +0900

    [Mission 2] 전상인 - App 컴포넌트 state말고 그냥 todoItems로, deleteAll 로직 수정

--- mission2/11th/sanginchun/src/components/App.js
+++ mission2/11th/sanginchun/src/components/App.js
@@ -7,20 +7,18 @@ class App {
   constructor($app) {
     if(!$app) throw new Error('$app이 없습니다')
 
-    this.state = {
-      todoItems: JSON.parse(localStorage.getItem('todoItems')) || [],
-    }
+    this.todoItems = JSON.parse(localStorage.getItem('todoItems')) || []
 
     this.todoList = new TodoList({
       $parent: $app,
-      todoItems: this.state.todoItems,
+      todoItems: this.todoItems,
       onDeleteBtnClick: this.deleteTodoItem.bind(this),
       onTodoTextClick: this.markCompletedTodoItem.bind(this),
     })
 
     this.todoCount = new TodoCount({
       $parent: $app,
-      todoItems: this.state.todoItems,
+      todoItems: this.todoItems,
     })
 
     new TodoInput({ $parent: $app, onSubmit: this.addTodoItem.bind(this) })
@@ -30,20 +28,18 @@ class App {
     $app.addEventListener('removeAll', this.deleteAllTodoItems.bind(this))
   }
 
-  setState(nextState) {
-    if(this.state.todoItems !== nextState.todoItems) {
-      this.validateTodoItems(nextState.todoItems)
-
-      this.setTodoItemsToLocal(nextState.todoItems)
-    }
+  setState(nextTodoItems) {
+    this.validateTodoItems(nextTodoItems)
     
-    this.state = nextState
+    this.todoItems = nextTodoItems
+    this.setTodoItemsToLocal(this.todoItems)
+
     this.render()
   }
 
   render() {
-    this.todoList.setState(this.state.todoItems)
-    this.todoCount.setState(this.state.todoItems)
+    this.todoList.setState(this.todoItems)
+    this.todoCount.setState(this.todoItems)
   }
 
   setTodoItemsToLocal(todoItems) {
@@ -51,27 +47,28 @@ class App {
   }
 
   addTodoItem(text) {
-    this.setState({
-      todoItems: [...this.state.todoItems, { text, isCompleted: false }]
-    })
+    this.setState([...this.todoItems, { text, isCompleted: false }])
   }
 
   deleteTodoItem(index) {
-    const nextTodoItems = [...this.state.todoItems]
+    const nextTodoItems = [...this.todoItems]
     nextTodoItems.splice(index, 1)
 
-    this.setState({ todoItems: nextTodoItems })
+    this.setState(nextTodoItems)
   }
 
   deleteAllTodoItems() {
-    this.setState({ todoItems: [] })
+    if(!this.todoItems.length) return
+
+    if(window.confirm('Todo 리스트 전체를 삭제할까요?'))
+      this.setState([])
   }
 
   markCompletedTodoItem(index) {
-    const nextTodoItems = [...this.state.todoItems]
+    const nextTodoItems = [...this.todoItems]
     nextTodoItems[index].isCompleted = true
 
-    this.setState({ todoItems: nextTodoItems })
+    this.setState(nextTodoItems)
   }
 
   validateTodoItems(todoItems) {
