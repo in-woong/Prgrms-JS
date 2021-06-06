231dd6705b4a6259b1e317149b57c302f1a9353a
hik3562 <36418489+lllilllilllilili@users.noreply.github.com>
2021-06-07 01:17:48 +0900

    [Mission1] 황인규 - 코드 리팩토링
    ;

--- mission1/11th/[11기]황인규/TodoList.js
+++ mission1/11th/[11기]황인규/TodoList.js
@@ -30,85 +30,88 @@ const data = [
       isCompleted: false,
     },
   ]
-
-
-  function validationCheck(data){
-    this.state = data 
-    //[Mission1] 보너스 구현사항 예외 처리 구현
-    if(!this.state)
-      throw new Error('빈 데이터 입니다.')
-    if(!Array.isArray(this.state))
-      throw new Error('Array형태가 아닙니다.')
-    this.state.forEach(function(element){
-        console.log(element)
-        if(typeof element === undefined) 
-            throw new Error('데이터 형식이 잘못되었습니다.')
-        if(typeof element.text !== 'string')
-            throw new Error('데이터 타입이 string이 아닙니다.')
-        if(typeof element.isCompleted !== 'boolean')
-            throw new Error('데이터 타입이 boolean이 아닙니다.')
-    })
-    
-  }
   
   //[Mission1] : TodoList 함수 생성
-  function TodoList(data, selector){
+  function TodoList(data, $app){
     
     this.state = data 
-    this.$elem = document.querySelector(`#${selector}`)
-    validationCheck(this.state)
+    
+    if(!$app){
+      throw new Error('$app이 올바르지 않습니다.');
+    }
+
+    if(!data || !Array.isArray(data)){
+      throw new Error('data가 올바르지 않습니다.');
+    }
+
+    this.$target = document.createElement('div')
+
+    //코드 가독성
+    $app.appendChild(this.$target);
+
+    this.$target.setAttribute('data-component-type', 'TodoList');
+    //this.$target.className = 'TodoList'
+    
+
+
     //[Mission1] 보너스 new 키워드 안 붙이고 함수 실행 시 에러 발생하게 하기 
     if(!new.target){
         throw new Error('new로 객체를 생성해주세요');
     }
   
-    this.render = function(){
-        let str=``
-        try{
-            const todoHtmlString = this.state.reduce((acc, cur, i)=>{
-                if(cur.isCompleted===true){
-                    str+=`<div><s>${cur.text}</s></div>`
-                }else{
-                    str+=`<div>${cur.text}</div>`
-                }
-            },'')
-            this.$elem.innerHTML=str
-        }catch(e){
-            console.log(e);
-        }
+    this.render = () =>{
+          const todoHtmlStr = this.state.map(({text, isCompleted}) =>
+            isCompleted ? `<li><s>${text}</s></li>` : `<li>${text}</li>`
+            //todo=>`<li>${todo.text}</li>`
+            
+            ).join('')
+          
+          this.$target.innerHTML=todoHtmlStr
     }
+    this.render()
 
     //[Mission1] 보너스 구현사항 setState
-    this.setState = function(nextData){
-        validationCheck(newData);
-        if(!new.target){
-            throw new Error('new로 객체를 생성해주세요');
-        }
-        try{
-            this.state = nextData
-            this.render()
-        }catch(e){
-            console.log(e)
-        }
+    this.setState = (nextState) =>{
+        this.state = nextState
+        this.render();
     }
 }
 
 
 
+try{
+  const $app = document.querySelector('#app')
+  const todoList = new TodoList(data, $app);
+  //new TodoList([{text : 'hey'}], $app);
+  //new TodoList(data3, document.querySelector(`#todo-list3`));
+  setTimeout(()=>{
+    todoList.setState([
+      ...data,
+      {
+        text:"first"
+      }
+    ])
+
+    setTimeout(()=>{
+      todoList.setState([
+        ...data,
+        {
+          text:"second"
+        }
+      ])
+    },2000);
+  },2000);
+
+  
+}catch(error){
+  alert(error.message)
+}
 
 
-var todoList = new TodoList(data, 'todo-list');
-var todoList2 = new TodoList(data2, 'todo-list2');
-var todoList3 = new TodoList(data3, 'todo-list3');
-
-todoList.render();
-todoList2.render();
-todoList3.render();
-
-todoList.setState([
-    {
-        text: 'setState',
-        isCompleted: true,
-    }
-])
+// todoList.setState([
+//     {
+//         text: 'setState',
+//         isCompleted: true,
+//     }
+// ])
 
