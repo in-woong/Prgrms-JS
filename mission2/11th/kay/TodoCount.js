fdad1ea1875841655a595a361d5f65ac279d521d
DESKTOP-6VLFG9G\User <asterisking@kbsmedia.co.kr>
2021-06-08 15:26:04 +0900

    [Mission2] 케이 - 필수구현&추가구현

---
+++ mission2/11th/kay/TodoCount.js
@@ -0,0 +1,23 @@
+function TodoCount(selector, data) {
+    this.data = data;
+    this.selector = selector;
+    this.target = document.querySelector(this.selector);
+    this.render = function() {
+      const count = calculateToCount();
+      this.target.innerHTML = `
+        <span>총 갯수: ${count.total}</span>
+        <span>완료 갯수: ${count.completed}</span>
+     `
+    }
+  
+    const calculateToCount = () => {
+      const filteredList = this.data.filter((item) => item.isCompleted);
+  
+      const count = {total:this.data.length, completed:filteredList.length};
+      return count;
+    }
+  
+    this.render()
+  }
+  
+  export default TodoCount
---
+++ mission2/11th/kay/TodoInput.js
@@ -0,0 +1,20 @@
+const ENTERKEY = 13;
+
+function TodoInput(selector) {
+
+  this.selector = selector;
+  this.target = document.querySelector(this.selector);
+
+  this.addEvent = function(callback) {
+    this.target.addEventListener('keyup', (event) => {
+      if(event.keyCode === ENTERKEY) {
+        if(typeof callback == 'function') {
+          callback(event.target.value);
+          this.target.value = '';
+        }
+      }
+    });
+  }
+}
+
+export default TodoInput;
