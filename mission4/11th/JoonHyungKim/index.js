7cf08064c26c29c8c2a34ce17312bd6d5bdd2797
JoonHyungKim <asy8000@naver.com>
2021-06-18 13:01:39 +0000

    [feat] api 추가

--- mission4/11th/JoonHyungKim/index.js
+++ mission4/11th/JoonHyungKim/index.js
@@ -1,5 +1,8 @@
 import App from "./src/components/App.js"
+import TodoApi from "./src/api/TodoApi.js"
 
 const data = JSON.parse(localStorage.getItem("todo")) || []
 
 const app = new App({ $app: document.querySelector("#app"), initState: data })
+
+const api = new TodoApi('JHKIM');
