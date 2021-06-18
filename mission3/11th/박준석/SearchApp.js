90d3bbfbe8f4f8848c2fd43f46df9dfbdf9701c6
Bandito <49611158+Suppplier@users.noreply.github.com>
2021-06-18 15:17:42 +0900

    [mission3] 박준석 - communiateWithAPI 구조 및 위치 변경

--- mission3/11th/박준석/SearchApp.js
+++ mission3/11th/박준석/SearchApp.js
@@ -1,24 +1,23 @@
 import SearchInput from './SearchInput.js'
 import SearchResult from './SearchResult.js'
 import SearchHistory from './SearchHistory.js'
+import { communicateWithAPI } from './utils.js'
+
+const API_URL = 'https://jjalbot.com';
+const URL_TAIL = (inputValue) => {return `/api/jjals?text=${inputValue}`}
 
 export default class SearchApp {
   constructor() {
     this.state = []
     this.searchInput = new SearchInput(async (inputValue) => {
-      const receivedData = await this.communicateWithAPI(inputValue)
+      const receivedData = await communicateWithAPI(API_URL, URL_TAIL(inputValue))
       this.setState(receivedData, inputValue)
     })
     this.searchResult = new SearchResult(this.state, '#search-result')
     this.searchHistory = new SearchHistory([], async (keyword) => {
-      const receivedData = await this.communicateWithAPI(keyword)
+      const receivedData = await communicateWithAPI(API_URL, URL_TAIL(keyword))
       this.setState(receivedData)
     })
-  }
-
-  async communicateWithAPI(inputValue) {
-    const receivedData = await fetch(`https://jjalbot.com/api/jjals?text=${inputValue}`);
-    return await receivedData.json();
   }
 
   setState(receivedData, inputValue) {
--- mission3/11th/박준석/utils.js
+++ mission3/11th/박준석/utils.js
@@ -1,1 +1,11 @@
 export const $ = (ele, dom = document) => dom.querySelector(ele)
+
+
+export const communicateWithAPI = async (url, tail) => {
+    const receivedData = await fetch(`${url}${tail}`);
+    if (receivedData.ok){
+        return await receivedData.json();
+    } else{
+        throw new Error("Received Data is unsuitable!");
+    }
+  }
