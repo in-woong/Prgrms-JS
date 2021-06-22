b75a34a5fe8f5e4519620d1597b4107bd96ac450
onethegarden <onethegarden@gmail.com>
2021-06-23 07:51:13 +0900

    eol 누락된 파일 적용

--- mission4/11th/jeongwonHan/src/components/user/UserName.js
+++ mission4/11th/jeongwonHan/src/components/user/UserName.js
@@ -1,20 +1,20 @@
 function UserName({ $target, state }) {
-    this.$target = $target
-    this.$UserName = document.createElement('h2')
-    this.$UserName.classList.add('userName')
-    this.$target.appendChild(this.$UserName)
-    this.state = state
-  
-    this.setState = function (nextState) {
-      this.state = nextState
-      this.render()
-    }
-  
-    this.render = () => {
-      this.$UserName.innerHTML = `${this.state.userName}의 TodoList입니다`
-    }
-  
+  this.$target = $target
+  this.$UserName = document.createElement('h2')
+  this.$UserName.classList.add('userName')
+  this.$target.appendChild(this.$UserName)
+  this.state = state
+
+  this.setState = function (nextState) {
+    this.state = nextState
     this.render()
   }
-  export default UserName
-  
+
+  this.render = () => {
+    this.$UserName.innerHTML = `${this.state.userName}의 TodoList입니다`
+  }
+
+  this.render()
+}
+
+export default UserName
