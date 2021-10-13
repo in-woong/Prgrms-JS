778a82fa7262ddeda418f2fa829c966dc5e4491f
고슴도치짱 <36418489+lllilllilllilili@users.noreply.github.com>
2021-10-13 23:48:28 +0900

    [Mission4] 황인규 필수 구현사항 & 보너스 구현사항 (#607)
    * feat: 🎸 [Mission4] - 프로젝트 초기화
    
    * feat: 🎸 [Mission4] - Dir 구조정의, eslint 추가
    
    Directory 구조 정의 및 eslint 추가, Component 분리 미흡
    
    * feat: 🎸 [Mission4] - TodoList App 업그레이드하기2
    
    Component 분리, TodoList App 업그레이드하기2 명세서 내용 구현
    
    ✅ Closes: 106
    
    * feat: 🎸 [Mission4] - Users 컴포넌트
    
    User 목록을 화면에 렌더링!
    
    ✅ Closes: 107
    
    * feat: 🎸 [Mission4] - 보너스:: API가 느린 경우의 인터랙션 처리 / Fix Bug
    
    ✅ Closes: 144
    
    * feat: 🎸 [Mission4] - 미니트렐로 추가, self review 진행 X
    
    주석 처리, 리뷰 받은 코드 수정, self review 진행 필요
    
    * feat: 🎸 [Mission4] - 미니 트렐로, self 리뷰 후 코드 수정
    
    sing quota, double quota / eol / state 상태 변화 시 데이터 처리/ 미션 4 과제 수정
    
    BREAKING CHANGE: 🧨 180
    
    ✅ Closes: 180

---
+++ mission4/11th/mission4_황인규/index.js
@@ -0,0 +1,17 @@
+import main from "./main.js"
+import {$} from "./utils/utils.js";
+
+(function() {
+    
+    const app = new main($("app"), {
+        data : [],
+        username : "",
+        isLoading : false,
+        //FIXEDME :: todos 활용에 대한 고민 
+        // todos : [{
+        //     id : "",
+        //     isCompleted : false,
+        // }],
+    });
+  })()
+  
