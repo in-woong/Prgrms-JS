1a41a2c02cc8e22709d5a119033258e529a165b5
yoonhe <yhehe228@gmail.com>
2021-10-25 21:20:12 +0900

    [필수 구현] input에 할 일을 입력하여 TodoList 컴포넌트의 데이터에 추가하도록 합니다.

--- mission1/12th/heaeunYoon/index.js
+++ mission1/12th/heaeunYoon/index.js
@@ -1,22 +1,51 @@
 import TodoList from './TodoList.js'
 
-const TODOS = [
+let todos = [
   {
     text: 'todo text',
     isCompleted: false,
   },
 ]
 
-const NEW_TODOS = [
-  {
-    text: '[ new ] todo text',
-    isCompleted: false,
-  },
-]
-
 const todoListOne = new TodoList({
   selector: '#todo-list-one',
-  data: TODOS,
+  data: todos,
 })
 
-todoListOne.setState(NEW_TODOS)
+const input = document.querySelector('#inputBox input')
+const button = document.querySelector('#inputBox button')
+
+const focusInput = () => {
+  input.focus()
+}
+
+const addTodo = () => {
+  if (input.value === '') {
+    alert('할일을 입력해주세요 ! ')
+    focusInput()
+    return
+  }
+
+  const newTodo = {
+    text: input.value,
+    isCompleted: false,
+  }
+
+  todos = [newTodo, ...todos]
+
+  todoListOne.setState(todos)
+
+  input.value = ''
+
+  focusInput()
+}
+
+input.addEventListener('keypress', ({ key }) => {
+  if (key === 'Enter') {
+    addTodo()
+  }
+})
+
+button.addEventListener('click', () => {
+  addTodo()
+})
