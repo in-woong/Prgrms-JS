64218b441008b562c2741a28dab5bf89b953ea3f
slamdunk11 <ruret@naver.com>
2021-10-26 00:14:51 +0900

    addTodo 기능 생성

--- mission2/12th/boilerplate/TodoList.js
+++ mission2/12th/boilerplate/TodoList.js
@@ -15,3 +15,5 @@ function TodoList($target, data) {
 
   this.render()
 }
+
+
--- mission2/12th/boilerplate/main.js
+++ mission2/12th/boilerplate/main.js
@@ -1,11 +1,32 @@
 var data = [
   {
     text: 'JS 공부하기',
+    isCompleted: false,
   },
   {
     text: 'JS 복습하기',
+    isCompleted: false,
   },
 ]
 
 const $target = document.querySelector('#todo-list')
 const todoList = new TodoList($target, data)
+
+const addTodo = () => {
+  const todoInput = document.getElementById('todoInput').value;
+  if (todoInput.length > 0) {
+    this.data.push({ text: todoInput, isCompleted: false, });
+    todoList.setState(this.data)
+  }
+  document.getElementById('todoInput').value = ''
+}
+
+const checkTodo = () => {
+  const index = this.data.findIndex(i => i.text == 'dd')
+  this.data[index].isCompleted = !this.data[index].isCompleted
+}
+
+const deleteTodo = () => {
+
+}
+
