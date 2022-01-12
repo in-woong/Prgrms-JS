565eef43aa91ce7f9998e0738dd85005dec989a7
Jin <1128msj@gmail.com>
2022-01-13 08:21:37 +0900

    [Mission4] 문상진 - 필수 구현사항(#106) (#679)
    * 필수구현
    
    * async,await 디버깅
    
    Co-authored-by: 문상진(Sangjin Moon)/Data Infrastructure팀/SKP <sangjin@sk.com>

---
+++ mission4/12th/sangjinMoon/localStorage.js
@@ -0,0 +1,23 @@
+export const getItem = (key, defaultValue) => {
+  try {
+    const storedValue = localStorage.getItem(key)
+
+    return storedValue ? JSON.parse(storedValue) : defaultValue
+  } catch {
+    return defaultValue
+  }
+}
+
+export const setItem = (key, value) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(value))
+  } catch {
+    alert('데이터 추가에 실패했습니다. 너무 데이터가 많아서 그럴 수 있어요.')
+  }
+}
+
+
+export default {
+  getItem,
+  setItem
+}
