72b40b672863eea089f0836aac6d7c4a6fe99cba
강인웅 <woong@gang-in-ungs-MacBook-Air.local>
2022-01-22 12:05:09 +0900

    ADD / mission2_기본설정

---
+++ mission2/13th/inwoong/data.js
@@ -0,0 +1,33 @@
+'use strict'
+let data = [
+  {
+    text: 'JS 공부하기',
+    isCompleted: true,
+  },
+  {
+    text: 'JS 복습하기',
+    isCompleted: false,
+  },
+]
+
+let done = [
+  {
+    text: 'wow',
+    isCompleted: true,
+  },
+  {
+    text: 'Play',
+    isCompleted: false,
+  },
+]
+
+let buy = [
+  {
+    text: 'Macbook',
+    isCompleted: false,
+  },
+  {
+    text: 'iphone13',
+    isCompleted: true,
+  },
+]
