470cd96ce69779de0eec79a2b01017396685aa12
rotoshine <rotoshine@gmail.com>
2022-02-09 21:48:10 +0900

    SearchHistory 구현

--- mission3/13th/roto/App.js
+++ mission3/13th/roto/App.js
@@ -1,21 +1,57 @@
 import SearchInput from './SearchInput.js'
 import SearchResult from './SearchResult.js'
+import { fetchJjalImages } from './api.js'
+import SearchHistory from './SearchHistory.js'
 
 export default function App({ $target }) {
   this.state = {
+    keyword: '',
+    histories: [],
     searchResults: [],
   }
 
   const searchInput = new SearchInput({
     $target,
-    onSearch: (keyword) => {
-      fetch(`https://jjalbot.com/api/jjals?text=${keyword}`)
-        .then((x) => x.json())
-        .then((data) => {
-          this.setState({
-            searchResults: data,
-          })
+    initialState: this.state.keyword,
+    onSearch: async (keyword) => {
+      try {
+        const data = await fetchJjalImages(keyword)
+
+        const nextHistories = [...this.state.histories]
+
+        const historyIndex = nextHistories.findIndex(
+          (history) => history === keyword
+        )
+
+        if (historyIndex > -1) {
+          nextHistories.splice(historyIndex, 1)
+        }
+        nextHistories.push(keyword)
+
+        this.setState({
+          histories: nextHistories,
+          searchResults: data,
         })
+      } catch (e) {
+        alert(e.message)
+      }
+    },
+  })
+
+  const searchHistory = new SearchHistory({
+    $target,
+    onClick: async (keyword) => {
+      try {
+        const data = await fetchJjalImages(keyword)
+
+        this.setState({
+          keyword,
+          histories: this.state.histories,
+          searchResults: data,
+        })
+      } catch (e) {
+        alert(e.message)
+      }
     },
   })
 
@@ -30,6 +66,8 @@ export default function App({ $target }) {
   }
 
   this.render = () => {
+    searchInput.setState(this.state.keyword)
+    searchHistory.setState(this.state.histories)
     searchResult.setState(this.state.searchResults)
   }
 }
--- mission3/13th/roto/SearchInput.js
+++ mission3/13th/roto/SearchInput.js
@@ -1,13 +1,28 @@
-export default function SearchInput({ $target, onSearch }) {
+import debounce from './debounce.js'
+
+export default function SearchInput({ $target, initialState, onSearch }) {
+  this.state = initialState
   this.$input = document.createElement('input')
   this.$input.placeholder = '검색어를 입력하세요'
 
   $target.appendChild(this.$input)
 
-  this.render = () => {}
+  this.setState = (nextState) => {
+    if (typeof nextState === 'string' && nextState.length > 0) {
+      this.state = nextState
+      this.render()
+    }
+  }
 
-  this.$input.addEventListener('keyup', function (e) {
-    onSearch(e.target.value)
+  this.render = () => {
+    this.$input.value = this.state
+  }
+
+  this.$input.addEventListener('keyup', (e) => {
+    const { value } = e.target
+    if (value.length > 0) {
+      debounce(() => onSearch(value), 500)
+    }
   })
   this.render()
 }
