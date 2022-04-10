46fed5880e1393ee0b0e9727674d573d96fabaac
YoonHan <hyku2014@gmail.com>
2022-04-10 17:50:11 +0900

    API 모듈 추가, 에러메시지 모듈 추가

---
+++ mission4/14th/yoonhan/api.js
@@ -0,0 +1,90 @@
+const API_BASE_URL = 'https://todo-api.roto.codes/yoonhan';
+
+export async function fetchTodoListData() {
+  return await fetch(API_BASE_URL).then((res) => {
+    if (!res.ok) {
+      return;
+    }
+
+    try {
+      const data = res.json();
+      return data;
+    } catch (e) {
+      return e;
+    }
+  });
+}
+
+export async function addTodoItem(content) {
+  return await fetch(API_BASE_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content,
+    }),
+  }).then((res) => {
+    if (!res.ok) {
+      return;
+    }
+
+    try {
+      const data = res.json();
+      return data;
+    } catch (e) {
+      return e;
+    }
+  });
+}
+
+export async function deleteTodoItem(todoId) {
+  return await fetch(`${API_BASE_URL}/${todoId}`, {
+    method: 'DELETE',
+  }).then((res) => {
+    if (!res.ok) {
+      return;
+    }
+
+    try {
+      const data = res.json();
+      return data;
+    } catch (e) {
+      return e;
+    }
+  });
+}
+
+export async function deleteAllTodoItem() {
+  return await fetch(`${API_BASE_URL}/all`, {
+    method: 'DELETE',
+  }).then((res) => {
+    if (!res.ok) {
+      return;
+    }
+
+    try {
+      const data = res.json();
+      return data;
+    } catch (e) {
+      return e;
+    }
+  });
+}
+
+export async function toggleTodoItem() {
+  return await fetch(API_BASE_URL, {
+    method: 'PUT',
+  }).then((res) => {
+    if (!res.ok) {
+      return;
+    }
+
+    try {
+      const data = res.json();
+      return data;
+    } catch (e) {
+      return e;
+    }
+  });
+}
